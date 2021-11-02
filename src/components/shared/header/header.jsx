28914bd788927eb7ea757aea791c9fd9769c9b6f
Maksim Gorodov <silencer.web@gmail.com>
2021-11-02 21:04:16 +0500

    feat: add component Header

--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 
+import Button from 'components/shared/button';
 import Container from 'components/shared/container';
+import Link from 'components/shared/link';
+import Logo from 'images/logo.inline.svg';
+
+const links = [
+  {
+    text: 'Docs',
+    to: '/',
+  },
+  {
+    text: 'Pricing',
+    to: '/',
+  },
+  {
+    text: 'Team',
+    to: '/',
+  },
+  {
+    text: 'Changelog',
+    to: '/',
+  },
+  {
+    text: 'Blog',
+    to: '/',
+  },
+];
 
 const Header = () => (
-  <header>
-    <Container>Header!</Container>
+  <header className="py-8 bg-black">
+    <Container className="flex items-center justify-between">
+      <nav>
+        <ul className="flex space-x-12">
+          {links.map(({ text, to }) => (
+            <li>
+              <Link to={to} theme="normal-white" size="sm">
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Link className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" to="/">
+        <Logo />
+      </Link>
+      <Button to="/" size="xs" theme="tertiary">
+        Sign Up
+      </Button>
+    </Container>
   </header>
 );
 
