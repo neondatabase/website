1454b04b45185785909fb694e91501bfaed70aec
Maksim Gorodov <silencer.web@gmail.com>
2021-11-03 13:27:21 +0500

    feat: add section Video

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import CTA from 'components/pages/home/cta';
 import Hero from 'components/pages/home/hero';
+import Video from 'components/pages/home/video';
 import Layout from 'components/shared/layout';
 
 const HomePage = () => (
   <Layout>
     <Hero />
     <CTA />
+    <Video />
   </Layout>
 );
 
