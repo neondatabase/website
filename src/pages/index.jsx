c000d6b184e3d1b3539c0f22b3428fe18393dfae
Maksim Gorodov <silencer.web@gmail.com>
2021-11-03 12:15:12 +0500

    feat: add section Hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Hero from 'components/pages/home/hero';
 import Layout from 'components/shared/layout';
 
-const HomePage = () => <Layout>Hello world!</Layout>;
+const HomePage = () => (
+  <Layout>
+    <Hero />
+  </Layout>
+);
 
 export default HomePage;
