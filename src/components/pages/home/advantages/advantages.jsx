e6203d69477e79bf5b1b37107740f0c49a8bdbfb
Maksim Gorodov <silencer.web@gmail.com>
2021-11-03 14:20:24 +0500

    refactor: combine Video and Advantages components

--- src/components/pages/home/advantages/advantages.jsx
+++ src/components/pages/home/advantages/advantages.jsx
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import Container from 'components/shared/container';
@@ -6,10 +7,35 @@ import CostEfficientIcon from 'icons/cost-efficient.inline.svg';
 import EasyToUseIcon from 'icons/easy-to-use.inline.svg';
 import ScalableIcon from 'icons/scalable.inline.svg';
 
+import PlayIcon from './images/play.inline.svg';
+
 const Advantages = () => (
-  <section className="pt-40 bg-black pb-80">
+  <section className="bg-black py-80">
     <Container>
-      <ul className="grid grid-cols-12 gap-x-10">
+      <div className="flex items-center space-x-[100px]">
+        <div className="relative">
+          <StaticImage className="max-w-[800px]" src="./images/cover.jpg" />
+          <button
+            className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-40 h-40 flex items-center justify-center rounded-full before:bg-[#00ace6] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:mix-blend-multiply"
+            type="button"
+          >
+            <PlayIcon
+              className="relative rounded-full"
+              style={{ 'box-shadow': '0px 10px 20px rgba(26, 26, 26, 0.4)' }}
+            />
+          </button>
+        </div>
+        <div>
+          <Heading className="max-w-[490px]" tag="h2" size="lg" theme="white">
+            Distributed Database, Made Simple
+          </Heading>
+          <p className="max-w-[600px] t-xl mt-5 text-white">
+            Go through our 5 minutes tutorials video and start using scalable, cost efficient
+            database architecture for your project.
+          </p>
+        </div>
+      </div>
+      <ul className="grid grid-cols-12 mt-40 gap-x-10">
         <li className="col-span-3">
           <ScalableIcon />
           <Heading className="mt-6" tag="h3" size="sm" theme="white">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 import Advantages from 'components/pages/home/advantages';
 import CTA from 'components/pages/home/cta';
 import Hero from 'components/pages/home/hero';
-import Video from 'components/pages/home/video';
 import Layout from 'components/shared/layout';
 
 const HomePage = () => (
   <Layout>
     <Hero />
     <CTA />
-    <Video />
     <Advantages />
   </Layout>
 );
