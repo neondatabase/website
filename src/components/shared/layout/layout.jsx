2ed0e78f1e06afd8fed1f2351c3133212299c0b5
Maksim Gorodov <silencer.web@gmail.com>
2021-11-08 15:32:51 +0500

    fix: add min viewport

--- src/components/shared/layout/layout.jsx
+++ src/components/shared/layout/layout.jsx
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Footer from 'components/shared/footer';
 import Header from 'components/shared/header';
 import SEO from 'components/shared/seo';
 
-const Layout = ({ children }) => (
-  <>
-    <SEO />
-    <Header />
-    <main className="overflow-hidden">{children}</main>
-    <Footer />
-  </>
-);
+const Layout = ({ children }) => {
+  useEffect(() => {
+    if (window && window.screen.width < 500) {
+      const viewportTag = document.querySelector('meta[name="viewport"]');
+      viewportTag.setAttribute('content', 'width=414, shrink-to-fit=yes, viewport-fit=cover');
+    }
+  }, []);
+
+  return (
+    <>
+      <SEO />
+      <Header />
+      <main className="overflow-hidden">{children}</main>
+      <Footer />
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
