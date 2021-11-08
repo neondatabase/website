ad4cd1de85f9134661707130da83daa6705a09db
Maksim Gorodov <silencer.web@gmail.com>
2021-11-08 18:05:09 +0500

    fix: remove min viewport

--- src/components/shared/layout/layout.jsx
+++ src/components/shared/layout/layout.jsx
@@ -1,27 +1,18 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Footer from 'components/shared/footer';
 import Header from 'components/shared/header';
 import SEO from 'components/shared/seo';
 
-const Layout = ({ children }) => {
-  useEffect(() => {
-    if (window && window.screen.width < 500) {
-      const viewportTag = document.querySelector('meta[name="viewport"]');
-      viewportTag.setAttribute('content', 'width=414, shrink-to-fit=yes, viewport-fit=cover');
-    }
-  }, []);
-
-  return (
-    <>
-      <SEO />
-      <Header />
-      <main className="overflow-hidden">{children}</main>
-      <Footer />
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <SEO />
+    <Header />
+    <main className="overflow-hidden">{children}</main>
+    <Footer />
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
