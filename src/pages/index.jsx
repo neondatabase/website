c9b74655ceb7b9720a6fc12c201db713a4a32611
Maksim Gorodov <silencer.web@gmail.com>
2021-11-11 12:41:44 +0500

    feat: add a check for window width

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -530,18 +530,20 @@ function setPositionsForDottedHorizontalLines() {
 }
 
 function setPositionsForElements() {
-  setPositionsForVerticalLines();
-  setPositionsForHorizontalLines();
-  // We have to call setPositionsForVerticalLines again because there is a dependency on horizontal line
-  // which is being positioned after first call of setPositionsForVerticalLines
-  // We can't swap calls of these functions because horizontal lines depend on vertical lines too
-  // We will have to make additional call any way
-  setPositionsForVerticalLines();
-  setPositionsForShapes();
-  setPositionsForCircles();
-  setPositionsForCirclesWithText();
-  setPositionsForDottedVerticalLines();
-  setPositionsForDottedHorizontalLines();
+  if (window.innerWidth >= 1024) {
+    setPositionsForVerticalLines();
+    setPositionsForHorizontalLines();
+    // We have to call setPositionsForVerticalLines again because there is a dependency on horizontal line
+    // which is being positioned after first call of setPositionsForVerticalLines
+    // We can't swap calls of these functions because horizontal lines depend on vertical lines too
+    // We will have to make additional call any way
+    setPositionsForVerticalLines();
+    setPositionsForShapes();
+    setPositionsForCircles();
+    setPositionsForCirclesWithText();
+    setPositionsForDottedVerticalLines();
+    setPositionsForDottedHorizontalLines();
+  }
 }
 
 const Lines = () => {
