7aa3f15ba6d66e4f6a73f3bc4db812f51cd1abfa
Maksim Gorodov <silencer.web@gmail.com>
2021-11-12 17:23:00 +0500

    refactor: clean up Lines components

--- src/components/pages/home/lines-1/lines-1.jsx
+++ src/components/pages/home/lines-1/lines-1.jsx
@@ -4,6 +4,28 @@ import setPositionsForElements from './utils/setPositionsForElements';
 
 import './lines-1.css';
 
+const verticalLines = [...Array(9)];
+const horizontalLines = [...Array(20)];
+const shapes = [
+  'bottom-left',
+  'bottom-right',
+  'top-left',
+  'top-left',
+  'top-right',
+  'bottom-right',
+  'bottom-right',
+  'bottom-right',
+];
+const circles = [...Array(7)];
+const circlesWithText = [
+  '51e3bade-4507-4f44-b960-675a2a272fa6',
+  'fe495254-ff45-4a1e-9207-2f8aa6482547',
+  '3022b5f9-5eab-444c-890a-f63b3caa5d28',
+  '24a9b481-97a0-4316-b27e-d1da09e2992a',
+];
+const dottedVerticalLines = [...Array(3)];
+const dottedHorizontalLines = [...Array(1)];
+
 const Lines1 = () => {
   useEffect(() => {
     setPositionsForElements();
@@ -15,80 +37,45 @@ const Lines1 = () => {
 
   return (
     <div className="lg:hidden" aria-hidden>
-      <div id="lines-1-vertical-line-1" className="vertical-line" />
-      <div id="lines-1-vertical-line-2" className="vertical-line" />
-      <div id="lines-1-vertical-line-3" className="vertical-line" />
-      <div id="lines-1-vertical-line-4" className="vertical-line" />
-      <div id="lines-1-vertical-line-5" className="vertical-line" />
-      <div id="lines-1-vertical-line-6" className="vertical-line" />
-      <div id="lines-1-vertical-line-7" className="vertical-line" />
-      <div id="lines-1-vertical-line-8" className="vertical-line" />
-      <div id="lines-1-vertical-line-9" className="vertical-line" />
+      {verticalLines.map((_, index) => (
+        <div id={`lines-1-vertical-line-${index + 1}`} className="vertical-line" key={index} />
+      ))}
 
-      <div id="lines-1-horizontal-line-1" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-2" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-3" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-4" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-5" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-6" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-7" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-8" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-9" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-10" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-11" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-12" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-13" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-14" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-15" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-16" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-17" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-18" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-19" className="horizontal-line" />
-      <div id="lines-1-horizontal-line-20" className="horizontal-line" />
+      {horizontalLines.map((_, index) => (
+        <div id={`lines-1-horizontal-line-${index + 1}`} className="horizontal-line" key={index} />
+      ))}
 
-      <div id="lines-1-shape-1" className="shape shape-bottom-left" />
-      <div id="lines-1-shape-2" className="shape shape-bottom-right" />
-      <div id="lines-1-shape-3" className="shape shape-top-left" />
-      <div id="lines-1-shape-4" className="shape shape-top-left" />
-      <div id="lines-1-shape-5" className="shape shape-top-right" />
-      <div id="lines-1-shape-6" className="shape shape-bottom-right" />
-      <div id="lines-1-shape-7" className="shape shape-bottom-right" />
-      <div id="lines-1-shape-8" className="shape shape-bottom-right" />
+      {shapes.map((side, index) => (
+        <div id={`lines-1-shape-${index + 1}`} className={`shape shape-${side}`} />
+      ))}
 
-      <div id="lines-1-circle-1" className="circle" />
-      <div id="lines-1-circle-2" className="circle" />
-      <div id="lines-1-circle-3" className="circle" />
-      <div id="lines-1-circle-4" className="circle" />
-      <div id="lines-1-circle-5" className="circle" />
-      <div id="lines-1-circle-6" className="circle" />
-      <div id="lines-1-circle-7" className="circle" />
+      {circles.map((_, index) => (
+        <div id={`lines-1-circle-${index + 1}`} className="circle" key={index} />
+      ))}
 
-      <div
-        id="lines-1-circle-with-text-1"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="51e3bade-4507-4f44-b960-675a2a272fa6"
-      />
-      <div
-        id="lines-1-circle-with-text-2"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="fe495254-ff45-4a1e-9207-2f8aa6482547"
-      />
-      <div
-        id="lines-1-circle-with-text-3"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="3022b5f9-5eab-444c-890a-f63b3caa5d28"
-      />
-      <div
-        id="lines-1-circle-with-text-4"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="24a9b481-97a0-4316-b27e-d1da09e2992a"
-      />
+      {circlesWithText.map((text, index) => (
+        <div
+          id={`lines-1-circle-with-text-${index + 1}`}
+          className="circle circle-with-text circle-with-text-right"
+          data-text={text}
+        />
+      ))}
 
-      <div id="lines-1-dotted-vertical-line-1" className="dotted-vertical-line" />
-      <div id="lines-1-dotted-vertical-line-2" className="dotted-vertical-line" />
-      <div id="lines-1-dotted-vertical-line-3" className="dotted-vertical-line" />
+      {dottedVerticalLines.map((_, index) => (
+        <div
+          id={`lines-1-dotted-vertical-line-${index + 1}`}
+          className="dotted-vertical-line"
+          key={index}
+        />
+      ))}
 
-      <div id="lines-1-dotted-horizontal-line-1" className="dotted-horizontal-line" />
+      {dottedHorizontalLines.map((_, index) => (
+        <div
+          id={`lines-1-dotted-horizontal-line-${index + 1}`}
+          className="dotted-horizontal-line"
+          key={index}
+        />
+      ))}
     </div>
   );
 };
--- src/components/pages/home/lines-2/lines-2.jsx
+++ src/components/pages/home/lines-2/lines-2.jsx
@@ -4,6 +4,17 @@ import setPositionsForElements from './utils/setPositionsForElements';
 
 import './lines-2.css';
 
+const verticalLines = [...Array(11)];
+const horizontalLines = [...Array(16)];
+const shapes = ['bottom-right', 'bottom-right', 'top-left', 'bottom-right'];
+const circles = [...Array(2)];
+const circlesWithText = [
+  'ce3c2e9d-42e6-459e-9af5-cff5d83470f2',
+  '80943a52-b0cd-4451-8db7-24d794eb3d63',
+  '75a3a2b3-9d94-4709-a05a-593d996ab7d7',
+];
+const dottedVerticalLines = [...Array(1)];
+
 const Lines2 = () => {
   useEffect(() => {
     setPositionsForElements();
@@ -15,60 +26,37 @@ const Lines2 = () => {
 
   return (
     <div className="lg:hidden" aria-hidden>
-      <div id="lines-2-vertical-line-1" className="vertical-line" />
-      <div id="lines-2-vertical-line-2" className="vertical-line" />
-      <div id="lines-2-vertical-line-3" className="vertical-line" />
-      <div id="lines-2-vertical-line-4" className="vertical-line" />
-      <div id="lines-2-vertical-line-5" className="vertical-line" />
-      <div id="lines-2-vertical-line-6" className="vertical-line" />
-      <div id="lines-2-vertical-line-7" className="vertical-line" />
-      <div id="lines-2-vertical-line-8" className="vertical-line" />
-      <div id="lines-2-vertical-line-9" className="vertical-line" />
-      <div id="lines-2-vertical-line-10" className="vertical-line" />
-      <div id="lines-2-vertical-line-11" className="vertical-line" />
+      {verticalLines.map((_, index) => (
+        <div id={`lines-2-vertical-line-${index + 1}`} className="vertical-line" key={index} />
+      ))}
 
-      <div id="lines-2-horizontal-line-1" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-2" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-3" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-4" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-5" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-6" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-7" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-8" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-9" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-10" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-11" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-12" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-13" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-14" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-15" className="horizontal-line" />
-      <div id="lines-2-horizontal-line-16" className="horizontal-line" />
+      {horizontalLines.map((_, index) => (
+        <div id={`lines-2-horizontal-line-${index + 1}`} className="horizontal-line" key={index} />
+      ))}
 
-      <div id="lines-2-shape-1" className="shape shape-bottom-right" />
-      <div id="lines-2-shape-2" className="shape shape-bottom-right" />
-      <div id="lines-2-shape-3" className="shape shape-top-left" />
-      <div id="lines-2-shape-4" className="shape shape-bottom-right" />
+      {shapes.map((side, index) => (
+        <div id={`lines-2-shape-${index + 1}`} className={`shape shape-${side}`} />
+      ))}
 
-      <div id="lines-2-circle-1" className="circle" />
-      <div id="lines-2-circle-2" className="circle" />
+      {circles.map((_, index) => (
+        <div id={`lines-2-circle-${index + 1}`} className="circle" key={index} />
+      ))}
 
-      <div
-        id="lines-2-circle-with-text-1"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="ce3c2e9d-42e6-459e-9af5-cff5d83470f2"
-      />
-      <div
-        id="lines-2-circle-with-text-2"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="80943a52-b0cd-4451-8db7-24d794eb3d63"
-      />
-      <div
-        id="lines-2-circle-with-text-3"
-        className="circle circle-with-text circle-with-text-right"
-        data-text="75a3a2b3-9d94-4709-a05a-593d996ab7d7"
-      />
+      {circlesWithText.map((text, index) => (
+        <div
+          id={`lines-2-circle-with-text-${index + 1}`}
+          className="circle circle-with-text circle-with-text-right"
+          data-text={text}
+        />
+      ))}
 
-      <div id="lines-2-dotted-vertical-line-1" className="dotted-vertical-line" />
+      {dottedVerticalLines.map((_, index) => (
+        <div
+          id={`lines-2-dotted-vertical-line-${index + 1}`}
+          className="dotted-vertical-line"
+          key={index}
+        />
+      ))}
     </div>
   );
 };
