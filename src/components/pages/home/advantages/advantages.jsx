e1248c8a1c81e43d53e263ce1a9bc95a1ff2aca5
Maksim Gorodov <silencer.web@gmail.com>
2021-11-23 21:35:03 +0500

    fix: remove blinking animation from subtitles and change lottie in the section SaaS

--- src/components/pages/home/advantages/advantages.jsx
+++ src/components/pages/home/advantages/advantages.jsx
@@ -19,10 +19,8 @@ const Advantages = () => {
   const titleRef = useRef();
 
   const {
-    isAnimationPlaying: scalableIsAnimationPlaying,
     animationVisibilityRef: scalableAnimationVisibilityRef,
     animationRef: scalableAnimationRef,
-    animationEntry: scalableAnimationEntry,
   } = useLottie({
     lottieOptions: {
       animationData: scalableAnimationData,
@@ -31,10 +29,8 @@ const Advantages = () => {
   });
 
   const {
-    isAnimationPlaying: costEfficientIsAnimationPlaying,
     animationVisibilityRef: costEfficientAnimationVisibilityRef,
     animationRef: costEfficientAnimationRef,
-    animationEntry: costEfficientAnimationEntry,
   } = useLottie({
     lottieOptions: {
       animationData: costEfficientAnimationData,
@@ -43,10 +39,8 @@ const Advantages = () => {
   });
 
   const {
-    isAnimationPlaying: easyToUseIsAnimationPlaying,
     animationVisibilityRef: easyToUseAnimationVisibilityRef,
     animationRef: easyToUseAnimationRef,
-    animationEntry: easyToUseAnimationEntry,
   } = useLottie({
     lottieOptions: {
       animationData: easyToUseAnimationData,
@@ -56,28 +50,22 @@ const Advantages = () => {
 
   const items = [
     {
-      isAnimationPlaying: scalableIsAnimationPlaying,
       animationVisibilityRef: scalableAnimationVisibilityRef,
       animationRef: scalableAnimationRef,
-      animationEntry: scalableAnimationEntry,
       title: 'Scalable',
       description:
         'Separation of storage and compute. allows Zenith reconfigure amount of the compute power on the fly.',
     },
     {
-      isAnimationPlaying: costEfficientIsAnimationPlaying,
       animationVisibilityRef: costEfficientAnimationVisibilityRef,
       animationRef: costEfficientAnimationRef,
-      animationEntry: costEfficientAnimationEntry,
       title: 'Cost Efficient',
       description:
         'Being serverless allows using of resources on-demand, which significantly cuts the costs.',
     },
     {
-      isAnimationPlaying: easyToUseIsAnimationPlaying,
       animationVisibilityRef: easyToUseAnimationVisibilityRef,
       animationRef: easyToUseAnimationRef,
-      animationEntry: easyToUseAnimationEntry,
       title: 'Easy to Use',
       description:
         'No complex onboarding needed. Use a single CLI command to create a new Zenith database.',
@@ -143,51 +131,35 @@ const Advantages = () => {
           </div>
         </div>
         <ul className="grid grid-cols-12 mt-40 grid-gap 3xl:mt-36 2xl:mt-32 xl:mt-24 lg:gap-y-16 lg:mt-20 md:grid-cols-1">
-          {items.map(
-            (
-              {
-                isAnimationPlaying,
-                animationVisibilityRef,
-                animationEntry,
-                animationRef,
-                title,
-                description,
-              },
-              index
-            ) => (
-              <li
-                className="col-span-4 max-w-[410px] 3xl:max-w-[340px] 2xl:max-w-[312px] xl:max-w-[260px] lg:col-span-6 lg:max-w-[300px] md:max-w-none"
-                key={index}
-                ref={animationVisibilityRef}
+          {items.map(({ animationVisibilityRef, animationRef, title, description }, index) => (
+            <li
+              className="col-span-4 max-w-[410px] 3xl:max-w-[340px] 2xl:max-w-[312px] xl:max-w-[260px] lg:col-span-6 lg:max-w-[300px] md:max-w-none"
+              key={index}
+              ref={animationVisibilityRef}
+            >
+              <div
+                id={`advantages-item-${index + 1}-icon`}
+                className="w-24 h-24 2xl:w-20 2xl:h-20 xl:w-[72px] xl:h-[72px] lg:w-16 lg:h-16"
+                ref={animationRef}
+                aria-hidden
+              />
+              <Heading
+                id={`advantages-item-${index + 1}-title`}
+                className="mt-6 xl:mt-5"
+                tag="h3"
+                size="sm"
+                theme="white"
               >
-                <div
-                  id={`advantages-item-${index + 1}-icon`}
-                  className="w-24 h-24 2xl:w-20 2xl:h-20 xl:w-[72px] xl:h-[72px] lg:w-16 lg:h-16"
-                  ref={animationRef}
-                  aria-hidden
-                />
-                <Heading
-                  id={`advantages-item-${index + 1}-title`}
-                  className="mt-6 xl:mt-5"
-                  tag="h3"
-                  size="sm"
-                  theme="white"
-                >
-                  <BlinkingText
-                    text={title}
-                    parentElement={animationEntry?.target}
-                    shouldAnimationStart={isAnimationPlaying}
-                  />
-                </Heading>
-                <p
-                  id={`advantages-item-${index + 1}-description`}
-                  className="mt-4 text-white t-xl xl:mt-3.5"
-                >
-                  {description}
-                </p>
-              </li>
-            )
-          )}
+                {title}
+              </Heading>
+              <p
+                id={`advantages-item-${index + 1}-description`}
+                className="mt-4 text-white t-xl xl:mt-3.5"
+              >
+                {description}
+              </p>
+            </li>
+          ))}
         </ul>
       </Container>
     </section>
