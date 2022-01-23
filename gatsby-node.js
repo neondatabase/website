40e8142a732830499bef92337d401d8b682082ce
Stas Kelvich <stas.kelvich@gmail.com>
2022-01-23 13:27:42 +0300

    ci: mix in old website to target main domain here

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+
+const fs = require('fs-extra');
+
+//
+// I want to switch our current public domain to this repo and use it deploy system,
+// hence moving new webisite into a subfolder and an old one to root.
+//
+exports.onPostBuild = function () {
+  // Store gatsby files in public/new insteaad of public.
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'public-new'));
+  fs.mkdirSync(path.join(__dirname, 'public'));
+  fs.renameSync(path.join(__dirname, 'public-new'), path.join(__dirname, 'public', 'new'));
+
+  // Copy few assets folder to root as their callers will try find that files both with and
+  // without pathPrefix.
+  ['lottie-assets', 'fonts', 'page-data'].forEach((dir) => {
+    fs.copySync(path.join(__dirname, 'public', 'new', dir), path.join(__dirname, 'public', dir));
+  });
+
+  // Copy current website to public/.
+  fs.readdirSync(path.join(__dirname, 'terminal')).forEach((file) => {
+    console.log(file);
+    fs.copyFileSync(path.join(__dirname, 'terminal', file), path.join(__dirname, 'public', file));
+  });
+};
