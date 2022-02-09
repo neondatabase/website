c5781d75643f03b73e7ff82c6d1e5558651ccb91
Maksim Gorodov <silencer.web@gmail.com>
2022-02-09 15:48:22 +0500

    fix: add a small fix

--- src/components/shared/subscribe-minimalistic/subscribe-minimalistic.jsx
+++ src/components/shared/subscribe-minimalistic/subscribe-minimalistic.jsx
@@ -4,7 +4,7 @@ import Container from 'components/shared/container';
 import SubscriptionForm from 'components/shared/subscription-form';
 
 const SubscribeMinimalistic = () => (
-  <section className="safe-paddings mt-48 bg-secondary-6 py-32 text-center 3xl:mt-44 2xl:mt-40 2xl:py-28 xl:mt-32 xl:py-20 lg:mt-24 lg:py-16 md:mt-20">
+  <section className="safe-paddings mt-48 bg-secondary-6 py-32 3xl:mt-44 2xl:mt-40 2xl:py-28 xl:mt-32 xl:py-20 lg:mt-24 lg:py-16 lg:text-center md:mt-20">
     <Container
       className="flex items-center justify-center space-x-14 lg:block lg:space-x-0 lg:space-y-7"
       size="md"
