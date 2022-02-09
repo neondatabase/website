a24945ff16b9443b9c09142a4472ec2ca607fba7
Maksim Gorodov <silencer.web@gmail.com>
2022-02-09 15:07:47 +0500

    feat: remove font IBM Plex Sans medium

--- src/components/pages/team/team/team.jsx
+++ src/components/pages/team/team/team.jsx
@@ -207,7 +207,7 @@ const Team = () => (
           <li className="flex" key={index}>
             <div className="w-36 flex-shrink-0 xs:w-32">{photo}</div>
             <div className="ml-5 xs:ml-3">
-              <h3 className="max-w-[100px] text-2xl font-medium leading-snug md:max-w-none xs:text-[20px] xs:leading-tight">
+              <h3 className="max-w-[100px] text-2xl font-semibold leading-snug md:max-w-none xs:text-[20px] xs:leading-tight">
                 {name}
               </h3>
               <p className="t-base mt-2 !leading-snug text-gray-2 xs:mt-1">{position}</p>
