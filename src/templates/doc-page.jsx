519a11ae17ac7122e0b7177b624f38e499325224
Anastasia Videneeva <nastya.videneeva@gmail.com>
2022-03-18 12:29:51 -0700

    fix(styles): add responsive paddings for doc content

--- src/templates/doc-page.jsx
+++ src/templates/doc-page.jsx
@@ -22,19 +22,17 @@ const DocPage = ({ data: { mdx: docData }, pageContext }) => {
       pageMetadata={SEO_DATA.docs({ title: frontmatter.title, description: excerpt || null })}
     >
       <Container size="md" className="relative">
-        <div className="mb-auto flex h-full flex-grow lg:flex-col">
-          <div className="absolute top-48 left-[calc((100%-860px)/2-313px)] w-[313px] flex-shrink-0 2xl:relative 2xl:left-0 2xl:top-0 2xl:mt-48 2xl:w-[256px] lg:hidden">
+        <div className="safe-paddings mb-auto flex h-full flex-grow pt-48 3xl:pt-44 2xl:pt-40 xl:pt-32 lg:flex-col lg:pt-6">
+          <div className="absolute top-48 left-[calc((100%-860px)/2-313px)] w-[313px] flex-shrink-0 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[256px] lg:hidden">
             <Sidebar sidebar={pageContext.docSidebar} currentSlug={pageContext.id} />
           </div>
-          <div className="hidden w-full lg:mt-8 lg:block">
+          <div className="hidden w-full lg:mb-8 lg:block">
             <DocMobileNav sidebar={pageContext.docSidebar} currentSlug={pageContext.id} />
           </div>
-          <main className="mx-auto mt-48 w-[860px] pb-48 2xl:w-[calc(100%-256px)] lg:mt-14 lg:w-full md:mt-10">
+          <main className="mx-auto w-[860px] pb-48 3xl:pb-44 2xl:w-[calc(100%-256px)] 2xl:pb-40 xl:pb-32 lg:w-full lg:pb-24 md:pb-20">
             <article className="relative">
               <div className="relative flex flex-col">
-                <h1 className="text-[52px] font-semibold leading-tight md:text-5xl">
-                  {frontmatter.title}
-                </h1>
+                <h1 className="t-5xl font-semibold">{frontmatter.title}</h1>
                 <Content content={content} className="!mt-6 !max-w-full md:!mt-5" />
               </div>
             </article>
