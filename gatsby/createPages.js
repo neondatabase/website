481638dc40025c1648139ee803c840d711058460
Maksim Gorodov <silencer.web@gmail.com>
2022-04-07 10:49:04 +0400

    feat: enable generation of docs, blog and static pages

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -1,14 +1,11 @@
-const { IS_PRODUCTION } = require('./constants');
 const createBlogPages = require('./createBlogPages');
 const createBlogPosts = require('./createBlogPosts');
 const createDocPages = require('./createDocPages');
 const createStaticPages = require('./createStaticPages');
 
 module.exports = async (options) => {
-  if (!IS_PRODUCTION) {
-    await createBlogPages(options);
-    await createBlogPosts(options);
-    await createStaticPages(options);
-    await createDocPages(options);
-  }
+  await createBlogPages(options);
+  await createBlogPosts(options);
+  await createStaticPages(options);
+  await createDocPages(options);
 };
