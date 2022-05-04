eb1d57a5623ff165051883f63d679d0974ae5593
Stas Kelvich <stas.kelvich@gmail.com>
2022-05-04 23:55:02 +0300

    Update GTM code

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: 'GTM-56XZ7MT',
+        id: 'GTM-MJLTK6F',
         includeInDevelopment: false,
         routeChangeEventName: 'landing_route_change',
         dataLayerName: 'landing',
