43243670666f3bf2b2d3037b7b81711abe032055
Nikita Shamgunov <nikita@neon.tech>
2022-05-04 19:54:34 -0700

    kill open source

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -81,7 +81,7 @@ const Hero = () => {
           </Heading>
           <p className="t-xl mt-7 max-w-[680px] text-white 3xl:max-w-[654px] 2xl:mt-6 2xl:max-w-[480px] xl:mt-5 xl:max-w-[456px] lg:mx-auto lg:max-w-[414px]">
             Neon is a fully managed Postgres built for the cloud.
-            Neon separated storage and compute to offer autoscaling, branching, and infinite storage. Neon is open source is written in Rust.
+            Neon separated storage and compute to offer autoscaling, branching, and infinite storage.
           </p>
           <Button
             id="hero-button"
