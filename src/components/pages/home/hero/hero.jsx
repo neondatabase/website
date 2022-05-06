2e1b0ecfdca31220742da652c1e3c4b0581744e4
Nikita Shamgunov <nikita@neon.tech>
2022-05-06 08:19:56 -0700

    trim hero

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -82,8 +82,7 @@ const Hero = () => {
             {titleContentLg}
           </Heading>
           <p className="t-xl mt-7 max-w-[550px] text-white 2xl:mt-6 2xl:max-w-[480px] xl:mt-5 xl:max-w-[456px] lg:mx-auto lg:max-w-[414px]">
-            Neon is a fully managed Postgres with a generous free tier. Neon is built for the cloud
-            - it separated storage and compute to offer autoscaling, branching, and infinite
+            Neon is a fully managed Postgres with a generous free tier. Neon separated storage and compute to offer autoscaling, branching, and infinite
             storage.
           </p>
           <Button
