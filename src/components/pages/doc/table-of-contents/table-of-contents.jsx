1ec000200a8ee152b287209c0bde87538ae74543
den-bratchikov <denbratch@gmail.com>
2022-05-16 18:19:58 +0500

    chore: add smooth scroll

--- src/components/pages/doc/table-of-contents/table-of-contents.jsx
+++ src/components/pages/doc/table-of-contents/table-of-contents.jsx
@@ -11,27 +11,52 @@ const TableOfContents = ({ contentRef }) => {
     }
   }, [contentRef]);
 
+  const handleAnchorClick = (e, anchor) => {
+    e.preventDefault();
+    document.querySelector(anchor).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+    // changing hash without default jumps to anchor
+    // eslint-disable-next-line no-restricted-globals
+    if (history.pushState) {
+      // eslint-disable-next-line no-restricted-globals
+      history.pushState(false, false, anchor);
+    } else {
+      // old browser support
+      window.location.hash = anchor;
+    }
+  };
+
   if (items.length === 0) return null;
 
   return (
-    <div className="absolute top-0 bottom-10 col-start-11 col-end-13 h-full xl:hidden">
-      <nav className="sticky top-10 bottom-10 max-h-[calc(100vh-40px-40px)] max-w-[260px] space-y-2 overflow-y-auto overflow-x-hidden pt-2">
-        <span className="py-2.5 text-lg font-semibold leading-none">Table of contents</span>
+    <div className="col-start-11 col-end-13 h-full xl:hidden">
+      <nav className="sticky top-10 bottom-10 max-h-[calc(100vh-40px-40px)] overflow-y-auto overflow-x-hidden">
+        <h3 className="py-2.5 text-lg font-semibold leading-snug">Table of contents</h3>
         <ul>
           {items.map((item, index) => {
             const linkClassName =
-              'py-2.5 block text-base leading-none transition-colors duration-200 hover:text-primary-2';
+              'py-2.5 block text-base leading-snug transition-colors duration-200 hover:text-primary-2';
             const linkHref = `#${item.id}`;
 
             return (
               <li key={index}>
                 {item.localName === 'h2' && (
-                  <a className={clsx(linkClassName, 'font-semibold')} href={linkHref}>
+                  <a
+                    className={clsx(linkClassName, 'font-semibold')}
+                    href={linkHref}
+                    onClick={(e) => handleAnchorClick(e, linkHref)}
+                  >
                     {item.textContent}
                   </a>
                 )}
                 {item.localName === 'h3' && (
-                  <a className={clsx(linkClassName, 'ml-3')} href={linkHref}>
+                  <a
+                    className={clsx(linkClassName, 'ml-3')}
+                    href={linkHref}
+                    onClick={(e) => handleAnchorClick(e, linkHref)}
+                  >
                     {item.textContent}
                   </a>
                 )}
