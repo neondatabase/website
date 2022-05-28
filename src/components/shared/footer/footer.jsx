43a991845f806f7f425337bd65e4d79e80d6998d
Nikita Shamgunov <nikita@neon.tech>
2022-05-28 10:25:20 -0700

    made in SF

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => (
         </Link>
         <div className="t-base space-y-6 xl:space-y-4 md:mt-8">
           <p>
-            Maid in SF and the World
+            Made in SF and the World
           </p>
           <p>Neon 2022 Ⓒ All rights reserved</p>
         </div>
