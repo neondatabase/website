6bdd98d8bebe64d43fe06d57f1c2bc835688bf43
Anna Stepanyan <anna.stepanyan@zenith.tech>
2022-06-14 14:27:02 +0200

    docs: uncomment the blogs from the main page

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -4,8 +4,8 @@ const createDocPages = require('./createDocPages');
 // const createStaticPages = require('./createStaticPages');
 
 module.exports = async (options) => {
-  // await createBlogPages(options);
-  // await createBlogPosts(options);
+  await createBlogPages(options);
+  await createBlogPosts(options);
   // await createStaticPages(options);
   await createDocPages(options);
 };
