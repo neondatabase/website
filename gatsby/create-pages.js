e94324281babd041e85099db7613be67384ae3ac
Mac#3 <denbratch@gmail.com>
2022-08-11 15:47:09 +0500

    feat: add redirect from release-notes to Changelog

--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -13,4 +13,11 @@ module.exports = async (options) => {
   await createDocPages(options);
   await createChangelogPages(options);
   await createChangelogPosts(options);
+
+  const { createRedirect } = options.actions;
+
+  createRedirect({
+    fromPath: `/docs/release-notes/`,
+    toPath: `/changelog/`,
+  });
 };
