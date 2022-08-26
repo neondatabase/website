afee895d1c8984a5e61852aff69fb794f05c6c62
Thi Van Le <vannnyle@gmail.com>
2022-08-26 17:51:20 +0200

    feat: add 404 not found page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Hero from 'components/pages/404/hero';
+import Layout from 'components/shared/layout';
+
+const NotFoundPage = () => (
+  <Layout headerTheme="white">
+    <Hero />
+  </Layout>
+);
+
+export default NotFoundPage;
