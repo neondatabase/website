7aac11d96cd82ef65d178dcf4d35702f6f2d9220
Thi Van Le <vannnyle@gmail.com>
2022-08-29 15:37:42 +0200

    fix: misc fixes

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -51,14 +51,14 @@ const SEO = ({
       {/* General */}
       <meta name="description" content={currentDescription} />
       {metaKeywords ? <meta name="keywords" content={metaKeywords} /> : null}
-      {isRobotsNoindexPage && <meta name="robots" content="noindex" />}
+      {isRobotsNoindexPage ? <meta name="robots" content="noindex" /> : null}
       {/* Open Graph */}
       <meta property="og:title" content={currentTitle} />
       <meta property="og:description" content={currentDescription} />
       <meta property="og:url" content={currentUrl} />
       <meta property="og:image" content={currentImagePath} />
       <meta property="og:type" content="website" />
-      {facebook && <meta property="fb:app_id" content={facebook.appId} />}
+      {facebook ? <meta property="fb:app_id" content={facebook.appId} /> : null}
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       {children}
