5c7e0e249ef3923d24ee0c29f49b4f1cbe26a235
Thi Van Le <vannnyle@gmail.com>
2022-09-02 17:03:47 +0200

    fix: remove guideline

--- src/templates/changelog-post.jsx
+++ src/templates/changelog-post.jsx
@@ -3,7 +3,6 @@ import { graphql } from 'gatsby';
 import React from 'react';
 import slugify from 'slugify';
 
-import Aside from 'components/pages/changelog-content/aside';
 import Hero from 'components/pages/changelog-content/hero/hero';
 import Container from 'components/shared/container';
 import Content from 'components/shared/content';
@@ -15,6 +14,7 @@ import SubscribeMinimalistic from 'components/shared/subscribe-minimalistic';
 import { CHANGELOG_BASE_PATH } from 'constants/changelog';
 import SEO_DATA from 'constants/seo-data';
 import AnchorIcon from 'icons/anchor.inline.svg';
+import getChangelogPostDateFromSlug from 'utils/get-changelog-post-date-from-slug';
 
 const ChangelogPostTemplate = ({
   data: {
@@ -26,34 +26,40 @@ const ChangelogPostTemplate = ({
     <Layout headerTheme="white">
       <Hero />
       <Container size="sm" className="relative mb-10 flex">
-        <article className="relative flex sm:flex-col">
-          <Aside slug={slug} label={frontmatter.label} />
-          <div className="relative before:absolute before:top-3.5 before:bottom-16 before:-left-11 before:h-auto before:w-px before:bg-gray-3 xl:before:hidden sm:mt-3">
-            <Heading
-              className="relative mb-5 !text-2xl leading-normal before:absolute before:top-3.5 before:-left-[49.5px] before:h-3 before:w-3 before:rounded-full before:border before:border-black before:bg-white xl:before:hidden"
-              tag="h3"
-              size="sm"
-              theme="black"
+        <article className="relative flex flex-col items-start">
+          <Heading tag="h3" size="sm" theme="black">
+            {frontmatter.label} release
+          </Heading>
+          <time
+            className="mt-3 whitespace-nowrap text-gray-2"
+            dateTime={getChangelogPostDateFromSlug(slug)}
+          >
+            {getChangelogPostDateFromSlug(slug)}
+          </time>
+          <Heading
+            className="relative mt-10 mb-5 !text-2xl leading-normal"
+            tag="h3"
+            size="sm"
+            theme="black"
+          >
+            <a
+              className="anchor absolute top-0 left-0 flex h-full -translate-x-full items-center justify-center px-2.5 opacity-0 transition-opacity duration-200 hover:opacity-100  group-hover:opacity-100 sm:group-hover:opacity-0"
+              href={`#${id}`}
+              aria-hidden
             >
-              <a
-                className="anchor absolute top-0 left-0 flex h-full -translate-x-full items-center justify-center px-2.5 opacity-0 transition-opacity duration-200 hover:opacity-100  group-hover:opacity-100 sm:group-hover:opacity-0"
-                href={`#${id}`}
-                aria-hidden
-              >
-                <AnchorIcon className="w-4" />
-              </a>
-              {frontmatter.title}
-            </Heading>
-            <Content content={body} />
-            <Link
-              className="mt-10 font-semibold lg:mt-8"
-              to={CHANGELOG_BASE_PATH}
-              size="sm"
-              theme="black-primary-1"
-            >
-              back to all changelogs
-            </Link>
-          </div>
+              <AnchorIcon className="w-4" />
+            </a>
+            {frontmatter.title}
+          </Heading>
+          <Content content={body} />
+          <Link
+            className="mt-10 font-semibold lg:mt-8"
+            to={CHANGELOG_BASE_PATH}
+            size="sm"
+            theme="black-primary-1"
+          >
+            back to all changelogs
+          </Link>
         </article>
       </Container>
       <SubscribeMinimalistic />
