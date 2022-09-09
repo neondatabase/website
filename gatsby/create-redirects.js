323908e55a2a71eaa95c1c0362b3fe1a68d72b74
Thi Van Le <vannnyle@gmail.com>
2022-09-09 18:05:49 +0200

    feat: add redirect validation

--- gatsby/create-redirects.js
+++ gatsby/create-redirects.js
@@ -3,6 +3,11 @@ module.exports = ({ redirectFrom, actions, pagePath }) => {
   // Default value of redirectFrom is ['']
   if (redirectFrom[0].length > 0) {
     redirectFrom.forEach((redirectFromPath) => {
+      // Required redirectForm validation
+      if (!redirectFromPath.startsWith('/')) {
+        throw new Error(`Redirect "${redirectFromPath}" does not start with '/'!`);
+      }
+
       actions.createRedirect({
         fromPath: redirectFromPath,
         toPath: pagePath,
