04af139ce6b081e52b20cad898f2cadf61380e60
Thi Van Le <vannnyle@gmail.com>
2022-10-01 13:34:03 +0200

    fix: minor fixes

--- src/components/pages/release-notes/release-note-list/release-note-list.jsx
+++ src/components/pages/release-notes/release-note-list/release-note-list.jsx
@@ -5,11 +5,11 @@ import Aside from 'components/pages/release-notes/aside';
 import Content from 'components/shared/content';
 
 const ReleaseNoteList = ({ items }) => (
-  <div className="relative space-y-12 before:absolute before:top-4 before:bottom-3 before:left-[178px] before:h-auto before:w-px before:bg-gray-3 xl:before:hidden sm:space-y-16">
+  <div className="relative space-y-12 before:absolute before:top-4 before:bottom-3 before:left-[180px] before:h-auto before:w-px before:bg-gray-3 xl:before:hidden sm:space-y-16">
     {items.map(({ body, slug, frontmatter: { label } }, index) => (
       <article className="relative flex sm:flex-col sm:space-y-3" key={index}>
         <Aside slug={slug} label={label} />
-        <div className="relative pl-56 before:absolute before:top-3 before:left-[173px] before:h-[11px] before:w-[11px] before:rounded-full before:bg-gray-1 xl:pl-0 xl:before:hidden">
+        <div className="relative pl-56 before:absolute before:top-3 before:left-[175px] before:h-[11px] before:w-[11px] before:rounded-full before:bg-gray-1 xl:pl-0 xl:before:hidden">
           <Content className="prose-h3:text-xl" content={body} showH3Anchors={false} />
         </div>
       </article>
