e269f26d6726fde6aeb9a2bb91a282d1aab101a7
Tatiana Nerucheva <tatiana@nerucheva.com>
2022-10-03 13:49:57 +0200

    feat(components): wrap the post author block with a link if it provided

--- src/components/pages/blog-post/hero/hero.jsx
+++ src/components/pages/blog-post/hero/hero.jsx
@@ -3,35 +3,45 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Hero = ({ title, description, author, date, readingTime, className }) => (
-  <div className={clsx('safe-paddings', className)}>
-    <h1 className="t-5xl font-semibold">{title}</h1>
-    <p className="t-2xl mt-6 xl:mt-5">{description}</p>
-    <div className="mt-8 flex items-center justify-between border-b border-b-gray-4 pb-8 2xl:mt-7 2xl:pb-7 xl:mt-6 xl:pb-6 sm:flex-col sm:items-start sm:space-y-6">
-      {author && (
-        <div className="flex items-center">
-          <GatsbyImage
-            className="w-10 shrink-0 rounded-full"
-            imgClassName="rounded-full"
-            image={getImage(author.postAuthor?.image?.localFile)}
-            alt={author.title}
-            loading="eager"
-          />
-          <span className="t-lg">
-            <span className="ml-3 font-semibold xs:ml-1.5">{author.title}</span>,{' '}
-            <span>{author.postAuthor?.role}</span>
-          </span>
-        </div>
-      )}
-      <span className="t-base flex items-center text-gray-2">
-        <span>{date}</span>
-        <span className="relative ml-3 pl-4 before:absolute before:top-1/2 before:left-0 before:inline-flex before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-gray-3">
-          {readingTime} min read
-        </span>
+import Link from 'components/shared/link';
+
+const Hero = ({ title, description, author, date, readingTime, className }) => {
+  const postAuthor = author && (
+    <div className="flex items-center">
+      <GatsbyImage
+        className="w-10 shrink-0 rounded-full"
+        imgClassName="rounded-full"
+        image={getImage(author.postAuthor?.image?.localFile)}
+        alt={author.title}
+        loading="eager"
+      />
+      <span className="t-lg">
+        <span className="ml-3 font-semibold xs:ml-1.5">{author.title}</span>,{' '}
+        <span>{author.postAuthor?.role}</span>
       </span>
     </div>
-  </div>
-);
+  );
+
+  return (
+    <div className={clsx('safe-paddings', className)}>
+      <h1 className="t-5xl font-semibold">{title}</h1>
+      <p className="t-2xl mt-6 xl:mt-5">{description}</p>
+      <div className="mt-8 flex items-center justify-between border-b border-b-gray-4 pb-8 2xl:mt-7 2xl:pb-7 xl:mt-6 xl:pb-6 sm:flex-col sm:items-start sm:space-y-6">
+        {postAuthor && author.postAuthor.url ? (
+          <Link to={author.postAuthor.url}>{postAuthor}</Link>
+        ) : (
+          postAuthor
+        )}
+        <span className="t-base flex items-center text-gray-2">
+          <span>{date}</span>
+          <span className="relative ml-3 pl-4 before:absolute before:top-1/2 before:left-0 before:inline-flex before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-gray-3">
+            {readingTime} min read
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
@@ -43,6 +53,7 @@ Hero.propTypes = {
       image: PropTypes.shape({
         localFile: PropTypes.shape({}),
       }),
+      url: PropTypes.string,
     }),
   }).isRequired,
   date: PropTypes.string.isRequired,
