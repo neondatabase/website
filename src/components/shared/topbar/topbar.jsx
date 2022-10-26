a47dc1a90e5d770bd0d4b922e49ca8098725e373
Raouf Chebri <raoufchebri@Raoufs-MacBook-Pro.local>
2022-10-26 14:28:06 +0200

    feat(constants): update blog url

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -6,7 +6,7 @@ import ArrowRightIcon from 'icons/arrow-right.inline.svg';
 const TopBar = () => (
   <Link
     className="safe-paddings relative z-40 flex h-11 w-full items-center justify-center bg-primary-1 px-4 py-3 transition-colors duration-200 hover:bg-[#1AFFB2] xs:h-auto"
-    to="/blog/postgresql-15-three-features-you-can-now-try-on-neon"
+    to="/blog/postgresql-15-three-features-you-can-try-with-neon/"
   >
     <span className="mr-4 border-r border-black border-opacity-20 py-1 pr-4 text-sm font-semibold">
       Neon supports PostgreSQL 15
