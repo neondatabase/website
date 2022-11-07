dad29ed35956ed2016fc29f045a4f43e734b5f0e
Thi Van Le <vannnyle@gmail.com>
2022-11-07 17:04:21 +0200

    fix: make chevron clickable in mobile-nav

--- src/components/pages/doc/mobile-nav/mobile-nav.jsx
+++ src/components/pages/doc/mobile-nav/mobile-nav.jsx
@@ -18,7 +18,7 @@ const MobileNav = ({ className, sidebar, currentSlug }) => {
     <nav className={clsx('safe-paddings relative border-b border-gray-7 bg-gray-9', className)}>
       <Container size="lg">
         <select
-          className="w-full appearance-none text-ellipsis bg-gray-9 py-3 outline-none"
+          className="relative z-10 w-full cursor-pointer appearance-none text-ellipsis bg-transparent py-3 outline-none"
           value={currentSlug}
           onChange={handleChange}
         >
