42b1c371bacb8492c6d9eff568df1ca4c2daea54
Thi Van Le <vannnyle@gmail.com>
2022-11-14 15:19:19 +0100

    fix: add feedback for both states

--- src/components/pages/doc/doc-footer/doc-footer.jsx
+++ src/components/pages/doc/doc-footer/doc-footer.jsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
-import { useLocalStorage } from 'react-use';
+import React, { useState } from 'react';
+import { useSessionStorage } from 'react-use';
 
 import Link from 'components/shared/link';
 import GitHubIcon from 'icons/github.inline.svg';
@@ -11,21 +11,15 @@ import ThumbsDownIcon from './images/thumbs-down.inline.svg';
 import ThumbsUpIcon from './images/thumbs-up.inline.svg';
 
 const DocFooter = ({ fileOriginPath, slug }) => {
-  const [isFeedbackPositive, setIsFeedbackPositive] = useState(false);
-  const [feedbackSent, setFeedbackSent] = useLocalStorage('isPageFeedbackSubmitted', []);
-  const [feedbackParams, setFeedbackParams] = useState({});
-
-  useEffect(() => {
-    setFeedbackParams({
-      isSent: Object.keys(feedbackSent).includes(slug),
-      isPositive: feedbackSent?.[slug]?.includes(true),
-      isNegative: feedbackSent?.[slug]?.includes(false),
-    });
-  }, [feedbackSent, slug]);
+  const [isFeedbackSent, setIsFeedbackSent] = useState(false);
+  const [feedbackSentData, setFeedbackSentData] = useSessionStorage('isPageFeedbackSubmitted', []);
 
   const handleFeedbackClick = (isPositive) => {
-    setFeedbackSent({ ...feedbackSent, [slug]: [isPositive] });
-    setIsFeedbackPositive(isPositive);
+    setFeedbackSentData({ ...feedbackSentData, [slug]: isPositive });
+    setIsFeedbackSent(true);
+    if (feedbackSentData[slug]) {
+      return;
+    }
     sendGtagEvent('page-feedback', {
       rate: isPositive ? 1 : 0,
     });
@@ -47,40 +41,25 @@ const DocFooter = ({ fileOriginPath, slug }) => {
         <div
           className={clsx(
             'flex items-center space-x-5 transition-opacity duration-200 xs:flex-col xs:space-x-0 xs:space-y-4',
-            isFeedbackPositive ? 'invisible opacity-0' : 'visible opacity-100'
+            isFeedbackSent ? 'invisible opacity-0' : 'visible opacity-100'
           )}
         >
           <span className="text-sm leading-tight">Was this page helpful?</span>
           <div className="flex space-x-3">
             <button
               className="group flex items-center space-x-1 rounded border border-gray-7 px-2.5 py-1.5 text-xs leading-none"
-              disabled={feedbackParams.isSent}
               type="button"
-              onClick={() => handleFeedbackClick(true)}
+              onClick={() => handleFeedbackClick(true, slug)}
             >
-              <ThumbsUpIcon
-                className={clsx(
-                  'h-2.5 w-2.5 shrink transition-colors duration-200',
-                  feedbackParams.isPositive ? 'text-primary-1' : 'text-gray-4',
-                  !feedbackParams.isSent && 'group-hover:text-primary-1 group-active:text-primary-1'
-                )}
-              />
+              <ThumbsUpIcon className="h-2.5 w-2.5 shrink transition-colors duration-200 group-hover:text-primary-1 group-active:text-primary-1" />
               <span>Yes</span>
             </button>
             <button
               className="group flex items-center space-x-1 rounded border border-gray-7 px-2.5 py-1.5 text-xs leading-none"
-              disabled={feedbackParams.isSent}
               type="button"
-              onClick={() => handleFeedbackClick(false)}
+              onClick={() => handleFeedbackClick(false, slug)}
             >
-              <ThumbsDownIcon
-                className={clsx(
-                  'h-2.5 w-2.5 shrink transition-colors duration-200 ',
-                  feedbackParams.isNegative ? 'text-secondary-1' : 'text-gray-4',
-                  !feedbackParams.isSent &&
-                    'group-hover:text-secondary-1 group-active:text-secondary-1'
-                )}
-              />
+              <ThumbsDownIcon className="h-2.5 w-2.5 shrink transition-colors duration-200 group-hover:text-secondary-1 group-active:text-secondary-1" />
               <span>No</span>
             </button>
           </div>
@@ -88,7 +67,7 @@ const DocFooter = ({ fileOriginPath, slug }) => {
         <span
           className={clsx(
             'absolute right-0 top-1/2 -translate-y-1/2 text-sm leading-tight transition-opacity duration-300',
-            isFeedbackPositive ? 'visible opacity-100' : 'invisible opacity-0'
+            isFeedbackSent ? 'visible opacity-100' : 'invisible opacity-0'
           )}
         >
           Thank you for your feedback!
