4b14e91443e5210cb6a61decd1c81ae16c9ce7ca
Raouf Chebri <raouf@chebri.com>
2022-11-22 12:11:39 +0100

    Update hero.jsx

--- src/components/pages/developer-days/hero/hero.jsx
+++ src/components/pages/developer-days/hero/hero.jsx
@@ -34,8 +34,11 @@ const Hero = () => (
           Neon Developer Days
         </h1>
         <p className="mt-5 max-w-[640px] text-center text-xl leading-normal md:text-lg">
-          Join us virtually on December 6th, 7th, and 8th to learn about Neon and how to build
+          Join us virtually every day, from the 6th to the 8th of December at 9:00 AM PST to learn about Neon and how to build
           better with Serverless Postgres. Register for event updates.
+        </p>
+        <p className="mt-5 max-w-[640px] text-center text-xl leading-normal md:text-lg">
+          Register for event updates.
         </p>
         <div className="relative">
           <Line className="absolute top-[calc(100%-30px)] right-16 2xl:top-[calc(100%-10px)] lg:hidden" />
@@ -48,6 +51,12 @@ const Hero = () => (
             formId={HUBSPOT_DEVELOPER_DAYS_1_FORM_ID}
           />
         </div>
+        <p className="mt-5 max-w-[640px] text-center text-xl leading-normal md:text-lg">
+          Add the event to your calendar         
+          <a target="_blank" className="mt-4" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NXUxdmdrMzRjaWM3Y3VyOGQ5cTBkdWJrOWxfMjAyMjEyMDZUMTcwMDAwWiBjXzkxMzJmMGI2NGIyZGFkZjgyZDliNDcxZmI4MjQ3OGJjM2Q4ZDMzNmRkMjg0MWI0MmZmOTZmMjIzYTdjMjI1NzNAZw&amp;tmsrc=c_9132f0b64b2dadf82d9b471fb82478bc3d8d336dd2841b42ff96f223a7c22573%40group.calendar.google.com&amp;scp=ALL">
+            <img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif">
+          </a>
+        </p>
       </div>
     </Container>
   </div>
