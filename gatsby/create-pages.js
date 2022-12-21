a89dcd4bc57a9867ae69e83acebf81d5835b28e3
Den-Bratchikov <denbratch@gmail.com>
2022-12-22 00:47:23 +0500

    fix: comment createStaticPages function for test

--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -3,12 +3,12 @@ const createBlogPages = require('./create-blog-pages');
 const createBlogPosts = require('./create-blog-posts');
 const createDocPages = require('./create-doc-pages');
 const createReleaseNotePost = require('./create-release-note-post');
-const createStaticPages = require('./create-static-pages');
+// const createStaticPages = require('./create-static-pages');
 
 module.exports = async (options) => {
   await createBlogPages(options);
   await createBlogPosts(options);
-  await createStaticPages(options);
+  // await createStaticPages(options);
   await createDocPages(options);
   await createAllPages(options);
   await createReleaseNotePost(options);
