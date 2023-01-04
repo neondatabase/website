885f25ac8268cceec74c37283119f5b7f73e52d9
Thi Van Le <vannnyle@gmail.com>
2023-01-04 18:07:18 +0100

    feat: integrate form with hubspot

--- src/components/pages/pricing/hero/hero.jsx
+++ src/components/pages/pricing/hero/hero.jsx
@@ -1,69 +1,177 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { StaticImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useCookie, useLocation } from 'react-use';
+import * as yup from 'yup';
 
 import Button from 'components/shared/button';
 import Container from 'components/shared/container';
 import Field from 'components/shared/field';
 import Link from 'components/shared/link';
+import { HUBSPOT_PRICING_FORM_ID } from 'constants/forms';
+import { sendHubspotFormData } from 'utils/forms';
 
-const Hero = () => (
-  <section className="bg-black pt-44 pb-40 text-white">
-    <Container className="text-center" size="md">
-      <h1 className="text-[72px] font-bold leading-tight">Talk to our Sales team</h1>
-      <p className="mx-auto mt-1.5 max-w-[787px] text-xl">
-        Are you interested in exceeding your free tier limits and learn about pricing? Please fill
-        out the form provided below and our team will review your submission
-      </p>
-      <div className="mx-auto mt-16 flex max-w-[1216px]">
-        <div className="relative w-full max-w-[696px] shrink-0 before:absolute before:inset-0 before:h-full before:w-full before:rounded-[20px] before:bg-primary-1 before:opacity-60 before:blur-[70px]">
-          <form
-            className="relative z-10 flex flex-col space-y-10 rounded-xl bg-gray-1 p-12"
-            style={{ boxShadow: '0px 20px 40px rgba(26, 26, 26, 0.4)' }}
-          >
-            <Field name="name" label="Your name *" />
-            <Field name="email" label="Email address *" type="email" />
-            <div className="flex space-x-10">
-              <Field className="basis-[340px]" name="company_website" label="Company website" />
-              <Field name="company_size" label="Company size" />
-            </div>
-            <Field name="message" label="Message" tag="textarea" />
-            <div className="flex items-center justify-between space-x-7">
-              <Button className="px-9 py-6 text-lg font-bold leading-none" theme="primary">
-                Send message
-              </Button>
-              <p className="leading-tight">
-                By submitting you agree to{' '}
-                <Link className="pb-1" to="/privacy" theme="underline-primary-1" size="xs">
-                  Neon’s Privacy Policy
-                </Link>
-                .
-              </p>
-            </div>
-          </form>
-        </div>
-        <div className="relative my-10 grow rounded-r-[20px] bg-secondary-2 pt-10 font-mono text-black before:absolute before:inset-0 before:h-full before:w-full before:rounded-[20px] before:bg-secondary-2 before:opacity-70 before:blur-[70px]">
-          <div className="relative z-10 flex h-full flex-col">
-            <h2
-              className="text-[56px] font-bold leading-none"
-              style={{
-                background: 'linear-gradient(180deg, #1A1A1A 0%, rgba(26, 26, 26, 0.8) 100%)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                backgroundClip: 'text',
-                textFillColor: 'transparent',
-              }}
+const schema = yup
+  .object({
+    name: yup.string().required('Your name is a required field'),
+    email: yup
+      .string()
+      .email('Please enter a valid email')
+      .required('Email address is a required field'),
+  })
+  .required();
+
+const Hero = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const [hubspotutk] = useCookie('hubspotutk');
+  const { href } = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const context = {
+    hutk: hubspotutk,
+    pageUri: href,
+  };
+
+  const onSubmit = async (data) => {
+    const { name, email, companyWebsite, companySize, message } = data;
+    setIsLoading(true);
+    try {
+      const response = await sendHubspotFormData({
+        formId: HUBSPOT_PRICING_FORM_ID,
+        context,
+        values: [
+          {
+            name: 'full_name',
+            value: name,
+          },
+          {
+            name: 'email',
+            value: email,
+          },
+          {
+            name: 'company_website',
+            value: companyWebsite,
+          },
+          {
+            name: 'company_size',
+            value: companySize,
+          },
+          {
+            name: 'message',
+            value: message,
+          },
+        ],
+      });
+
+      if (response.ok) {
+        setIsLoading(false);
+        setIsSuccess(true);
+      } else {
+        setIsLoading(false);
+        setErrorMessage('Something went wrong. Please reload the page and try again.');
+      }
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+      setErrorMessage('Something went wrong. Please reload the page and try again.');
+    }
+  };
+  return (
+    <section className="bg-black pt-44 pb-40 text-white">
+      <Container className="text-center" size="md">
+        <h1 className="text-[72px] font-bold leading-tight">Talk to our Sales team</h1>
+        <p className="mx-auto mt-1.5 max-w-[787px] text-xl">
+          Are you interested in exceeding your free tier limits and learn about pricing? Please fill
+          out the form provided below and our team will review your submission
+        </p>
+        <div className="mx-auto mt-16 flex max-w-[1216px]">
+          <div className="relative w-full max-w-[696px] shrink-0 before:absolute before:inset-0 before:h-full before:w-full before:rounded-[20px] before:bg-primary-1 before:opacity-60 before:blur-[70px]">
+            <form
+              className="relative z-10 grid gap-y-10 rounded-xl bg-gray-1 p-12 pb-14"
+              style={{ boxShadow: '0px 20px 40px rgba(26, 26, 26, 0.4)' }}
+              onSubmit={handleSubmit(onSubmit)}
             >
-              On Demand!
-            </h2>
-            <p className="mt-2.5 text-[23px] font-bold leading-snug text-[#3E3E29]">
-              Only pay for what you use.
-            </p>
-            <StaticImage class="mt-auto" src="./images/illustration.png" />
+              <Field
+                name="name"
+                label="Your name *"
+                register={register('name')}
+                error={errors.name?.message}
+              />
+              <Field
+                name="email"
+                label="Email address *"
+                type="email"
+                register={register('email')}
+                error={errors.email?.message}
+              />
+              <div className="flex space-x-10">
+                <Field
+                  className="basis-[340px]"
+                  name="companyWebsite"
+                  label="Company website"
+                  register={register('companyWebsite')}
+                />
+                <Field name="companySize" label="Company size" register={register('companySize')} />
+              </div>
+              <Field name="message" label="Message" tag="textarea" register={register('message')} />
+              <div className="relative mt-2 flex items-center">
+                <Button
+                  className="w-[194px] !px-9 !py-6 !text-lg"
+                  type="submit"
+                  loading={isLoading}
+                  theme="primary"
+                  size="xs"
+                  disabled={isLoading || isSuccess}
+                >
+                  {isSuccess ? 'Sent!' : 'Send message'}
+                </Button>
+                <p className="ml-7 leading-tight">
+                  By submitting you agree to{' '}
+                  <Link className="pb-1" to="/privacy" theme="underline-primary-1" size="xs">
+                    Neon’s Privacy Policy
+                  </Link>
+                  .
+                </p>
+                {errorMessage && (
+                  <span className="absolute left-1/2 top-[calc(100%+1rem)] w-full -translate-x-1/2 text-sm leading-none text-secondary-1">
+                    {errorMessage}
+                  </span>
+                )}
+              </div>
+            </form>
+          </div>
+          <div className="relative my-9 grow rounded-r-[20px] bg-secondary-2 pt-10 font-mono text-black before:absolute before:inset-0 before:h-full before:w-full before:rounded-[20px] before:bg-secondary-2 before:opacity-70 before:blur-[70px]">
+            <div className="relative z-10 flex h-full flex-col">
+              <h2
+                className="text-[56px] font-bold leading-none"
+                style={{
+                  background: 'linear-gradient(180deg, #1A1A1A 0%, rgba(26, 26, 26, 0.8) 100%)',
+                  WebkitBackgroundClip: 'text',
+                  WebkitTextFillColor: 'transparent',
+                  backgroundClip: 'text',
+                  textFillColor: 'transparent',
+                }}
+              >
+                On Demand!
+              </h2>
+              <p className="mt-2.5 text-[23px] font-bold leading-snug text-[#3E3E29]">
+                Only pay for what you use.
+              </p>
+              <StaticImage className="mt-auto" src="./images/illustration.png" alt="Illustration" />
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-  </section>
-);
+      </Container>
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/shared/field/field.jsx
+++ src/components/shared/field/field.jsx
@@ -2,21 +2,30 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Field = ({ className, name, label, type, tag: Tag }) => (
-  <div className={clsx('flex flex-col items-start', className)}>
+const Field = ({ className, name, label, type, tag: Tag, register, error }) => (
+  <div className={clsx('relative flex flex-col items-start', className)}>
     <label className="leading-none" htmlFor={name}>
       {label}
     </label>
     <Tag
       className={clsx(
-        'remove-autocomplete-styles mt-2.5 w-full rounded border-[3px] border-transparent bg-[#404040] px-4 py-3.5 text-lg leading-none outline-none transition-colors duration-200 focus:border-primary-1 focus:bg-black',
+        'remove-autocomplete-styles-black-theme mt-2.5 w-full rounded border-[3px] bg-[#404040] px-4 py-3.5 text-lg leading-none text-white outline-none transition-colors duration-200 focus:bg-black',
         Tag === 'input' && 'h-14',
-        Tag === 'textarea' && 'min-h-[112px]'
+        Tag === 'textarea' && 'min-h-[112px]',
+        error
+          ? 'border-secondary-1 focus:border-secondary-1'
+          : 'border-transparent focus:border-primary-1'
       )}
       id={name}
       name={name}
       type={type}
+      {...register}
     />
+    {error && (
+      <p className="absolute top-[calc(100%+0.5rem)] text-sm leading-none text-secondary-1">
+        {error}
+      </p>
+    )}
   </div>
 );
 
@@ -26,12 +35,16 @@ Field.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
   tag: PropTypes.oneOf(['input', 'textarea']),
+  register: PropTypes.shape({}),
+  error: PropTypes.string,
 };
 
 Field.defaultProps = {
   className: null,
   type: 'text',
   tag: 'input',
+  register: {},
+  error: null,
 };
 
 export default Field;
