fa9b8b624cba1e5b59c79b87a40c671834d51299
Thi Van Le <vannnyle@gmail.com>
2023-01-10 13:34:49 +0100

    feat: additional mask

--- src/components/pages/pricing/hero/hero.jsx
+++ src/components/pages/pricing/hero/hero.jsx
@@ -96,7 +96,7 @@ const Hero = () => {
                   className={clsx(
                     'relative w-full max-w-[696px] shrink-0 2xl:max-w-[535px] lg:max-w-none',
                     'before:shadow-gradient-primary-1 before:absolute before:inset-0 before:h-full before:w-full before:rounded-[22px] before:shadow-[0_0_70px_30px_rgb(0_230_153/30%)] sm:before:shadow-none',
-                    'after:pointer-events-none after:absolute after:-inset-40 after:rounded-[22px] after:opacity-10 after:[filter:url("#noiseFilter")] after:[-webkit-mask-size:100%_100%] after:[mask-image:url("/images/mask.svg")] after:[-webkit-mask-repeat:no-repeat] after:[-webkit-mask-position:center_center] lg:after:hidden'
+                    'after:pointer-events-none after:absolute after:-inset-40 after:rounded-[22px] after:opacity-10 after:[filter:url("#noiseFilter")] after:[-webkit-mask-size:100%_100%] after:[-webkit-mask-repeat:no-repeat] after:[-webkit-mask-position:center_center] after:[mask-image:url("/images/mask-lg.svg")] lg:after:hidden'
                   )}
                 >
                   <ContactForm formState={formState} setFormState={setFormState} />
@@ -105,7 +105,7 @@ const Hero = () => {
                   className={clsx(
                     'relative my-9 flex-1 font-mono text-black lg:my-0',
                     'before:absolute before:inset-0 before:h-full before:w-full before:rounded-[22px] before:bg-secondary-2 before:shadow-[0_0_70px_30px_rgb(240_240_117/30%)] sm:before:shadow-none',
-                    'after:pointer-events-none after:absolute after:-inset-40 after:rounded-[22px] after:opacity-10 after:[filter:url("#noiseFilter")] after:[-webkit-mask-size:100%_100%] after:[mask-image:url("/images/mask.svg")] after:[-webkit-mask-repeat:no-repeat] after:[-webkit-mask-position:center_center] lg:after:hidden'
+                    'after:pointer-events-none after:absolute after:-inset-40 after:rounded-[22px] after:opacity-10 after:[filter:url("#noiseFilter")] after:[-webkit-mask-size:100%_100%] after:[-webkit-mask-repeat:no-repeat] after:[-webkit-mask-position:center_center] after:[mask-image:url("/images/mask-md.svg")] lg:after:hidden'
                   )}
                 >
                   <div className="relative z-10 flex h-full w-full flex-col overflow-hidden rounded-r-[20px] bg-secondary-2 pt-10 lg:flex-row lg:rounded-[20px] lg:pt-0 md:flex-col md:items-center">
