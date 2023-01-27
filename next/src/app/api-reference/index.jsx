683c00a7b188265c77c9df8c0f74e6a9f3837fd3
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-01-27 23:38:48 +0200

    chore: add api-reference

---
+++ next/src/app/api-reference/index.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import SwaggerUI from 'swagger-ui';
+
+import 'swagger-ui/dist/swagger-ui.css';
+
+const ApiReferencePage = () => {
+  React.useEffect(() => {
+    fetch('https://dqjnwjfwjj8yz.cloudfront.net/api_spec/release/v1.json')
+      .then((res) => res.json())
+      .then((spec) => {
+        spec.servers = [
+          {
+            url: 'https://console.neon.tech/api/v1',
+          },
+        ];
+
+        SwaggerUI({
+          spec,
+          dom_id: '#swagger',
+          deepLinking: true,
+          filter: true,
+        });
+      });
+  }, []);
+
+  return <div id="swagger" />;
+};
+
+export default ApiReferencePage;
