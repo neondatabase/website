437c0db5c29a19bb12169ebaf8fec641847432ca
Raman Yasel <raman.yasel@gmail.com>
2023-01-27 16:44:23 +0300

    fix: fix eslint errors

--- next/src/pages/404.jsx
+++ next/src/pages/404.jsx
@@ -2,10 +2,10 @@ import Head from 'next/head';
 
 import 'styles/globals.css';
 
-import SEO_DATA from 'constants/seo-data';
 import SEO from 'components/shared/seo';
 import Layout from 'components/shared/layout';
 import Hero from 'components/pages/404/hero';
+import SEO_DATA from 'constants/seo-data';
 
 const GlobalNotFoundPage = () => (
   <>
