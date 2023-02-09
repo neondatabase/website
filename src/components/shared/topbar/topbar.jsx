129ae9b99fc7773554306ca0ff586c21512afaee
Raouf Chebri <raouf@chebri.com>
2023-02-09 16:51:04 +0100

    update banner

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -6,10 +6,10 @@ import ArrowRightIcon from 'icons/arrow-right.inline.svg';
 const TopBar = () => (
   <Link
     className="safe-paddings relative z-40 flex h-11 w-full items-center justify-center bg-primary-1 px-4 py-3 leading-none transition-colors duration-200 hover:bg-[#1AFFB2] xs:h-auto"
-    to="/developer-days-1"
+    to="/neon-vercel-integration"
   >
     <span className="mr-4 truncate border-r border-black border-opacity-20 py-1 pr-4 text-sm font-semibold">
-      Watch Neon Developer Days 🚀
+      Neon 🤝 Vercel: Create a branch for every Preview Deployment
     </span>
     <span className="inline-flex items-center text-sm font-bold xs:hidden">
       <span>Read More</span>
