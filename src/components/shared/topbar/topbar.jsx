818a762ff1213fea1bef924645f5de6e7ccd41b9
Raouf Chebri <raouf@chebri.com>
2023-02-10 14:31:33 +0100

    Update topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -6,7 +6,7 @@ import ArrowRightIcon from 'icons/arrow-right.inline.svg';
 const TopBar = () => (
   <Link
     className="safe-paddings relative z-40 flex h-11 w-full items-center justify-center bg-primary-1 px-4 py-3 leading-none transition-colors duration-200 hover:bg-[#1AFFB2] xs:h-auto"
-    to="/neon-vercel-integration"
+    to="/blog/neon-vercel-integration"
   >
     <span className="mr-4 truncate border-r border-black border-opacity-20 py-1 pr-4 text-sm font-semibold">
       Neon integration on Vercel: Create a branch for every Preview Deployment
