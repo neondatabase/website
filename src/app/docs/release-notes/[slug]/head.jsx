12152a6034dea12b8c1a5e978ebc7b1016c68b06
Raman Yasel <raman.yasel@gmail.com>
2023-03-15 13:51:47 +0300

    feat: add rss feed generate task

--- src/app/docs/release-notes/[slug]/head.jsx
+++ src/app/docs/release-notes/[slug]/head.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import SEO from 'components/shared/seo';
+import { RELEASE_NOTES_BASE_PATH } from 'constants/docs';
 import SEO_DATA from 'constants/seo-data';
 import { getPostBySlug, RELEASE_NOTES_DIR_PATH } from 'utils/api-docs';
 
@@ -10,11 +11,19 @@ const Head = ({ params }) => {
   } = getPostBySlug(currentSlug, RELEASE_NOTES_DIR_PATH);
 
   return (
-    <SEO
-      {...SEO_DATA.releaseNotePost({
-        title: `${label} release`,
-      })}
-    />
+    <>
+      <SEO
+        {...SEO_DATA.releaseNotePost({
+          title: `${label} release`,
+        })}
+      />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="Release Notes RSS feed"
+        href={`${RELEASE_NOTES_BASE_PATH}/rss.xml`}
+      />
+    </>
   );
 };
 
