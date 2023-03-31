e646c2bcf0fec8af53dd2f3a642b2c345d595d1b
Raman Yasel <raman.yasel@gmail.com>
2023-03-31 15:23:15 +0300

    feat: add adaptive calculator styles

--- src/components/pages/pricing/calculator/calculator.jsx
+++ src/components/pages/pricing/calculator/calculator.jsx
@@ -98,13 +98,13 @@ const Calculator = () => {
           </h2>
         </div>
 
-        <div className="mx-auto mt-12 grid max-w-[1220px] grid-cols-[1fr_338px] gap-[10px]">
-          <div className="row-span-1 flex rounded-lg bg-gray-1">
-            <div className="grow px-7 py-6">
-              <h3 className="text-2xl font-medium leading-none tracking-tight text-white">
+        <div className="mx-auto mt-12 grid max-w-[1220px] grid-cols-[1fr_338px] gap-[10px] 2xl:grid-cols-[1fr_314px] xl:mt-10 lg:grid-cols-1 sm:mt-6">
+          <div className="row-span-1 flex rounded-lg bg-gray-1 md:flex-col">
+            <div className="grow px-7 py-6 xl:py-5 xl:px-6 md:px-5 md:pb-3">
+              <h3 className="text-2xl font-medium leading-none tracking-tight text-white xl:text-xl">
                 Compute
               </h3>
-              <div className="mt-8 flex flex-col gap-2">
+              <div className="mt-8 flex flex-col gap-2 md:mt-7">
                 <div className="flex justify-between">
                   <h4 className="font-medium leading-none tracking-tight">
                     <span>Compute size</span>
@@ -122,7 +122,7 @@ const Calculator = () => {
                     {COMPUTE_UNITS_VALUES.min}
                   </span>
                   <Slider.Root
-                    className="relative mx-4 flex h-5 w-64 grow touch-none items-center"
+                    className="md:w-aut md:pb-3o relative mx-4 flex h-5 w-64 grow touch-none items-center"
                     defaultValue={[COMPUTE_UNITS_VALUES.default]}
                     min={COMPUTE_UNITS_VALUES.min}
                     max={COMPUTE_UNITS_VALUES.max}
@@ -131,7 +131,7 @@ const Calculator = () => {
                     onValueChange={(value) => setComputeUnits(value[0])}
                   >
                     <Slider.Track className="relative h-[6px] w-full grow rounded-[10px] bg-gray-2">
-                      <Slider.Range className="absolute h-full h-[6px] rounded-full bg-primary-1" />
+                      <Slider.Range className="absolute h-full rounded-full bg-primary-1" />
                     </Slider.Track>
                     <Slider.Thumb
                       className={clsx(
@@ -147,7 +147,7 @@ const Calculator = () => {
                   </span>
                 </div>
               </div>
-              <div className="mt-8 flex flex-col gap-2">
+              <div className="mt-8 flex flex-col gap-2 xl:mt-6 md:mt-6">
                 <div className="flex justify-between">
                   <h4 className="font-medium leading-none tracking-tight">
                     Active time
@@ -165,7 +165,7 @@ const Calculator = () => {
                     {COMPUTE_TIME_VALUES.min} h
                   </span>
                   <Slider.Root
-                    className="relative mx-4 flex h-5 w-64 grow touch-none items-center"
+                    className="md:w-aut md:pb-3o relative mx-4 flex h-5 w-64 grow touch-none items-center"
                     defaultValue={[COMPUTE_TIME_VALUES.default]}
                     min={COMPUTE_TIME_VALUES.min}
                     max={COMPUTE_TIME_VALUES.max}
@@ -174,7 +174,7 @@ const Calculator = () => {
                     onValueChange={(value) => setActiveTime(value[0])}
                   >
                     <Slider.Track className="relative h-[6px] w-full grow rounded-[10px] bg-gray-2">
-                      <Slider.Range className="absolute h-full h-[6px] rounded-full bg-primary-1" />
+                      <Slider.Range className="absolute h-full rounded-full bg-primary-1" />
                     </Slider.Track>
                     <Slider.Thumb
                       className={clsx(
@@ -191,21 +191,25 @@ const Calculator = () => {
                 </div>
               </div>
             </div>
-            <div className="flex w-[189px] shrink-0 flex-col items-center border-l border-dashed border-[#303236] py-8">
-              <h2 className="text-lg font-medium leading-none tracking-tight">Subtotal</h2>
-              <p className="mt-12 text-[40px] leading-none tracking-tighter text-[#00E599]">
+            <div className="flex w-[189px] shrink-0 flex-col items-center border-l border-dashed border-[#303236] py-8 xl:w-[155px] lg:w-[168px] md:w-full md:flex-row md:items-center md:justify-between md:border-l-0 md:border-t md:px-5 md:py-5">
+              <h2 className="text-lg font-medium leading-none tracking-tight md:text-base">
+                Subtotal
+              </h2>
+              <p className="mt-12 text-[40px] leading-none tracking-tighter text-[#00E599] xl:text-3xl md:mt-0 md:text-xl">
                 ${computeTimeCost.toFixed(2)}
+                <span className="block text-center text-sm leading-none tracking-normal text-[#EFEFF0] md:ml-1 md:inline-block md:align-middle">
+                  /month
+                </span>
               </p>
-              <span className="text-sm leading-none text-[#EFEFF0]">/month</span>
-              <span className="mt-12 text-sm leading-none text-[#EFEFF0]">
+              <span className="mt-12 text-sm leading-none text-[#EFEFF0] xl:mt-10 md:hidden">
                 <span className="text-[#00E599]">${COMPUTE_TIME_PRICE}</span> / hour
               </span>
             </div>
           </div>
 
-          <div className="row-span-1 flex rounded-lg bg-gray-1">
-            <div className="grow px-7 py-6 pb-8">
-              <h3 className="text-2xl font-medium leading-none tracking-tighter text-white">
+          <div className="row-span-1 flex rounded-lg bg-gray-1 md:flex-col">
+            <div className="grow px-7 py-6 pb-8 xl:py-5 xl:px-6 md:px-5 md:pb-3">
+              <h3 className="text-2xl font-medium leading-none tracking-tighter text-white xl:text-xl">
                 Project storage
               </h3>
               <div className="mt-7 flex flex-col gap-2">
@@ -220,7 +224,7 @@ const Calculator = () => {
                     {STORAGE_VALUES.min} GiB
                   </span>
                   <Slider.Root
-                    className="relative mx-4 flex h-5 w-64 grow touch-none items-center"
+                    className="relative mx-4 flex h-5 w-64 grow touch-none items-center md:w-auto"
                     defaultValue={[STORAGE_VALUES.default]}
                     min={STORAGE_VALUES.min}
                     max={STORAGE_VALUES.max}
@@ -229,7 +233,7 @@ const Calculator = () => {
                     onValueChange={(value) => setStorageValue(value[0])}
                   >
                     <Slider.Track className="relative h-[6px] w-full grow rounded-[10px] bg-gray-2">
-                      <Slider.Range className="absolute h-full h-[6px] rounded-full bg-primary-1" />
+                      <Slider.Range className="absolute h-full rounded-full bg-primary-1" />
                     </Slider.Track>
                     <Slider.Thumb
                       className={clsx(
@@ -244,17 +248,22 @@ const Calculator = () => {
                 </div>
               </div>
             </div>
-            <div className="flex w-[189px] shrink-0 flex-col items-center justify-center border-l border-dashed border-[#303236] py-8">
-              <p className="text-[40px] leading-none tracking-tighter text-[#00E599]">
+            <div className="flex w-[189px] shrink-0 flex-col items-center justify-center border-l border-dashed border-[#303236] py-8 xl:w-[155px] lg:w-[168px] md:w-full md:flex-row md:items-center md:justify-between md:border-l-0 md:border-t md:px-5 md:py-5">
+              <h2 className="hidden text-base font-medium leading-none tracking-tight md:block">
+                Subtotal
+              </h2>
+              <p className="text-[40px] leading-none tracking-tighter text-[#00E599] xl:text-3xl md:text-xl">
                 ${storageCost.toFixed(2)}
+                <span className="block text-center text-sm leading-none tracking-normal text-[#EFEFF0] md:ml-1 md:inline-block md:align-middle">
+                  /month
+                </span>
               </p>
-              <span className="text-sm leading-none text-[#EFEFF0]">/month</span>
             </div>
           </div>
 
-          <div className="row-span-1 flex rounded-lg bg-gray-1">
-            <div className="grow px-6 pt-7 pb-9">
-              <h3 className="text-2xl font-medium leading-none tracking-tight text-white [text-shadow:0px_0px_20px_rgba(255,_255,_255,_0.05)]">
+          <div className="row-span-1 flex rounded-lg bg-gray-1 md:flex-col">
+            <div className="grow px-6 pt-7 pb-9 xl:py-8 lg:pb-6 md:px-5 md:py-5">
+              <h3 className="text-2xl font-medium leading-none tracking-tight text-white [text-shadow:0px_0px_20px_rgba(255,_255,_255,_0.05)] xl:text-xl">
                 Data transfer and Written data
               </h3>
               {isAdvanced ? (
@@ -294,7 +303,7 @@ const Calculator = () => {
                 <p className="mt-5 text-base tracking-tight text-[#94979E]">
                   Accounts for x% of your monthly cost, on average.
                   <button
-                    className="relative mx-2 text-primary-1 after:absolute after:-bottom-1 after:left-0 after:h-[1px] after:w-full after:bg-primary-1 after:opacity-40"
+                    className="relative mx-2 text-primary-1 after:absolute after:-bottom-1 after:left-0 after:h-[1px] after:w-full after:bg-primary-1 after:opacity-40 xl:mx-0 xl:block"
                     type="button"
                     onClick={() => setIsAdvanced(true)}
                   >
@@ -303,46 +312,51 @@ const Calculator = () => {
                 </p>
               )}
             </div>
-            <div className="flex w-[189px] shrink-0 flex-col items-center justify-center border-l border-dashed border-[#303236]">
-              <p className="text-[40px] leading-none tracking-tighter text-[#00E599]">
+            <div className="flex w-[189px] shrink-0 flex-col items-center justify-center border-l border-dashed border-[#303236] xl:w-[155px] lg:w-[168px] md:w-full md:flex-row md:items-center md:justify-between md:border-l-0 md:border-t md:px-5 md:py-5">
+              <h2 className="hidden text-base font-medium leading-none tracking-tight md:block">
+                Subtotal
+              </h2>
+              <p className="text-[40px] leading-none tracking-tighter text-[#00E599] xl:text-3xl md:text-xl">
                 ${writtenAndTransferDataCost.toFixed(2)}
+                <span className="block text-center text-sm leading-none tracking-normal text-[#EFEFF0] md:ml-1 md:inline-block md:align-middle">
+                  /month
+                </span>
               </p>
-              <span className="text-sm leading-none text-[#EFEFF0]">/month</span>
             </div>
           </div>
 
-          <div className="col-start-2 row-span-3 row-start-1 flex flex-col rounded-lg bg-secondary-2 p-7 pb-9">
-            <h3 className="text-center text-lg font-semibold leading-none tracking-tight text-black [text-shadow:0px_0px_20px_rgba(255,_255,_255,_0.05)]">
+          <div className="col-start-2 row-span-3 row-start-1 flex flex-col rounded-lg bg-secondary-2 p-7 pb-9 lg:col-start-1 lg:row-span-1 lg:grid lg:grid-cols-2 lg:gap-x-60 sm:grid-cols-1 sm:gap-x-0">
+            <h3 className="text-center text-lg font-semibold leading-none tracking-tight text-black [text-shadow:0px_0px_20px_rgba(255,_255,_255,_0.05)] lg:col-start-2 sm:col-start-1">
               Estimated price
             </h3>
-            <p className="mt-8 text-center text-[72px] font-medium leading-none tracking-tighter text-black">
+            <p className="mt-8 text-center text-[72px] font-medium leading-none tracking-tighter text-black xl:mt-10 xl:text-[60px] lg:col-start-2 sm:col-start-1 sm:mt-8">
               <span>{estimatedPrice}</span>
               <span className="mt-1 block text-base tracking-normal">/per month</span>
             </p>
-            <ul className="my-11 flex flex-col space-y-8">
-              <li className="relative flex pl-[3.25rem] text-lg leading-none tracking-tight text-black after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05]">
+            <ul className="my-11 flex flex-col space-y-8 text-lg leading-none tracking-tight text-black lg:col-span-1 lg:row-span-3 lg:row-start-1 lg:my-0 lg:self-center sm:row-span-1 sm:my-8 sm:items-center">
+              <li className="relative flex pl-[3.25rem] after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05] lg:pl-0">
                 <CheckIcon className="mr-2" />
                 <span className="mr-1 font-semibold">{computeUnits}</span>
                 <span>compute units</span>
               </li>
-              <li className="relative flex pl-[3.25rem] text-lg leading-none tracking-tight text-black after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05]">
+              <li className="relative flex pl-[3.25rem] after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05] lg:pl-0">
                 <CheckIcon className="mr-2" />
                 <span className="mr-1 font-semibold">{storageValue} GiB</span>
                 <span>storage</span>
               </li>
-              <li className="relative flex pl-[3.25rem] text-lg leading-none tracking-tight text-black after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05]">
+              <li className="relative flex pl-[3.25rem] text-black after:absolute after:left-0 after:-bottom-4 after:h-[1px] after:w-full after:bg-[#0C0D0D] after:opacity-[0.05] lg:pl-0">
                 <CheckIcon className="mr-2" />
                 {isAdvanced && <span className="mr-1 font-semibold">{writtenDataValue} GiB</span>}
                 <span>written data</span>
               </li>
-              <li className="relative flex pl-[3.25rem] text-lg leading-none tracking-tight text-black">
+              <li className="relative flex pl-[3.25rem] lg:pl-0">
                 <CheckIcon className="mr-2" />
                 {isAdvanced && <span className="mr-1 font-semibold">{dataTransferValue} GiB</span>}
                 <span>data transfer</span>
               </li>
             </ul>
             <Button
-              className="mt-auto w-full !bg-black py-6 !text-lg !text-white sm:max-w-none"
+              className="mt-auto w-full !bg-black py-6 !text-lg !text-white xl:mt-4 lg:col-start-2 lg:mt-8 sm:col-start-1 sm:mx-auto sm:mt-1 sm:max-w-[200px]"
               theme="primary"
               to="https://console.neon.tech/sign_in"
               size="sm"
@@ -365,7 +379,7 @@ const Tooltip = ({ content, id }) => (
     >
       <InfoIcon />
     </span>
-    <span className="pointer-events-none absolute left-[calc(100%+10px)] top-1/2 z-50 w-[15rem] -translate-y-1/2 rounded-[4px] rounded-sm bg-[#242628] px-4 py-1.5 text-sm font-normal leading-snug leading-none tracking-tight text-[#AFB1B6] opacity-0 shadow-tooltip transition-opacity duration-200 peer-hover:opacity-100 lg:static lg:mt-1.5 lg:translate-y-0 lg:bg-transparent lg:p-0 lg:opacity-100">
+    <span className="pointer-events-none absolute left-[calc(100%+10px)] top-1/2 z-50 w-[15rem] -translate-y-1/2 rounded-[4px] bg-gray-2 px-4 py-1.5 text-sm font-normal leading-snug tracking-tight text-[#AFB1B6] opacity-0 shadow-tooltip transition-opacity duration-200 peer-hover:opacity-100 lg:static lg:mt-1.5 lg:hidden lg:translate-y-0 lg:bg-transparent lg:p-0">
       {content}
     </span>
     <span className="absolute left-[calc(100%+6px)] top-1/2 h-0 w-0 -translate-y-1/2 border-t-4 border-b-4 border-r-4 border-transparent border-r-gray-2 opacity-0 transition-opacity duration-200 peer-hover:opacity-100 lg:hidden" />
