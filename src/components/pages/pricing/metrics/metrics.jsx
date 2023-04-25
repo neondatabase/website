e9366a644f87eb33cf4171b146f7e1ab456bde7d
Raman Yasel <raman.yasel@gmail.com>
2023-04-25 15:23:16 +0300

    feat: add metrics animation

--- src/components/pages/pricing/metrics/metrics.jsx
+++ src/components/pages/pricing/metrics/metrics.jsx
@@ -1,20 +1,18 @@
 'use client';
 
-import { Transition } from '@headlessui/react';
 import useScrollPosition from '@react-hook/window-scroll';
+import { Alignment, Fit, Layout, useRive, useStateMachineInput } from '@rive-app/react-canvas';
 import clsx from 'clsx';
-import React, { useState, useRef, useLayoutEffect, useMemo, createRef } from 'react';
+import React, { useState, useRef, useMemo, createRef, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import Container from 'components/shared/container';
 import Link from 'components/shared/link';
 import LINKS from 'constants/links';
 import useWindowSize from 'hooks/use-window-size';
 
-const buttons = ['Cloud Journey', 'Training', 'Engineering', 'Training v2'];
-
 const items = [
   {
-    image: '/images/pricing/1.png',
     name: 'Compute time',
     priceFrom: 'From $0.102 /hour',
     details: 'Compute time is the amount of computing capacity used per hour.',
@@ -42,7 +40,6 @@ const items = [
     ],
   },
   {
-    image: '/images/pricing/2.png',
     name: 'Project storage',
     priceFrom: 'From $0.000164 /Gigabyte-hour',
     details: 'Project storage is the amount of data and history in your Neon projects.',
@@ -70,7 +67,6 @@ const items = [
     ],
   },
   {
-    image: '/images/pricing/3.png',
     name: 'Written data',
     priceFrom: 'From $0.096 /Gigabyte',
     details: 'Written data is the amount of data written from Neon compute to storage.',
@@ -98,7 +94,6 @@ const items = [
     ],
   },
   {
-    image: '/images/pricing/4.png',
     name: 'Data transfer',
     priceFrom: 'From $0.09 /Gigabyte',
     details: 'Data transfer is the amount of data transferred out of Neon.',
@@ -130,13 +125,38 @@ const items = [
 const Metrics = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const sectionRef = useRef();
-  const slideRefs = useMemo(() => [...Array(buttons.length)].map(() => createRef()), []);
+  const slideRefs = useMemo(() => [...Array(items.length)].map(() => createRef()), []);
   const { height: pageHeight } = useWindowSize();
   const scrollY = useScrollPosition();
+  const [contentRef, isContentInView] = useInView({ triggerOnce: true });
+  const { RiveComponent, rive } = useRive({
+    src: '/animations/pages/pricing/pricing.riv',
+    autoplay: false,
+    stateMachines: 'SM',
+    layout: new Layout({
+      fit: Fit.FitWidth,
+      alignment: Alignment.Center,
+    }),
+  });
+  const animationStageInput = useStateMachineInput(rive, 'SM', 'Stage (0-3)', 0);
+  const animationReverseInput = useStateMachineInput(rive, 'SM', 'Reverse', false);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+    if (rive && isContentInView) {
+      rive.play();
+    }
+  }, [rive, isContentInView]);
+
+  useEffect(() => {
+    if (!animationStageInput || !animationReverseInput) return;
+
+    animationReverseInput.value = currentSlide < animationStageInput.value;
+    animationStageInput.value = currentSlide;
+  }, [currentSlide, animationStageInput, animationReverseInput]);
+
+  useEffect(() => {
     const currentScrollTop = scrollY;
-    const switchPoints = [...Array(buttons.length + 1)].map(
+    const switchPoints = [...Array(items.length + 1)].map(
       (_, index) => sectionRef.current.offsetTop + pageHeight * index - pageHeight + 700
     );
 
@@ -151,7 +171,7 @@ const Metrics = () => {
     <section className="safe-paddings mt-40 lg:mt-24 md:mt-20" ref={sectionRef}>
       <div className="relative flex flex-col">
         <Container className="flex flex-col items-center" size="mdDoc">
-          <span className="rounded-full bg-[rgba(19,236,182,0.1)] py-[7px] px-[14px] text-[12px] font-semibold uppercase leading-none tracking-[0.02em] text-primary-1">
+          <span className="rounded-full bg-[rgba(19,236,182,0.1)] px-[14px] py-[7px] text-[12px] font-semibold uppercase leading-none tracking-[0.02em] text-primary-1">
             Metrics
           </span>
           <h2 className="mt-3 text-[56px] font-medium leading-none tracking-tighter 2xl:text-[44px] 2xl:leading-snug xl:text-4xl lg:text-center lg:text-[36px] lg:leading-tight">
@@ -172,43 +192,15 @@ const Metrics = () => {
         <Container className="grid h-full w-full grid-cols-12 items-start xs:gap-8" size="sm">
           <div className="relative col-span-7 col-start-1 h-full">
             <div className="sticky top-0 h-screen min-h-[770px] 2xl:min-h-[835px]">
-              {items.map(({ image, name }, index) => (
-                <Transition
-                  className="h-full w-full"
-                  show={currentSlide === index}
-                  enter="transition-opacity duration-1000"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="transition-opacity duration-1000"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                  unmount={false}
-                  key={index}
-                >
-                  <div
-                    className={clsx(
-                      'absolute my-20 flex h-full w-full items-center justify-center',
-                      {
-                        hidden: currentSlide !== index,
-                      }
-                    )}
-                  >
-                    <img
-                      className="h-auto w-full max-w-[710px]"
-                      width={590}
-                      height={830}
-                      src={image}
-                      // inView={itemsInView[index]}
-                      loading="lazy"
-                      alt={`${name  } illustration`}
-                    />
-                  </div>
-                </Transition>
-              ))}
+              <div
+                className={clsx('absolute flex h-full w-full items-center justify-center px-16')}
+              >
+                <RiveComponent width={590} height={830} aria-hidden />
+              </div>
             </div>
           </div>
           <div className="col-span-5 col-start-8 text-left">
-            <div className="space-y-14 lg:space-y-0">
+            <div className="space-y-14 lg:space-y-0" ref={contentRef}>
               {items.map(({ name, priceFrom, details, prices }, index) => (
                 <div
                   className="flex h-screen min-h-[900px] flex-col justify-center px-6 2xl:min-h-[835px] lg:min-h-[770px]"
