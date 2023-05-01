b7e16bbe5462fc027efe59717f0ed3e4aeb926bb
Thi Van Le <vannnyle@gmail.com>
2023-05-01 16:12:24 +0200

    feat: add rss link to release-notes

--- src/components/pages/release-notes/hero/hero.jsx
+++ src/components/pages/release-notes/hero/hero.jsx
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Container from 'components/shared/container';
+import Link from 'components/shared/link/link';
+import { RELEASE_NOTES_BASE_PATH } from 'constants/docs';
+
+import RSSLogo from './images/rss.inline.svg';
 
 const TITLE = 'Release notes';
 const DESCRIPTION = 'The latest product updates from Neon';
@@ -13,7 +17,16 @@ const Hero = ({ className = null, withContainer = false }) => {
     <div className={className}>
       <Tag className={clsx('mb-6 sm:mb-7')} size="sm">
         <h1 className="text-[36px] font-semibold xl:text-3xl">{TITLE}</h1>
-        <p className="mt-3">{DESCRIPTION}</p>
+        <div className="flex items-center justify-between">
+          <p className="mt-3">{DESCRIPTION}</p>
+          <Link
+            className="flex items-center text-secondary-8 transition-colors duration-200 hover:text-secondary-7 dark:text-primary-1 dark:hover:text-primary-2"
+            to={`${RELEASE_NOTES_BASE_PATH}rss.xml`}
+          >
+            <RSSLogo className="mr-1.5" />
+            <span className="leading-none">RSS feed</span>
+          </Link>
+        </div>
       </Tag>
     </div>
   );
