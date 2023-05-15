826950d26def189449bed722aa02b9772f09b9d0
Thi Van Le <vannnyle@gmail.com>
2023-05-15 15:56:05 +0200

    feat: get mdx message

--- src/components/pages/doc/chat-widget/message/message.jsx
+++ src/components/pages/doc/chat-widget/message/message.jsx
@@ -1,31 +1,53 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import { useMemo, useState } from 'react';
+
+import Content from 'components/shared/content/content';
+import serializeMdx from 'utils/serialize-mdx';
 
 import ExampleIcon from '../images/example.inline.svg';
 import UserIcon from '../images/user.inline.svg';
 
-const Message = ({ message }) => (
-  <div
-    className={clsx(
-      'flex items-start px-5 py-2.5',
-      message.role === 'user'
-        ? ' bg-[rgba(36,38,40,0.04)] text-gray-new-50 dark:bg-gray-new-15/40 dark:text-gray-new-60'
-        : 'text-black dark:text-white'
-    )}
-  >
-    <span
+const Message = ({ message }) => {
+  const [mdxSource, setMdxSource] = useState(null);
+
+  useMemo(() => {
+    const fetchMdx = async () => {
+      const serializedContent = await serializeMdx(message.content);
+      setMdxSource(serializedContent);
+    };
+
+    fetchMdx();
+  }, [message.content]);
+
+  return (
+    <div
       className={clsx(
-        'mr-3 flex h-7 w-7 shrink-0 items-center justify-center rounded-full',
+        'flex items-start px-5 py-2.5',
         message.role === 'user'
-          ? 'bg-gray-new-90 text-gray-new-50 dark:bg-gray-new-15 dark:text-gray-new-60'
-          : 'bg-secondary-8/10 text-secondary-8 dark:bg-primary-1/10 dark:text-primary-1'
+          ? ' bg-[rgba(36,38,40,0.04)] text-gray-new-50 dark:bg-gray-new-15/40 dark:text-gray-new-60'
+          : 'text-black dark:text-white'
       )}
     >
-      {message.role === 'user' ? <UserIcon /> : <ExampleIcon />}
-    </span>
-    <div className="pt-0.5">{message.content}</div>
-  </div>
-);
+      <span
+        className={clsx(
+          'mr-3 flex h-7 w-7 shrink-0 items-center justify-center rounded-full',
+          message.role === 'user'
+            ? 'bg-gray-new-90 text-gray-new-50 dark:bg-gray-new-15 dark:text-gray-new-60'
+            : 'bg-secondary-8/10 text-secondary-8 dark:bg-primary-1/10 dark:text-primary-1'
+        )}
+      >
+        {message.role === 'user' ? <UserIcon /> : <ExampleIcon />}
+      </span>
+      {mdxSource && (
+        <Content
+          className="prose-chat prose pt-0.5 dark:prose-invert xs:prose-code:break-words"
+          content={mdxSource}
+        />
+      )}
+    </div>
+  );
+};
 
 Message.propTypes = {
   message: PropTypes.shape({
