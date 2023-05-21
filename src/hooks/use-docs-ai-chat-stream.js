237ec5bc4469ecbdd1706df377a4e0fdddb249d0
Kirill Bolotskii <kirill.bolotsky@gmail.com>
2023-05-22 00:57:11 +0400

    fix: refactor some more to fix the prod issue

--- src/hooks/use-docs-ai-chat-stream.js
+++ src/hooks/use-docs-ai-chat-stream.js
@@ -1,41 +1,42 @@
 import { useCallback, useState, useEffect } from 'react';
-import { flushSync } from 'react-dom';
+// import { flushSync } from 'react-dom';
 
 const useDocsAIChatStream = ({ isMounted, signal }) => {
   const [messages, setMessages] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const response = await fetch('/api/open-ai', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          message: messages[messages.length - 1],
-        }),
-        signal,
-      });
+  const fetchData = useCallback(
+    async (currentMessages) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('/api/open-ai', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            message: currentMessages[currentMessages.length - 1],
+          }),
+          signal,
+        });
 
-      if (response.ok) {
-        const reader = response.body.getReader();
-        while (isMounted) {
-          const { done, value } = await reader.read();
-          if (done) break;
+        if (response.ok) {
+          const reader = response.body.getReader();
+          while (isMounted) {
+            const { done, value } = await reader.read();
+            if (done) break;
 
-          // Process the received chunk value
-          const chunk = new TextDecoder().decode(value);
-          let parsedChunk;
-          try {
-            parsedChunk = JSON.parse(chunk);
-          } catch (e) {
-            parsedChunk = chunk;
-          }
-          // Update the messages state with the received data
-          flushSync(() => {
+            // Process the received chunk value
+            const chunk = new TextDecoder().decode(value);
+            let parsedChunk;
+            try {
+              parsedChunk = JSON.parse(chunk);
+            } catch (e) {
+              parsedChunk = chunk;
+            }
+            // Update the messages state with the received data
+            // flushSync(() => {
             setMessages((prevMessages) => {
               // this prevents leak if user has
               // bailed out early
@@ -58,24 +59,26 @@ const useDocsAIChatStream = ({ isMounted, signal }) => {
                 },
               ];
             });
-          });
+            // });
+          }
+        } else {
+          throw Error('Something went wrong. Please try again!');
         }
-      } else {
-        throw Error('Something went wrong. Please try again!');
+      } catch (error) {
+        console.error(error);
+        if (error.name === 'AbortError') return;
+        setError(error?.message || error || 'Something went wrong. Please try again!');
+      } finally {
+        setIsLoading(false);
       }
-    } catch (error) {
-      console.error(error);
-      if (error.name === 'AbortError') return;
-      setError(error?.message || error || 'Something went wrong. Please try again!');
-    } finally {
-      setIsLoading(false);
-    }
-  }, [isMounted, signal, messages]);
+    },
+    [isMounted, signal]
+  );
 
   useEffect(() => {
     if (messages.length && messages[messages.length - 1].role === 'user') {
       setMessages((prevMessages) => [...prevMessages, { role: 'assistant', content: '' }]);
-      fetchData();
+      fetchData(messages);
     }
   }, [messages, fetchData]);
 
