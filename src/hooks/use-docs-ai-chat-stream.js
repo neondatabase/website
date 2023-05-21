3d7cde52efce817cd45b6cb23bfdaabd8bedc4cb
Kirill Bolotskii <kirill.bolotsky@gmail.com>
2023-05-21 23:58:57 +0400

    fix: made stream-like answer functioning in production build

--- src/hooks/use-docs-ai-chat-stream.js
+++ src/hooks/use-docs-ai-chat-stream.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { flushSync } from 'react-dom';
 
 const useDocsAIChatStream = ({ isMounted, signal }) => {
@@ -6,78 +6,78 @@ const useDocsAIChatStream = ({ isMounted, signal }) => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch('/api/open-ai', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            message: messages[messages.length - 1],
-          }),
-          signal,
-        });
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('/api/open-ai', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: messages[messages.length - 1],
+        }),
+        signal,
+      });
 
-        if (response.ok) {
-          const reader = response.body.getReader();
-          while (isMounted) {
-            const { done, value } = await reader.read();
-            if (done) break;
+      if (response.ok) {
+        const reader = response.body.getReader();
+        while (isMounted) {
+          const { done, value } = await reader.read();
+          if (done) break;
 
-            // Process the received chunk value
-            const chunk = new TextDecoder().decode(value);
-            let parsedChunk;
-            try {
-              parsedChunk = JSON.parse(chunk);
-            } catch (e) {
-              parsedChunk = chunk;
-            }
-            // Update the messages state with the received data
-            flushSync(() => {
-              setMessages((prevMessages) => {
-                // this prevents leak if user has
-                // bailed out early
-                if (!prevMessages.length) return prevMessages;
-                const { role, content } = prevMessages[prevMessages.length - 1];
-                if (role === 'assistant') {
-                  return [
-                    ...prevMessages.slice(0, -1),
-                    {
-                      role: 'assistant',
-                      content: content.concat(parsedChunk),
-                    },
-                  ];
-                }
+          // Process the received chunk value
+          const chunk = new TextDecoder().decode(value);
+          let parsedChunk;
+          try {
+            parsedChunk = JSON.parse(chunk);
+          } catch (e) {
+            parsedChunk = chunk;
+          }
+          // Update the messages state with the received data
+          flushSync(() => {
+            setMessages((prevMessages) => {
+              // this prevents leak if user has
+              // bailed out early
+              if (!prevMessages.length) return prevMessages;
+              const { role, content } = prevMessages[prevMessages.length - 1];
+              if (role === 'assistant') {
                 return [
-                  ...prevMessages,
+                  ...prevMessages.slice(0, -1),
                   {
                     role: 'assistant',
-                    content: parsedChunk,
+                    content: content.concat(parsedChunk),
                   },
                 ];
-              });
+              }
+              return [
+                ...prevMessages,
+                {
+                  role: 'assistant',
+                  content: parsedChunk,
+                },
+              ];
             });
-          }
-        } else {
-          throw Error('Something went wrong. Please try again!');
+          });
         }
-      } catch (error) {
-        console.error(error);
-        if (error.name === 'AbortError') return;
-        setError(error?.message || error || 'Something went wrong. Please try again!');
-      } finally {
-        setIsLoading(false);
+      } else {
+        throw Error('Something went wrong. Please try again!');
       }
-    };
+    } catch (error) {
+      console.error(error);
+      if (error.name === 'AbortError') return;
+      setError(error?.message || error || 'Something went wrong. Please try again!');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [isMounted, signal, messages]);
 
+  useEffect(() => {
     if (messages.length && messages[messages.length - 1].role === 'user') {
       setMessages((prevMessages) => [...prevMessages, { role: 'assistant', content: '' }]);
       fetchData();
     }
-  }, [messages, isMounted, signal]);
+  }, [messages, fetchData]);
 
   return { messages, setMessages, error, setError, isLoading };
 };
