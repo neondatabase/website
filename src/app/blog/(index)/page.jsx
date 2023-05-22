5c809eb08c606111a9f10a0babc7c8f8e897ef84
Thi Van Le <vannnyle@gmail.com>
2023-05-22 16:07:54 +0200

    feat: release notes

--- src/app/blog/(index)/page.jsx
+++ src/app/blog/(index)/page.jsx
@@ -1,15 +1,20 @@
 import FeaturedPostsList from 'components/pages/blog/featured-posts-list';
 import PostsList from 'components/pages/blog/posts-list';
+import ReleaseNotesList from 'components/pages/blog/release-notes-list';
+import { getAllReleaseNotes } from 'utils/api-docs';
 import { getAllWpPosts } from 'utils/api-posts';
 
 export default async function BlogPage() {
   const nodes = await getAllWpPosts();
+  const releaseNotes = await getAllReleaseNotes();
   const featuredPosts = nodes.slice(0, 5);
+  const featuredReleaseNotes = releaseNotes.slice(0, 4);
 
   return (
     <div className="col-span-8 col-start-3 -mx-[30px] grid gap-y-20">
       <FeaturedPostsList posts={featuredPosts} />
       <PostsList title="Community" posts={nodes} alignment="right" />
+      <ReleaseNotesList items={featuredReleaseNotes} />
       <PostsList title="Company" posts={nodes} alignment="left" />
     </div>
   );
---
+++ src/components/pages/blog/release-notes-list/release-notes-list.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+
+import Link from 'components/shared/link/link';
+import LINKS from 'constants/links';
+import getExcerpt from 'utils/get-excerpt';
+import getReleaseNotesCategoryFromSlug from 'utils/get-release-notes-category-from-slug';
+import getReleaseNotesDateFromSlug from 'utils/get-release-notes-date-from-slug';
+
+const ReleaseNotesList = ({ items }) => (
+  <section className="rounded-xl bg-black-new px-10 pt-7 pb-10">
+    <div className="flex items-center justify-between">
+      <h2 className="text-2xl leading-none tracking-tighter">Release notes</h2>
+      <Link
+        className="flex items-center leading-none"
+        theme="blue"
+        to={LINKS.releaseNotes}
+        withArrow
+      >
+        All release notes
+      </Link>
+    </div>
+    <ul className="mt-6 grid grid-cols-4 gap-x-[50px] border-t border-gray-new-20/60 py-8">
+      {items.map(({ slug, content }, index) => {
+        const { capitalisedCategory: category } = getReleaseNotesCategoryFromSlug(slug);
+        const { datetime, label } = getReleaseNotesDateFromSlug(slug);
+        const title = getExcerpt(content, 50);
+        return (
+          <li className="group" key={index}>
+            <Link className="flex flex-col" to={`${LINKS.releaseNotes}/${slug}`}>
+              <span className="text-xs font-medium uppercase leading-none tracking-wider text-blue-80">
+                {category}
+              </span>
+              <div className="relative mt-4 after:absolute after:top-1/2 after:left-0 after:h-px after:w-[calc(100%+50px)] after:-translate-y-1/2 after:bg-blue-80 group-last:after:w-full">
+                <span className="relative z-10 flex h-2.5 w-2.5 rounded-full border-[1.4px] border-blue-80 bg-black-new" />
+              </div>
+              <h3 className="mt-4 text-lg font-medium leading-tight tracking-[-0.02em]">{title}</h3>
+              <time
+                className="mt-2 text-sm leading-none tracking-[-0.02em] text-gray-new-80"
+                dateTime={datetime}
+              >
+                {label}
+              </time>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+);
+
+ReleaseNotesList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string,
+      content: PropTypes.string,
+    })
+  ),
+};
+
+export default ReleaseNotesList;
