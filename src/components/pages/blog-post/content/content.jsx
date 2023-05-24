29793dce837ec6d2c85ef72b84e6d2832fbc59b0
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-24 17:05:20 +0800

    chore: remove large cover from blog post page

--- src/components/pages/blog-post/content/content.jsx
+++ src/components/pages/blog-post/content/content.jsx
@@ -1,40 +1,22 @@
-import Image from 'next/image';
 import PropTypes from 'prop-types';
 
 import CTA from '../cta';
 
-const Content = ({ html, title, cover, className = null }) => (
+const Content = ({ html, className = null }) => (
   <div className={className}>
-    {cover && (
-      <Image
-        className="mb-10 rounded-md md:mb-8"
-        src={cover?.mediaItemUrl}
-        width={716}
-        height={375}
-        alt={cover?.altText || title}
-        priority
-      />
-    )}
-    <div className="prose-blog prose prose-lg">
-      {html}
-      {/* TODO: remove this CTA */}
-      <CTA
-        title="Want to improve your workflow?"
-        description="Create a database branch using Vercel"
-        buttonText="Sign up"
-        buttonUrl="/signup"
-      />
-    </div>
+    {html}
+    {/* TODO: remove this CTA */}
+    <CTA
+      title="Want to improve your workflow?"
+      description="Create a database branch using Vercel"
+      buttonText="Sign up"
+      buttonUrl="/signup"
+    />
   </div>
 );
 
 Content.propTypes = {
   html: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
-  title: PropTypes.string.isRequired,
-  cover: PropTypes.shape({
-    mediaItemUrl: PropTypes.string.isRequired,
-    altText: PropTypes.string,
-  }),
   className: PropTypes.string,
 };
 
