3c790fa162ae7eb1773854755790b1fe2e078e59
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-24 16:53:37 +0800

    chore: update tag colors

--- src/constants/blog.js
+++ src/constants/blog.js
@@ -3,14 +3,14 @@ const BLOG_CATEGORY_BASE_PATH = `${BLOG_BASE_PATH}category/`;
 const BLOG_POSTS_PER_PAGE = 50;
 const CATEGORY_COLORS = {
   company: 'text-green-45',
-  engineering: 'text-yellow-70',
-  community: 'text-brown-70',
+  engineering: 'text-purple-70',
+  community: 'text-yellow-70',
 };
 
 const CATEGORY_BG_COLORS = {
   company: 'bg-green-45/10',
-  engineering: 'bg-yellow-70/10',
-  community: 'bg-brown-70/10',
+  engineering: 'bg-purple-70/10',
+  community: 'bg-yellow-70/10',
 };
 
 export {
