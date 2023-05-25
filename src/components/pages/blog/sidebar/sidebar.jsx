f2c544d4ae24ee4c2abbe23764a67fb4538c16c6
Thi Van Le <vannnyle@gmail.com>
2023-05-25 11:25:08 +0200

    fix: sticky sidebar

--- src/components/pages/blog/sidebar/sidebar.jsx
+++ src/components/pages/blog/sidebar/sidebar.jsx
@@ -49,35 +49,37 @@ const socialLinks = [
 ];
 
 const Sidebar = () => (
-  <aside className="col-span-2 flex flex-col lt:col-span-full">
-    <nav className="no-scrollbars sticky top-16 mb-10 md:-mx-4 md:max-w-5xl md:overflow-auto md:px-4">
-      <ul className=" flex flex-col lt:flex-row lt:gap-x-7 lt:py-3.5 md:after:shrink-0 md:after:grow-0 md:after:basis-px md:after:content-['']">
-        {categories.map(({ name, slug }, index) => (
-          <li className="py-1.5 first:pt-0 last:pb-0 lt:py-0" key={index}>
-            <BlogNavLink name={name} slug={slug} />
-          </li>
-        ))}
-      </ul>
-    </nav>
-    <div className="sticky bottom-10 mt-auto lt:hidden">
-      <h2 className="text-xs font-semibold uppercase leading-none tracking-[0.02em] text-gray-new-70">
-        Follow us
-      </h2>
-      <ul className="mt-4 flex flex-wrap gap-4">
-        {socialLinks.map(({ name, url, icon: Icon }, index) => (
-          <li className="flex items-center" key={index}>
-            <a
-              className="group flex items-center justify-center rounded-full"
-              aria-label={name}
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Icon className="h-4 w-4 text-gray-new-70 transition-colors duration-200 group-hover:text-green-45" />
-            </a>
-          </li>
-        ))}
-      </ul>
+  <aside className="col-span-2 lt:col-span-full">
+    <div className="sticky top-10 flex h-[90vh] min-h-[300px] flex-col gap-y-10">
+      <nav className="no-scrollbars md:-mx-4 md:max-w-5xl md:overflow-auto md:px-4">
+        <ul className=" flex flex-col lt:flex-row lt:gap-x-7 lt:py-3.5 md:after:shrink-0 md:after:grow-0 md:after:basis-px md:after:content-['']">
+          {categories.map(({ name, slug }, index) => (
+            <li className="py-1.5 first:pt-0 last:pb-0 lt:py-0" key={index}>
+              <BlogNavLink name={name} slug={slug} />
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="mt-auto lt:hidden">
+        <h2 className="text-xs font-semibold uppercase leading-none tracking-[0.02em] text-gray-new-70">
+          Follow us
+        </h2>
+        <ul className="mt-4 flex flex-wrap gap-4">
+          {socialLinks.map(({ name, url, icon: Icon }, index) => (
+            <li className="flex items-center" key={index}>
+              <a
+                className="group flex items-center justify-center rounded-full"
+                aria-label={name}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon className="h-4 w-4 text-gray-new-70 transition-colors duration-200 group-hover:text-green-45" />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   </aside>
 );
