c9431dd44c454bed60974eb223125443b4c2365f
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-25 19:30:09 +0800

    Revert "chore: add fill for images"
    This reverts commit b4f4366594e296e19c15123afb4c1fcff3e9d2a6.

--- src/components/pages/blog/blog-post-card/blog-post-card.jsx
+++ src/components/pages/blog/blog-post-card/blog-post-card.jsx
@@ -48,14 +48,14 @@ const BlogPostCard = ({
           rel={url ? 'noopener noreferrer' : undefined}
         >
           {largeCover?.mediaItemUrl ? (
-            <div className="relative aspect-video overflow-hidden rounded-md">
-              <Image
-                className="absolute left-0 top-0 min-h-full min-w-full object-cover"
-                src={largeCover?.mediaItemUrl}
-                alt={largeCover?.altText || title}
-                fill
-              />
-            </div>
+            <Image
+              className="w-full rounded-md"
+              src={largeCover?.mediaItemUrl}
+              alt={largeCover?.altText || title}
+              width={716}
+              height={403}
+              quality="95"
+            />
           ) : (
             <Image
               className="w-full rounded-md"
