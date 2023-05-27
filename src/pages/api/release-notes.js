6bfb4e8ab3046e719d064f0f70842c961f077930
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-05-27 13:36:49 +0300

    test: debugging the route api

--- src/pages/api/release-notes.js
+++ src/pages/api/release-notes.js
@@ -1,27 +1,46 @@
-import { getPostBySlug, getPostSlugs } from 'utils/api-docs';
+import fs from 'fs/promises';
+import path from 'path';
+
+import matter from 'gray-matter';
+
+import { getPostSlugs } from 'utils/api-docs';
 
 const RELEASE_NOTES_DIR_PATH = `content/release-notes`;
 
+const getPostBySlug = async (slug, pathname) => {
+  try {
+    const pathDirectory = path.join(process.cwd(), `${pathname}/${slug}.md`);
+    const source = await fs.readFile(pathDirectory, 'utf8');
+    const { data, content } = matter(source);
+    return { data, content };
+  } catch (e) {
+    console.error(`Error reading file ${pathname}/${slug}.md: `, e);
+    return null;
+  }
+};
+
 export default async function handler(req, res) {
   const slugs = await getPostSlugs(RELEASE_NOTES_DIR_PATH);
+  console.log(`Got slugs: `, slugs);
 
-  const releaseNotes = slugs
-    .reverse()
-    .map((slug) => {
-      try {
-        const post = getPostBySlug(slug, RELEASE_NOTES_DIR_PATH);
-        const { data, content } = post;
-        return { slug: slug.replace('/', ''), isDraft: data?.isDraft, content };
-      } catch (error) {
-        console.error(error);
-        return null;
-      }
+  const releaseNotes = await Promise.all(
+    slugs.reverse().map(async (slug) => {
+      const post = await getPostBySlug(slug, RELEASE_NOTES_DIR_PATH);
+      if (!post) return null;
+      const { data, content } = post;
+      return { slug: slug.replace(/\//g, ''), isDraft: data?.isDraft, content };
     })
-    .filter((item) => item && (process.env.NODE_ENV !== 'production' || !item.isDraft));
+  );
 
-  if (releaseNotes.includes(null)) {
+  const filteredReleaseNotes = releaseNotes.filter(
+    (item) => item && (process.env.NODE_ENV !== 'production' || !item.isDraft)
+  );
+
+  console.log(`Got release notes: `, filteredReleaseNotes);
+
+  if (filteredReleaseNotes.includes(null)) {
     return res.status(500).json({ message: 'An error occurred while fetching release notes' });
   }
 
-  res.status(200).json(releaseNotes);
+  res.status(200).json(filteredReleaseNotes);
 }
