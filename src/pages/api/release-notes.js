f69ec8be2a2b3afa0dcfa275ddb1a73f6d3e4975
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-05-27 13:21:05 +0300

    chore: add small adjustments

--- src/pages/api/release-notes.js
+++ src/pages/api/release-notes.js
@@ -1,31 +1,27 @@
-import { RELEASE_NOTES_DIR_PATH } from 'constants/docs';
 import { getPostBySlug, getPostSlugs } from 'utils/api-docs';
 
-export default async function handler(req, res) {
-  try {
-    const slugs = (await getPostSlugs(RELEASE_NOTES_DIR_PATH)).map((slug) => slug.replace('/', ''));
+const RELEASE_NOTES_DIR_PATH = `content/release-notes`;
 
-    const releaseNotesPromises = slugs.reverse().map(async (slug) => {
+export default async function handler(req, res) {
+  const slugs = await getPostSlugs(RELEASE_NOTES_DIR_PATH);
+
+  const releaseNotes = slugs
+    .reverse()
+    .map((slug) => {
       try {
-        const post = await getPostBySlug(slug, RELEASE_NOTES_DIR_PATH);
+        const post = getPostBySlug(slug, RELEASE_NOTES_DIR_PATH);
         const { data, content } = post;
-        if (process.env.NODE_ENV === 'production') {
-          if (data?.isDraft) {
-            return null;
-          }
-        }
-        return { slug, isDraft: data?.isDraft, content };
+        return { slug: slug.replace('/', ''), isDraft: data?.isDraft, content };
       } catch (error) {
-        console.error(`Error fetching post by slug: ${slug}`, error);
+        console.error(error);
         return null;
       }
-    });
+    })
+    .filter((item) => item && (process.env.NODE_ENV !== 'production' || !item.isDraft));
 
-    const releaseNotes = (await Promise.all(releaseNotesPromises)).filter((item) => item !== null);
-
-    res.status(200).json(releaseNotes);
-  } catch (error) {
-    console.error('Error in API handler: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  if (releaseNotes.includes(null)) {
+    return res.status(500).json({ message: 'An error occurred while fetching release notes' });
   }
+
+  res.status(200).json(releaseNotes);
 }
