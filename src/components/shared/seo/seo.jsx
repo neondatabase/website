f987041f2c777ec1ef39fb112d63caef3c93aa5b
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-05-29 13:16:54 +0800

    feat: add og:url property

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -39,7 +39,7 @@ const SEO = ({
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={currentImagePath} />
-      <meta property="og:url" content={currentImagePath} />
+      <meta property="og:url" content={canonicalUrl} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
