617de5257c0a1584eb9e7a6d71e76b4c06de9c2a
Thi Van Le <vannnyle@gmail.com>
2023-05-30 16:24:52 +0200

    feat: preload riv file

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,13 @@ import SEO_DATA from 'constants/seo-data';
 
 const NotFoundPage = () => (
   <>
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+    />
     <title>{SEO_DATA[404].title}</title>
+
     <Layout headerTheme="white" footerWithTopBorder>
       <Hero />
     </Layout>
--- src/app/pricing/preload-resources.jsx
+++ src/app/pricing/preload-resources.jsx
@@ -1,11 +1,19 @@
 'use client';
 
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-export function PreloadResources() {
-  ReactDOM.preload('/animations/pages/pricing/pricing.riv', {
-    as: 'fetch',
-    crossorigin: 'anonymous',
-    media: '(min-width: 1024px)',
-  });
-}
+export const PreloadResources = () => {
+  const head = ReactDOM.createRoot(document.head);
+
+  // TODO: use preload instead of render once it's supported by React https://github.com/facebook/react/pull/26237
+  head.render(
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/animations/pages/pricing/pricing.riv"
+      as="fetch"
+      media="(min-width: 1024px)"
+    />
+  );
+  return null;
+};
