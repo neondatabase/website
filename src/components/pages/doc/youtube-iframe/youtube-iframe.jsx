a99c0343fcd39dbf7e125f79f68bdc784926e321
Thi Van Le <vannnyle@gmail.com>
2023-05-31 17:33:08 +0200

    fix: return with & height

--- src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
+++ src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
@@ -4,6 +4,8 @@ const YoutubeIframe = ({ embedId }) => (
   <figure className="not-prose relative my-8 h-0 overflow-hidden pb-[56.25%]">
     <iframe
       className="absolute left-0 top-0 !my-0 h-full w-full"
+      width="796"
+      height="447"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       title="Embedded YouTube"
