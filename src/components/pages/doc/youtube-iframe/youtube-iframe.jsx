93019199e755fa71fc3a5662bff126d750ebb098
Thi Van Le <vannnyle@gmail.com>
2023-05-31 17:20:08 +0200

    fix: minor fix

--- src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
+++ src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 const YoutubeIframe = ({ embedId }) => (
   <figure className="not-prose relative my-8 h-0 overflow-hidden pb-[56.25%]">
     <iframe
-      className="absolute left-0 top-0 !m-0 h-full w-full"
-      width="796"
-      height="447"
+      className="absolute left-0 top-0 !my-0 h-full w-full"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       title="Embedded YouTube"
