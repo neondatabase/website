a58d093c93995479721c5ddf5869505f73aa2f4f
Thi Van Le <vannnyle@gmail.com>
2023-05-31 17:09:42 +0200

    fix: remove redundant margin

--- src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
+++ src/components/pages/doc/youtube-iframe/youtube-iframe.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 const YoutubeIframe = ({ embedId }) => (
-  <figure className="relative h-0 overflow-hidden pb-[56.25%]">
+  <figure className="not-prose relative my-8 h-0 overflow-hidden pb-[56.25%]">
     <iframe
-      className="absolute left-0 top-0 h-full w-full"
-      width="853"
-      height="480"
+      className="absolute left-0 top-0 !m-0 h-full w-full"
+      width="796"
+      height="447"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       title="Embedded YouTube"
