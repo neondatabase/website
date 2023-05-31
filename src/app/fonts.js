3139775613e98958dee6d8710aa4424fa220676a
Thi Van Le <vannnyle@gmail.com>
2023-05-31 11:30:59 +0200

    fix: set font preload false

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -7,7 +7,6 @@ export const ibm_plex_sans = localFont({
       weight: '300',
       style: 'normal',
       display: 'swap',
-      preload: false,
     },
     {
       path: './fonts/ibm-plex-sans/ibm-plex-sans-regular.woff2',
@@ -20,14 +19,12 @@ export const ibm_plex_sans = localFont({
       weight: '500',
       style: 'normal',
       display: 'swap',
-      preload: false,
     },
     {
       path: './fonts/ibm-plex-sans/ibm-plex-sans-semibold.woff2',
       weight: '600',
       style: 'normal',
       display: 'swap',
-      preload: false,
     },
     {
       path: './fonts/ibm-plex-sans/ibm-plex-sans-bold.woff2',
@@ -46,15 +43,14 @@ export const ibm_plex_mono = localFont({
       weight: '400',
       style: 'normal',
       display: 'swap',
-      preload: false,
     },
     {
       path: './fonts/ibm-plex-mono/ibm-plex-mono-bold.woff2',
       weight: '700',
       style: 'normal',
       display: 'swap',
-      preload: false,
     },
   ],
+  preload: false,
   variable: '--font-ibm-plex-mono',
 });
