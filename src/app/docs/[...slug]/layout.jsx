8628358f655b0daed3f64e7c115c203c29cbd8f0
Thi Van Le <vannnyle@gmail.com>
2023-06-05 16:00:03 +0200

    feat: add new color

--- src/app/docs/[...slug]/layout.jsx
+++ src/app/docs/[...slug]/layout.jsx
@@ -24,7 +24,7 @@ const DocsLayout = async ({ children, params }) => {
           size="mdDoc"
         >
           <Sidebar
-            className="relative col-start-1 col-end-4 max-w-[254px] pb-20 pt-[111px] before:absolute before:-right-5 before:top-0 before:z-10 before:h-full before:w-screen before:bg-gray-new-98 dark:before:bg-gray-new-15/20 lg:hidden"
+            className="relative col-start-1 col-end-4 max-w-[254px] pb-20 pt-[111px] before:absolute before:-right-5 before:top-0 before:z-10 before:h-full before:w-screen before:bg-gray-new-98 dark:before:bg-gray-new-10 lg:hidden"
             sidebar={sidebar}
             currentSlug={currentSlug}
           />
