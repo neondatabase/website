931bb217246b99fb02bf609a4a691780fbad496a
Sasha Sushko <sasha@sushko.me>
2023-06-07 14:30:27 +0300

    fix: trivial: make banner's link relative

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -14,7 +14,7 @@ const TopBar = () => {
   return isPricingPage ? null : (
     <Link
       className="safe-paddings relative z-40 flex h-11 w-full items-center justify-center bg-primary-1 px-4 py-3 leading-none transition-colors duration-200 hover:bg-[#1AFFB2] xs:h-auto"
-      to="https://neon.tech/blog/postgres-autoscaling"
+      to="/blog/postgres-autoscaling"
     >
       <span className="mr-4 truncate border-r border-black border-opacity-20 py-1 pr-4 text-sm font-semibold">
         Announcing autoscaling in feature-preview!
