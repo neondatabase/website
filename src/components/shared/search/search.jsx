85ccde78127ec47d7ab835b7a5f0334f3c4253d7
Thi Van Le <vannnyle@gmail.com>
2023-06-12 12:30:14 +0200

    feat: style search input

--- src/components/shared/search/search.jsx
+++ src/components/shared/search/search.jsx
@@ -9,7 +9,7 @@ const Search = ({ className = null }) => {
   const ref = useRef(null);
 
   return (
-    <div className={clsx('relative', className)} ref={ref}>
+    <div className={clsx('relative flex items-center justify-between', className)} ref={ref}>
       <DocSearch
         appId="RAPNCKAFQF"
         apiKey="a975b8d2e7c08607b212bf690f8eb40a"
