e8a3128647364be863711aec651fdff03444fe74
Thi Van Le <vannnyle@gmail.com>
2023-06-13 12:02:03 +0200

    fix: command keys

--- src/components/shared/search/search.jsx
+++ src/components/shared/search/search.jsx
@@ -5,15 +5,19 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { useRef } from 'react';
 
+const INDEX_NAME = 'neon';
+const API_KEY = 'a975b8d2e7c08607b212bf690f8eb40a';
+const APP_ID = 'RAPNCKAFQF';
+
 const Search = ({ className = null }) => {
   const ref = useRef(null);
 
   return (
     <div className={clsx('relative flex items-center justify-between', className)} ref={ref}>
       <DocSearch
-        appId="RAPNCKAFQF"
-        apiKey="a975b8d2e7c08607b212bf690f8eb40a"
-        indexName="neon"
+        appId={APP_ID}
+        apiKey={API_KEY}
+        indexName={INDEX_NAME}
         placeholder="Search..."
         hitComponent={({ hit, children }) => (
           <a
