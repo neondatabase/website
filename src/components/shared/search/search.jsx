e232709537c5c6f8590d53446c2e837220a61cae
Thi Van Le <vannnyle@gmail.com>
2023-06-13 18:02:02 +0200

    fix: customize search result tree

--- src/components/shared/search/search.jsx
+++ src/components/shared/search/search.jsx
@@ -5,6 +5,8 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { useRef } from 'react';
 
+import Link from '../link/link';
+
 const INDEX_NAME = 'neon';
 const API_KEY = 'a975b8d2e7c08607b212bf690f8eb40a';
 const APP_ID = 'RAPNCKAFQF';
@@ -20,15 +22,48 @@ const Search = ({ className = null }) => {
         indexName={INDEX_NAME}
         placeholder="Search..."
         hitComponent={({ hit, children }) => (
-          <a
+          <Link
             className={clsx({
-              'DocSearch-Hit--Result': hit._snippetResult,
+              'DocSearch-Hit--Result': hit.__is_result?.(),
+              'DocSearch-Hit--Parent': hit.__is_parent?.(),
+              'DocSearch-Hit--FirstChild': hit.__is_first?.(),
+              'DocSearch-Hit--LastChild': hit.__is_last?.(),
+              'DocSearch-Hit--Child': hit.__is_child?.(),
             })}
-            href={hit.url}
+            to={hit.url}
           >
+            {console.log(hit)}
             {children}
-          </a>
+          </Link>
         )}
+        transformItems={(items) =>
+          items.map((item, index) => {
+            // We transform the absolute URL into a relative URL to
+            // leverage Next's preloading.
+            const a = document.createElement('a');
+            a.href = item.url;
+
+            if (item.hierarchy?.lvl0) {
+              item.hierarchy.lvl0 = item.hierarchy.lvl0.replace(/&amp;/g, '&');
+            }
+
+            if (item._highlightResult?.hierarchy?.lvl0?.value) {
+              item._highlightResult.hierarchy.lvl0.value =
+                item._highlightResult.hierarchy.lvl0.value.replace(/&amp;/g, '&');
+            }
+
+            return {
+              ...item,
+              url: `${a.pathname}${a.hash}`,
+              __is_result: () => true,
+              __is_parent: () => item.type === 'lvl1' && items.length > 1 && index === 0,
+              __is_child: () =>
+                item.type !== 'lvl1' && items.length > 1 && items[0].type === 'lvl1' && index !== 0,
+              __is_first: () => index === 1,
+              __is_last: () => index === items.length - 1 && index !== 0,
+            };
+          })
+        }
         insights
       />
     </div>
