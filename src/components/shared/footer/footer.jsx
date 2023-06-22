bb1efa231874c48c855debe87725353e5f3ac7b7
Thi Van Le <vannnyle@gmail.com>
2023-06-22 18:03:39 +0200

    refactor: remove importing react

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import Container from 'components/shared/container';
