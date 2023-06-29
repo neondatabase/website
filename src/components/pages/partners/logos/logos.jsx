c074b36a0003913fd6ec984a2f52ee7fee0a1077
Sasha Sushko <sasha@sushko.me>
2023-06-29 13:45:41 +0300

    feat: partners logos glow

--- src/components/pages/partners/logos/logos.jsx
+++ src/components/pages/partners/logos/logos.jsx
@@ -1,80 +1,9 @@
 import React from 'react';
 
-import airplaneLogo from './images/airplane.svg';
-import bunnyshellLogo from './images/bunnyshell.svg';
-import cloudflareLogo from './images/cloudflare.svg';
-import fabricIoLogo from './images/fabric.svg';
-import hasuraLogo from './images/hasura.svg';
-import illaLogo from './images/illa.svg';
-import octolisLogo from './images/octolis.svg';
-import replitLogo from './images/replit.svg';
-import snapletLogo from './images/snaplet.svg';
-import vercelLogo from './images/vercel.svg';
-import wundergraphLogo from './images/wundergraph.svg';
-
-const logos = [
-  {
-    src: bunnyshellLogo,
-    alt: 'Bunnyshell',
-  },
-  {
-    src: vercelLogo,
-    alt: 'Vercel',
-  },
-  {
-    src: replitLogo,
-    alt: 'Replit',
-  },
-  {
-    src: hasuraLogo,
-    alt: 'Hasura',
-  },
-  {
-    src: illaLogo,
-    alt: 'Illa',
-  },
-  {
-    src: octolisLogo,
-    alt: 'Octolis',
-  },
-  {
-    src: cloudflareLogo,
-    alt: 'Cloudflare',
-  },
-  {
-    src: airplaneLogo,
-    alt: 'Airplane',
-  },
-  {
-    src: wundergraphLogo,
-    alt: 'Wundergraph',
-  },
-  {
-    src: fabricIoLogo,
-    alt: 'TheFabric.IO consulting',
-  },
-  {
-    src: snapletLogo,
-    alt: 'Snaplet',
-  },
-];
-
 const Logos = () => (
-  <div className="logos relative flex overflow-x-hidden">
-    <ul className="animate-logos whitespace-nowrap">
-      {logos.map(({ src, alt }, index) => (
-        <li key={`logo_${index}`} className="inline-block">
-          <img className="mx-11 h-12 max-w-none" src={src} alt={alt} />
-        </li>
-      ))}
-    </ul>
-    <ul className="absolute top-0 animate-logos-hidden whitespace-nowrap">
-      {logos.map(({ src, alt }, index) => (
-        <li key={index} className="inline-block">
-          <img className="mx-11 h-12 max-w-none" src={src} alt={alt} />
-        </li>
-      ))}
-    </ul>
+  <div className="logos">
+    <div className="logos-front" />
+    <div className="logos-back" />
   </div>
 );
 export default Logos;
--- tailwind.config.js
+++ tailwind.config.js
@@ -214,14 +214,6 @@ module.exports = {
             transform: 'translateY(-2%) scale(.96)',
           },
         },
-        logos: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        'logos-hidden': {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
       }),
       animation: {
         'text-blink': 'text-blink 0.4s cubic-bezier(0.4, 0, 0.2, 1)',
@@ -230,8 +222,6 @@ module.exports = {
         'dialog-show': 'dialogShow 0.3s cubic-bezier(.16,1,.3,1)',
         'dialog-hide': 'dialogHide 0.3s cubic-bezier(.16,1,.3,1)',
         loading: 'loading 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        logos: 'logos 35s linear infinite',
-        'logos-hidden': 'logos-hidden 35s linear infinite',
       },
       typography: () => ({
         DEFAULT: {
