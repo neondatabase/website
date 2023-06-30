101690ea6f627cbaf6f75653333e700a66ae035c
Thi Van Le <vannnyle@gmail.com>
2023-06-30 19:23:36 +0200

    fix: adjust styles for hero, benefits

--- src/components/pages/partners/hero/hero.jsx
+++ src/components/pages/partners/hero/hero.jsx
@@ -6,9 +6,9 @@ import Container from 'components/shared/container/container';
 import logoPattern from './images/logo-pattern.jpg';
 
 const Hero = () => (
-  <section className="hero safe-paddings relative overflow-hidden pb-[198px] pt-[344px] xl:pb-[166px] xl:pt-[328px] lg:pb-[134px] lg:pt-[264px] md:pb-[94px] md:pt-[191px]">
+  <section className="hero safe-paddings relative overflow-hidden pb-32 pt-[306px] xl:pb-[166px] xl:pt-[328px] lg:pb-[134px] lg:pt-[264px] md:pb-[94px] md:pt-[191px]">
     <Image
-      className="pointer-events-none absolute left-1/2 top-0 h-auto w-[1920px] max-w-none -translate-x-1/2 object-cover xl:top-[-18px] lg:-top-3 lg:w-[1460px] md:top-[-18px] md:w-[1200px]"
+      className="pointer-events-none absolute left-1/2 top-[68px] h-auto w-[1472px] max-w-none -translate-x-1/2 object-cover xl:top-[-18px] lg:-top-3 lg:w-[1460px] md:top-[-18px] md:w-[1200px]"
       src={logoPattern}
       alt=""
       quality={85}
@@ -17,7 +17,7 @@ const Hero = () => (
     />
     <Container className="relative z-10 flex flex-col items-center text-center" size="lg">
       <div className="absolute left-1/2 top-0 -z-10 h-[270px] w-3/4 -translate-x-1/2 rounded-[1000px] bg-black-new blur-[30px] lg:w-full" />
-      <h1 className="mx-auto max-w-[968px] text-[72px] font-medium leading-none tracking-tighter 2xl:text-6xl xl:text-[56px] lg:max-w-[700px] lg:text-[52px] md:text-4xl">
+      <h1 className="mx-auto max-w-[968px] text-6xl font-medium leading-none tracking-tighter xl:text-[56px] lg:max-w-[700px] lg:text-[52px] md:text-4xl">
         Unlock <mark className="bg-transparent text-green-45">new revenue</mark> streams
         by&nbsp;partnering with Neon
       </h1>
