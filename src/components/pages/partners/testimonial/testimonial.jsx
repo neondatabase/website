0241e90a6353c8da355dc3cd62d847b77d8f8f6f
Thi Van Le <vannnyle@gmail.com>
2023-06-30 13:46:49 +0200

    fix: insert inline svg

--- src/components/pages/partners/testimonial/testimonial.jsx
+++ src/components/pages/partners/testimonial/testimonial.jsx
@@ -1,17 +1,14 @@
 import Container from 'components/shared/container/container';
 
-import quoteIcon from './images/quote.svg';
+import QuoteIcon from './images/quote.inline.svg';
 
 const Testimonial = () => (
   <figure className="testimonial safe-paddings mt-[200px] xl:mt-36 lg:mt-28 md:mt-20">
     <Container className="flex flex-col items-center text-center" size="xs">
-      <img
+      <QuoteIcon
         className="h-20 w-20 xl:h-[72px] xl:w-[72px] lg:h-16 lg:w-16 md:h-12 md:w-12"
-        src={quoteIcon}
         width={80}
         height={80}
-        alt=""
-        loading="lazy"
         aria-hidden
       />
       <blockquote className="mt-5 lg:mt-4">
