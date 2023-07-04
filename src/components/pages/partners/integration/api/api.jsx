3241e5953f17b2f6e0daa2d1fdfef090b33f91ee
Thi Van Le <vannnyle@gmail.com>
2023-07-04 11:00:49 +0200

    fix: adjust styles for api block

--- src/components/pages/partners/integration/api/api.jsx
+++ src/components/pages/partners/integration/api/api.jsx
@@ -29,11 +29,11 @@ const items = [
 
 const Api = () => (
   <div className="api mt-[104px] grid w-full grid-cols-10 items-center gap-x-10 xl:mt-24 xl:gap-x-6 lg:mt-20 lg:gap-y-7 md:mt-10 md:gap-y-6">
-    <div className="order-1 col-span-5 justify-self-end lg:order-none lg:col-span-full lg:justify-self-center">
+    <div className="order-1 col-span-5 ml-10 justify-self-start 2xl:ml-8 lg:order-none lg:col-span-full lg:ml-0 lg:justify-self-stretch">
       <h3 className="text-4xl font-medium leading-tight tracking-tighter xl:text-[32px] lg:text-center lg:text-[28px] md:text-[22px]">
         API
       </h3>
-      <p className="mt-2.5 max-w-[550px] text-lg font-light leading-snug xl:text-base lg:mx-auto lg:max-w-[584px] lg:text-center md:mt-2">
+      <p className="mt-2.5 max-w-[550px] text-lg font-light leading-snug xl:text-base lg:max-w-full lg:text-center md:mt-2">
         Manage Projects, branches, databases, and more. Easily integrate Neon in your Product and
         offer it to your users.
       </p>
