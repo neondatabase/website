7d0ac9325140731b5aef6b5ffbf8f3d9a909d864
Thi Van Le <vannnyle@gmail.com>
2023-07-11 15:39:33 +0200

    fix: minor fixes

--- src/components/pages/doc/mobile-nav/mobile-nav.jsx
+++ src/components/pages/doc/mobile-nav/mobile-nav.jsx
@@ -112,7 +112,7 @@ const MobileNav = ({ className = null, sidebar }) => {
         </ul>
         <ul className="mt-7">
           {sidebar.map((item, index) => (
-            <Item {...item} key={index} />
+            <Item {...item} closeMenu={toggleMenu} key={index} />
           ))}
         </ul>
       </motion.div>
