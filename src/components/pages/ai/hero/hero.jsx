0d03ed099e642c32448b55896fe00ca5d9c0e586
Thi Van Le <vannnyle@gmail.com>
2023-07-27 17:50:50 +0200

    chore: update text in hero

--- src/components/pages/ai/hero/hero.jsx
+++ src/components/pages/ai/hero/hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => (
     >
       <div className="absolute left-1/2 top-0 -z-10 h-[270px] w-3/4 -translate-x-1/2 rounded-[1000px] bg-black-new blur-[30px] lg:w-full" />
       <h1 className="max-w-[967px] text-6xl font-medium leading-none tracking-extra-tight">
-        Build your next <span className="text-green-45">AI and LLM</span> application on top of
-        Postgres
+        Powering next gen AI apps
+        <br /> with Postgres
       </h1>
       <p className="mt-5 max-w-[716px] text-[21px] font-light leading-snug tracking-extra-tight">
-        Introducing pg_embedding for AI vector search a faster alternative
-        <br /> to pgvector that scales to millions of rows
+        Build and scale transformative LLM application with Postgres
+        <br /> using pgvector and pg_embedding
       </p>
       <AnimatedButton
         className="relative mt-9 px-[34px] py-[17px] text-lg font-semibold tracking-extra-tight lg:mt-7 md:mt-6"
