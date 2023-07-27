7f15bc7ae470c0ee1d1be1f950363284aadebf09
Thi Van Le <vannnyle@gmail.com>
2023-07-28 00:08:13 +0200

    feat: adaptive styles for xl screen (wip)

--- src/components/pages/ai/hero/hero.jsx
+++ src/components/pages/ai/hero/hero.jsx
@@ -2,17 +2,17 @@ import AnimatedButton from 'components/shared/animated-button';
 import Container from 'components/shared/container/container';
 
 const Hero = () => (
-  <section className="hero safe-paddings pt-36">
+  <section className="hero safe-paddings pt-36 xl:pt-[120px]">
     <Container
       className="container relative z-10 flex flex-col items-center text-center"
       size="medium"
     >
       <div className="absolute left-1/2 top-0 -z-10 h-[270px] w-3/4 -translate-x-1/2 rounded-[1000px] bg-black-new blur-[30px] lg:w-full" />
-      <h1 className="max-w-[967px] text-6xl font-medium leading-none tracking-extra-tight">
+      <h1 className="max-w-[967px] text-6xl font-medium leading-none tracking-extra-tight xl:text-[56px]">
         Powering next gen AI apps
         <br /> with Postgres
       </h1>
-      <p className="mt-5 max-w-[716px] text-[21px] font-light leading-snug tracking-extra-tight">
+      <p className="mt-5 max-w-[716px] text-[21px] font-light leading-snug tracking-extra-tight xl:text-lg">
         Build and scale transformative LLM application with Postgres
         <br /> using pgvector and pg_embedding
       </p>
--- src/components/pages/ai/stats/stats.jsx
+++ src/components/pages/ai/stats/stats.jsx
@@ -13,13 +13,13 @@ const Stats = () => (
     <Container className="grid grid-cols-12" size="medium">
       <div className="col-span-full flex flex-col items-center">
         <GradientLabel>Fast and Accurate</GradientLabel>
-        <h2 className="mt-5 max-w-[690px] text-center text-5xl font-medium leading-none">
+        <h2 className="mt-5 max-w-[690px] text-center text-5xl font-medium leading-none tracking-extra-tight xl:mt-4 xl:max-w-[610px] xl:text-[44px]">
           Scale your AI apps to millions of rows with Neon
         </h2>
       </div>
-      <div className="col-span-10 col-start-2 mt-12 grid grid-cols-[287px_auto_287px] gap-x-7">
-        <div className="grid gap-y-7">
-          <div className="relative flex flex-col items-center overflow-hidden rounded-xl bg-gray-new-8 pb-[30px] pt-5">
+      <div className="xl:grid-col-12 col-span-10 col-start-2 mt-12 grid grid-cols-[23.5%_auto_23.5%] gap-x-7 xl:col-span-full xl:col-start-1 xl:mt-10 xl:grid-cols-12 xl:gap-x-6">
+        <div className="grid gap-y-7 xl:col-span-3 xl:gap-y-6">
+          <div className="relative flex flex-col items-center overflow-hidden rounded-xl bg-gray-new-8 pb-[30px] pt-5 xl:pb-5 xl:pt-4">
             <Image
               className="absolute left-1/2 top-1/2 h-full w-auto -translate-x-1/2 -translate-y-1/2 rounded-xl"
               src={githubBg}
@@ -28,18 +28,18 @@ const Stats = () => (
               alt=""
             />
             <img
-              className="relative z-10"
+              className="relative z-10 xl:h-[92px] xl:w-[92px]"
               src={github}
               width={120}
               height={120}
               alt=""
               loading="lazy"
             />
-            <span className="relative z-10 mt-3.5 text-lg font-medium leading-none">
+            <span className="relative z-10 mt-3.5 text-lg font-medium leading-none xl:mt-1.5">
               Fully open-source
             </span>
           </div>
-          <div className="relative overflow-hidden rounded-xl bg-gray-new-8 py-[38px]">
+          <div className="relative overflow-hidden rounded-xl bg-gray-new-8 py-[38px] xl:py-7">
             <Image
               className="absolute inset-0 rounded-xl"
               src={scaleBg}
@@ -47,35 +47,35 @@ const Stats = () => (
               height={257}
               alt=""
             />
-            <div className="relative z-10 flex flex-col items-center justify-center ">
-              <span className="text-2xl leading-none">Scales to</span>
-              <span className="text-[112px] font-semibold leading-none tracking-[-0.08em] text-green-45">
+            <div className="relative z-10 flex flex-col items-center justify-center">
+              <span className="text-2xl leading-none xl:text-lg">Scales to</span>
+              <span className="text-[112px] font-semibold leading-none tracking-[-0.08em] text-green-45 xl:text-7xl">
                 1M+
               </span>
-              <span className="text-[32px] leading-none tracking-extra-tight text-green-45">
+              <span className="text-[32px] leading-none tracking-extra-tight text-green-45 xl:text-2xl">
                 rows
               </span>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-y-7">
+        <div className="flex flex-col gap-y-7 xl:col-span-6 xl:gap-y-6">
           <div className="rounded-xl bg-gray-new-8 px-6">
-            <h3 className="text-20 pb-5 pt-4 text-xl leading-none tracking-extra-tight">
+            <h3 className="text-20 pb-5 pt-4 text-xl leading-none tracking-extra-tight xl:pb-4 xl:text-lg">
               Explore the lates release of pg_embedding
             </h3>
-            <div className="border-t border-dashed border-gray-new-15 pb-7 pt-6">
-              <p className="text-[13px] uppercase leading-none tracking-wider text-gray-new-50">
+            <div className="border-t border-dashed border-gray-new-15 pb-7 pt-6 xl:py-4">
+              <p className="text-[13px] uppercase leading-none tracking-wider text-gray-new-50 xl:text-xs">
                 Query execution time (ms) at 99% recall
               </p>
-              <div className="mt-5 flex items-center space-x-2.5">
-                <span className="inline-block h-8 w-[17.5%] rounded bg-[linear-gradient(90deg,rgba(0,229,153,0.20)0%,#00E599_67.45%,#4DFFC4_93.23%)]" />
-                <span className="text-[13px] leading-none text-gray-new-90">1.9s</span>
+              <div className="mt-5 flex items-center space-x-2.5 xl:mt-3">
+                <span className="inline-block h-8 w-[17.5%] rounded bg-[linear-gradient(90deg,rgba(0,229,153,0.20)0%,#00E599_67.45%,#4DFFC4_93.23%)] xl:h-7" />
+                <span className="text-[13px] leading-none text-gray-new-90 xl:text-xs">1.9s</span>
               </div>
-              <div className="mt-5 flex items-center space-x-2.5">
-                <span className="inline-block h-8 w-[93%] rounded bg-[linear-gradient(90deg,rgba(255,255,255,0.20)0%,#FFF_100%)] opacity-60" />
-                <span className="text-[13px] leading-none text-gray-new-90">10s</span>
+              <div className="mt-5 flex items-center space-x-2.5 xl:mt-3">
+                <span className="inline-block h-8 w-[93%] rounded bg-[linear-gradient(90deg,rgba(255,255,255,0.20)0%,#FFF_100%)] opacity-60 xl:h-7" />
+                <span className="text-[13px] leading-none text-gray-new-90 xl:text-xs">10s</span>
               </div>
-              <div className="mt-6 flex items-center space-x-5">
+              <div className="mt-6 flex items-center space-x-5 xl:mt-4 xl:text-sm">
                 <span className="flex items-center space-x-1.5 leading-none">
                   <span className="inline-block h-2 w-2 rounded-full bg-green-45" />
                   <span>HNSW</span>
@@ -87,8 +87,8 @@ const Stats = () => (
               </div>
             </div>
           </div>
-          <div className="rounded-xl bg-gray-new-8 px-7 pb-7 pt-6">
-            <p className="text-[26px] font-light leading-snug tracking-extra-tight">
+          <div className="rounded-xl bg-gray-new-8 px-7 pb-7 pt-6 xl:px-6 xl:pb-5 xl:pt-4">
+            <p className="text-[26px] font-light leading-snug tracking-extra-tight xl:max-w-[390px] xl:text-lg">
               The new pg_embedding extension brings{' '}
               <mark className="bg-transparent text-green-45">20x the speed</mark> for{' '}
               <mark className="bg-transparent text-green-45">99% accuracy</mark> to graph-based
@@ -96,25 +96,25 @@ const Stats = () => (
             </p>
           </div>
         </div>
-        <div className="grid grid-rows-2 gap-y-7">
-          <div className="flex flex-col items-center rounded-2xl bg-[linear-gradient(225deg,#9FD_6.87%,#6FC_24.88%,#00E599_85.59%)] px-[52px] pb-10 pt-8 text-black-new">
+        <div className="grid grid-rows-2 gap-y-7 xl:col-span-3  xl:grid-rows-none xl:gap-y-6">
+          <div className="flex flex-col items-center rounded-2xl bg-[linear-gradient(225deg,#9FD_6.87%,#6FC_24.88%,#00E599_85.59%)] pb-10 pt-8 text-black-new xl:pb-[30px] xl:pt-6">
             <span className="text-bg-clipped space-x-2 bg-[linear-gradient(180deg,#0C0D0D_19.17%,rgba(12,13,13,0.80)67.50%)] leading-none">
-              <span className="text-[120px] font-medium tracking-[-0.08em]">20</span>
-              <span className="text-[80px] font-bold">x</span>
+              <span className="text-[120px] font-medium tracking-[-0.08em] xl:text-[88px]">20</span>
+              <span className="text-[80px] font-bold xl:text-[60px]">x</span>
             </span>
-            <span className="mt-2 text-xl font-medium leading-none tracking-extra-tight">
+            <span className="mt-2 text-xl font-medium leading-none tracking-extra-tight xl:text-lg">
               Faster than ivf-flat
             </span>
           </div>
-          <div className="relative flex items-center justify-center overflow-hidden rounded-2xl bg-gray-new-8">
+          <div className="relative flex items-center justify-center overflow-hidden rounded-2xl bg-gray-new-8 xl:py-[52px]">
             <Image
-              className="absolute left-1/2 top-1/2 h-full w-auto -translate-x-1/2 -translate-y-1/2 rounded-2xl"
+              className="absolute left-1/2 top-1/2 h-auto w-full -translate-x-1/2 -translate-y-1/2 rounded-2xl"
               src={hnswBg}
               width={287}
               height={236}
               alt=""
             />
-            <span className="text-bg-clipped relative z-10 max-w-[127px] bg-[linear-gradient(180deg,#FFF_28.26%,#BFBFBF_100%)] text-center text-[44px] font-medium leading-[1.05] tracking-extra-tight">
+            <span className="text-bg-clipped relative z-10 max-w-[127px] bg-[linear-gradient(180deg,#FFF_28.26%,#BFBFBF_100%)] text-center text-[44px] font-medium leading-[1.05] tracking-extra-tight xl:max-w-[93px] xl:text-[32px]">
               Uses HNSW
             </span>
           </div>
