1cbe73fe624b17ec06baf6a7016c7f9129d521a1
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-07-28 17:27:57 +0400

    chore: animation positioning

--- src/components/pages/ai/hero/hero.jsx
+++ src/components/pages/ai/hero/hero.jsx
@@ -6,12 +6,11 @@ import AnimatedButton from 'components/shared/animated-button';
 import Container from 'components/shared/container/container';
 
 const Hero = () => (
-  <section className="hero safe-paddings relative min-h-screen pb-[200px] pt-36 xl:pt-[120px]">
+  <section className="hero safe-paddings relative pb-[390px] pt-36 xl:pt-[120px]">
     <Container
       className="container relative z-10 flex flex-col items-center text-center"
       size="medium"
     >
-      <div className="absolute left-1/2 top-0 -z-10 h-[270px] w-3/4 -translate-x-1/2 rounded-[1000px] bg-black-new blur-[30px] lg:w-full" />
       <h1 className="max-w-[967px] text-6xl font-medium leading-none tracking-extra-tight xl:text-[56px]">
         Powering next gen AI apps
         <br /> with Postgres
@@ -32,8 +31,10 @@ const Hero = () => (
       </AnimatedButton>
     </Container>
 
-    <div className="absolute left-0 top-0 h-full w-full">
-      <Spline scene="/animations/pages/ai/scene.splinecode" />
+    <div className="absolute left-0 top-0 h-full min-h-[1207px] w-full">
+      <div className="absolute bottom-0 left-0 h-full w-full">
+        <Spline scene="/animations/pages/ai/scene.splinecode" />
+      </div>
     </div>
   </section>
 );
