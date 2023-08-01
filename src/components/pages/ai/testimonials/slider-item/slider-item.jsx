62732ed499fde939944c50b6b6c69e7467691882
Thi Van Le <vannnyle@gmail.com>
2023-08-01 13:26:01 +0200

    fix: adjust styles

--- src/components/pages/ai/testimonials/slider-item/slider-item.jsx
+++ src/components/pages/ai/testimonials/slider-item/slider-item.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import TwitterIcon from './images/twitter.inline.svg';
 
 const SliderItem = ({ text, authorName, authorTitle }) => (
-  <figure className="flex min-h-[292px] flex-col items-center lg:min-h-[215px]">
+  <figure className="flex min-h-[292px] flex-col items-center lg:min-h-[248px] md:min-h-[215px] sm:min-h-[297px] xs:min-h-[326px]">
     <TwitterIcon
       className="h-[72px] w-[72px] xl:h-16 xl:w-16 md:h-12 md:w-12"
       width={72}
