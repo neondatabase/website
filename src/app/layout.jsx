8d5834e451b20bb2d4d6b8a568f9d47842a1dec0
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-02 17:52:54 +0400

    chore: add caching of animations and preload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,11 @@
-import Script from 'next/script';
+'use client';
 
 import 'styles/globals.css';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 import TopBar from 'components/shared/topbar';
+import LINKS from 'constants/links';
 
 import ThemeProvider from './provider';
 
@@ -15,41 +18,54 @@ const fontsPaths = [
 export const preferredRegion = 'edge';
 
 // eslint-disable-next-line react/prop-types
-const RootLayout = ({ children }) => (
-  <html lang="en">
-    <head>
-      {process.env.NODE_ENV === 'production' && (
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
+const RootLayout = ({ children }) => {
+  const pathname = usePathname();
+  const isAIPage = pathname === LINKS.ai;
+
+  return (
+    <html lang="en">
+      <head>
+        {process.env.NODE_ENV === 'production' && (
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'';f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer', 'GTM-MJLTK6F');
       `}
-        </Script>
-      )}
-      {fontsPaths.map((fontPath, index) => (
-        <link
-          rel="preload"
-          href={`${fontsBasePath}${fontPath}`}
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-          key={index}
-        />
-      ))}
-    </head>
-    <body>
-      {process.env.NODE_ENV === 'production' && (
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJLTK6F" height="0" width="0" style="display: none; visibility: hidden" aria-hidden="true"></iframe>`,
-          }}
-        />
-      )}
-      <ThemeProvider>
-        <TopBar />
-        {children}
-      </ThemeProvider>
-    </body>
-  </html>
-);
+          </Script>
+        )}
+        {fontsPaths.map((fontPath, index) => (
+          <link
+            rel="preload"
+            href={`${fontsBasePath}${fontPath}`}
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+            key={index}
+          />
+        ))}
+        {isAIPage && (
+          <link
+            rel="preload"
+            href="/animations/pages/ai/scene.splinecode"
+            as="fetch"
+            crossOrigin="anonymous"
+          />
+        )}
+      </head>
+      <body>
+        {process.env.NODE_ENV === 'production' && (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJLTK6F" height="0" width="0" style="display: none; visibility: hidden" aria-hidden="true"></iframe>`,
+            }}
+          />
+        )}
+        <ThemeProvider>
+          <TopBar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
