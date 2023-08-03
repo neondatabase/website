0bd1f3c2cec2eac19d93f8a83f6e68b83340a3f8
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-08-03 21:13:11 +0400

    fix: minor changes

--- src/components/pages/ai/testimonials/slider-item/slider-item.jsx
+++ src/components/pages/ai/testimonials/slider-item/slider-item.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const SliderItem = ({ text, authorName, authorTitle }) => (
-  <figure className="flex min-h-[222px] flex-col items-center justify-center lg:min-h-[184px] md:min-h-[164px] sm:min-h-[190px] xs:min-h-[246px]">
+  <figure className="flex min-h-[222px] flex-col items-center justify-center lg:min-h-[184px] md:min-h-[164px] sm:min-h-[252px]">
     <blockquote className="mt-6 lg:mt-4">
       <p
         className="with-link-primary max-w-[796px] text-[28px] font-light leading-snug tracking-tighter lg:max-w-[584px] lg:text-2xl md:text-[23px] md:leading-tight"
