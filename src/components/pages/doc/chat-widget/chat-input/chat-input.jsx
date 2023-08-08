7e112fcb79941c8a5f1644d9a6f43a87d7bdb5e6
Sasha Sushko <sasha@sushko.me>
2023-08-08 19:26:37 +0400

    fix: remove useless iseEffect

--- src/components/pages/doc/chat-widget/chat-input/chat-input.jsx
+++ src/components/pages/doc/chat-widget/chat-input/chat-input.jsx
@@ -1,23 +1,17 @@
 import PropTypes from 'prop-types';
-import { forwardRef, useEffect } from 'react';
+import { forwardRef } from 'react';
 
-const ChatInput = forwardRef(({ defaultValue = null, onEnterPress }, ref) => {
-  useEffect(() => {
-    if (defaultValue) {
-      ref.current.value = defaultValue;
-      ref.current.focus();
-    }
-  }, [defaultValue, ref]);
-
+const ChatInput = forwardRef(({ defaultValue = '', onEnterPress }, ref) => {
   const onInput = (evt) => {
     if (evt.key === 'Enter') {
       onEnterPress();
     }
   };
+
   return (
     <input
       className="peer w-full appearance-none rounded border border-gray-new-90 px-2.5 py-2 text-base leading-normal transition-colors duration-200 placeholder:text-gray-new-80 focus:outline-none dark:border-gray-new-20 dark:bg-black dark:placeholder:text-gray-new-30"
-      defaultValue={ref.current?.value}
+      defaultValue={defaultValue}
       ref={ref}
       type="text"
       placeholder="How can I help you?"
