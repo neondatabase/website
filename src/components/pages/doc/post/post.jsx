9eeec5991efb942e479b971dba9982c4696e2115
Thi Van Le <vannnyle@gmail.com>
2023-08-15 17:13:20 +0200

    fix: decrease container width

--- src/components/pages/doc/post/post.jsx
+++ src/components/pages/doc/post/post.jsx
@@ -48,8 +48,7 @@ const Post = ({
   <>
     <div
       className={clsx(
-        '-mx-10 flex flex-col 2xl:mx-0 xl:col-span-9 xl:ml-11 xl:max-w-[750px] lg:ml-0 lg:max-w-none lg:pt-0 md:mx-auto md:pb-[70px] sm:pb-8',
-        isReleaseNotes ? 'col-span-7' : 'col-span-6 2xl:col-span-7 2xl:mx-5 xl:mr-0'
+        'col-span-6 -mx-10 flex flex-col 2xl:col-span-7 2xl:mx-5 xl:col-span-9 xl:ml-11 xl:mr-0 xl:max-w-[750px] lg:ml-0 lg:max-w-none lg:pt-0 md:mx-auto md:pb-[70px] sm:pb-8'
       )}
     >
       {breadcrumbs.length > 0 && <Breadcrumbs breadcrumbs={breadcrumbs} />}
