02b8ac805f578be66f52d3b3d0607195b97d848a
Thi Van Le <vannnyle@gmail.com>
2023-08-15 14:26:57 +0200

    fix: set date as a link

--- src/components/pages/release-notes/release-note-list/release-note-list.jsx
+++ src/components/pages/release-notes/release-note-list/release-note-list.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import Content from 'components/shared/content';
 import Link from 'components/shared/link';
-import ArrowIcon from 'icons/arrow-right.inline.svg';
 import generateReleaseNotePath from 'utils/generate-release-note-path';
 import getReleaseNotesDateFromSlug from 'utils/get-release-notes-date-from-slug';
 
@@ -20,22 +19,19 @@ const ReleaseNoteList = ({ className, items }) => (
               'relative ml-1.5 w-full pb-12 pl-7 before:absolute before:-left-1 before:top-2.5 before:z-10 before:h-[9px] before:w-[9px] before:rounded-full before:bg-primary-1 after:absolute after:bottom-0 after:left-0 after:top-0 after:h-auto after:w-px after:bg-gray-7 group-first:after:top-2.5 group-last:after:bottom-6 dark:before:bg-secondary-2 dark:after:bg-gray-2 sm:ml-0 sm:max-w-full sm:pb-0 sm:pl-0 sm:before:hidden sm:after:hidden'
             )}
           >
-            <time
-              className="whitespace-nowrap text-xl font-semibold leading-normal"
-              dateTime={datetime}
-            >
-              {label}
-            </time>
-
-            <Content className="mt-3 prose-h3:text-lg" content={content} withoutAnchorHeading />
             <Link
-              className="flex items-center font-medium text-secondary-8 hover:text-secondary-7 dark:text-primary-1"
-              theme="black"
+              className="transition-colors duration-200 hover:text-secondary-8 dark:hover:text-green-45"
               to={releaseNotesPath}
             >
-              Release note page
-              <ArrowIcon className="ml-2" aria-hidden />
+              <time
+                className="whitespace-nowrap text-xl font-semibold leading-normal"
+                dateTime={datetime}
+              >
+                {label}
+              </time>
             </Link>
+
+            <Content className="mt-3 prose-h3:text-lg" content={content} withoutAnchorHeading />
           </div>
         </article>
       );
