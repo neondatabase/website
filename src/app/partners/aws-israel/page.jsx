b0b9fb65f012ddc203bee2851cdb777025938fdf
danieltprice <10074684+danieltprice@users.noreply.github.com>
2023-08-29 08:01:30 -0300

    Update page.jsx

--- src/app/partners/aws-israel/page.jsx
+++ src/app/partners/aws-israel/page.jsx
@@ -33,8 +33,6 @@ const AWSPage = () => (
     <CTAWithElephant
       className="mt-[136px] xl:mt-[104px] lg:mt-20 md:mt-16"
       buttonClassName="px-[78px] xl:px-14"
-      label="Coming soon"
-      labelTheme="gray"
       title="Try Neon<br/> in the Israel region"
       description="We are delighted to announce that Neon is now available in the new Israel (Tel Aviv) AWS region."
       buttonText="Sign up"
