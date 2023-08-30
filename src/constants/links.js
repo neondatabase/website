4d5807951099a74bdbe259db19ad09696fa37052
danieltprice <10074684+danieltprice@users.noreply.github.com>
2023-08-30 08:35:39 -0300

    Update links.js

--- src/constants/links.js
+++ src/constants/links.js
@@ -4,7 +4,7 @@ export default {
   careers: '/careers',
   blog: '/blog',
   signup: 'https://console.neon.tech/sign_in',
-  security: '/docs/security/overview',
+  security: '/docs/security/security-overview',
   releaseNotes: '/docs/release-notes',
   discord: 'https://discord.gg/YKY4CBXZT2',
   discussions: 'https://github.com/neondatabase/neon/discussions',
