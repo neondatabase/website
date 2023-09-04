a47be92c27496358c9a5c83748aad50a6c37078b
Thi Van Le <vannnyle@gmail.com>
2023-09-04 17:09:32 +0200

    fix: add all blog posts title

--- src/app/blog/(index)/category/[slug]/page.jsx
+++ src/app/blog/(index)/category/[slug]/page.jsx
@@ -8,6 +8,11 @@ import { getBlogCategoryDescription } from 'constants/seo-data';
 import { getAllWpBlogCategories, getWpPostsByCategorySlug } from 'utils/api-posts';
 import getMetadata from 'utils/get-metadata';
 
+const generateBlogTitle = (category) => {
+  if (category.slug === 'all-posts') return 'All blog posts';
+  return `${category.name} Blog`;
+};
+
 export default async function BlogCategoryPage({ params: { slug } }) {
   const categories = await getAllWpBlogCategories();
   const posts = await getWpPostsByCategorySlug(slug);
@@ -17,7 +22,7 @@ export default async function BlogCategoryPage({ params: { slug } }) {
 
   return (
     <>
-      <h1 className="sr-only">{`${category.name} Blog`}</h1>
+      <h1 className="sr-only">{generateBlogTitle(category)}</h1>
       <div className="grid grid-cols-3 gap-x-10 gap-y-16 2xl:gap-y-12 xl:gap-x-6 xl:gap-y-10 md:grid-cols-2 md:gap-y-5 sm:grid-cols-1">
         {posts.map((post, index) => (
           <BlogPostCard
@@ -42,8 +47,9 @@ export async function generateMetadata({ params }) {
   const category = categories.find((cat) => cat.slug === params.slug);
 
   if (!category) return notFound();
+
   return getMetadata({
-    title: `${category.name} Blog - Neon`,
+    title: `${generateBlogTitle(category)} - Neon`,
     description: getBlogCategoryDescription(params.slug),
     pathname: `${BLOG_CATEGORY_BASE_PATH}${params.slug}`,
     imagePath: '/images/social-previews/blog.jpg',
