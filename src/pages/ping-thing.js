f0bab135745ea8b73d5ef400f760fbe5491ceea8
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-04 11:13:07 -0400

    test: return empty div on ping-thing page

--- src/pages/ping-thing.js
+++ src/pages/ping-thing.js
@@ -1,6 +1,6 @@
 // This page is intentionally blank.
 // It's been added so the redirect in next.config.js can work with a rewrite to demos/ping-thing
 
-export default function () {
-  return null;
+export default function PingThing() {
+  return <div>Ping Thing</div>;
 }
