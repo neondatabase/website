ee08cf5451a243edd8259860ff6fb1a93daf2487
Thi Van Le <vannnyle@gmail.com>
2023-10-18 17:27:58 +0200

    chore: minor update

--- cypress/e2e/apply-form.cy.js
+++ cypress/e2e/apply-form.cy.js
@@ -34,8 +34,8 @@ describe('Apply Form', () => {
 
   it('displays an error message when the form is submitted with no required fields filled in', () => {
     cy.get("input[name='integration_type']").click({ force: true });
-    cy.get("input[name='firstname']").type('Test');
-    cy.get("input[name='lastname']").type('Test');
+    cy.get("input[name='firstname']").type('Autotest');
+    cy.get("input[name='lastname']").type('Autotest');
     cy.get('form').submit();
     cy.getByData('submit-button').should('exist').contains('Apply');
     cy.getByData('error-field-message').should('exist').contains('This is a required field');
@@ -56,8 +56,8 @@ describe('Apply Form', () => {
     ).as('formErrorSubmit');
 
     cy.get("input[name='integration_type']").click({ force: true });
-    cy.get("input[name='firstname']").type('Test');
-    cy.get("input[name='lastname']").type('Test');
+    cy.get("input[name='firstname']").type('Autotest');
+    cy.get("input[name='lastname']").type('Autotest');
     cy.get("input[name='email']").type('info@example.com');
     cy.get("input[name='company']").type('Example, Inc.');
     cy.get("textarea[name='message']").type('I would like to become a partner.');
