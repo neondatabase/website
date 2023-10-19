8bd712e088ad0f27a99cffc320507c9efa0eea3e
Thi Van Le <vannnyle@gmail.com>
2023-10-19 15:07:50 +0200

    feat: e2e test for contact form

--- cypress/e2e/partners.cy.js
+++ cypress/e2e/partners.cy.js
@@ -1,23 +1,10 @@
-import 'dotenv/config';
-
 describe('Apply Form', () => {
   beforeEach(() => {
-    cy.visit(`${Cypress.env('NEXT_PUBLIC_DEFAULT_SITE_URL')}/partners`);
+    cy.visit(`/partners`);
   });
 
   it('allows users to apply to become a partner', () => {
-    cy.intercept(
-      {
-        method: 'POST',
-        url: `https://api.hsforms.com/submissions/v3/integration/submit/**`,
-      },
-      {
-        statusCode: 200,
-        body: {
-          status: 'success',
-        },
-      }
-    ).as('formSuccessSubmit');
+    cy.formSuccessSubmit();
 
     cy.get("input[name='integration_type']").click({ force: true });
     cy.get("input[name='firstname']").type('Autotest');
@@ -42,18 +29,7 @@ describe('Apply Form', () => {
   });
 
   it('displays an error message when there is server error', () => {
-    cy.intercept(
-      {
-        method: 'POST', // or whatever method the form uses
-        url: `https://api.hsforms.com/submissions/v3/integration/submit/**`,
-      },
-      {
-        statusCode: 500,
-        body: {
-          error: 'Internal server error',
-        },
-      }
-    ).as('formErrorSubmit');
+    cy.formErrorSubmit();
 
     cy.get("input[name='integration_type']").click({ force: true });
     cy.get("input[name='firstname']").type('Autotest');
