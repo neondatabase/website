352a0b2aa42b93c8805389f72067c71dd937d99c
Thi Van Le <vannnyle@gmail.com>
2023-11-03 16:24:49 +0100

    feat: active item of ToC

--- src/components/pages/doc/table-of-contents/table-of-contents.jsx
+++ src/components/pages/doc/table-of-contents/table-of-contents.jsx
@@ -1,29 +1,67 @@
 'use client';
 
+import { useThrottleCallback } from '@react-hook/throttle';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import TOCIcon from './images/toc.inline.svg';
 
 const linkClassName =
   'py-1.5 block text-sm leading-tight transition-colors duration-200 text-gray-new-40 hover:text-black-new dark:text-gray-new-90 dark:hover:text-white [&_code]:rounded-sm [&_code]:leading-none [&_code]:py-px [&_code]:bg-gray-new-94 [&_code]:px-1.5 [&_code]:font-mono [&_code]:font-normal dark:[&_code]:bg-gray-new-15';
 
-const TableOfContents = ({ items }) => {
-  const handleAnchorClick = (e, anchor) => {
-    e.preventDefault();
-    document.querySelector(anchor).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-    // changing hash without default jumps to anchor
+const CURRENT_ANCHOR_GAP_PX = 16;
+
+const handleAnchorClick = (e, anchor) => {
+  e.preventDefault();
+  document.querySelector(anchor).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+  // changing hash without default jumps to anchor
+  // eslint-disable-next-line no-restricted-globals
+  if (history.pushState) {
     // eslint-disable-next-line no-restricted-globals
-    if (history.pushState) {
-      // eslint-disable-next-line no-restricted-globals
-      history.pushState(false, false, anchor);
-    } else {
-      // old browser support
-      window.location.hash = anchor;
-    }
-  };
+    history.pushState(false, false, anchor);
+  } else {
+    // old browser support
+    window.location.hash = anchor;
+  }
+};
+const TableOfContents = ({ items }) => {
+  const titles = useRef([]);
+  const [currentAnchor, setCurrentAnchor] = useState(null);
+
+  useEffect(() => {
+    titles.current = items
+      .map(({ id }) => document.getElementById(id))
+      .filter((anchor) => anchor !== null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateCurrentAnchor = useCallback(() => {
+    const currentTitleIdx = titles.current.findIndex(
+      (anchor) => anchor.getBoundingClientRect().top - CURRENT_ANCHOR_GAP_PX >= 0
+    );
+
+    const idx =
+      currentTitleIdx === -1 ? titles.current.length - 1 : Math.max(currentTitleIdx - 1, 0);
+
+    const currentTitle = titles.current[idx];
+
+    setCurrentAnchor(currentTitle.id);
+  }, []);
+
+  const onScroll = useThrottleCallback(updateCurrentAnchor, 5);
+
+  useEffect(() => {
+    updateCurrentAnchor();
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (items.length === 0) return null;
 
@@ -40,7 +78,9 @@ const TableOfContents = ({ items }) => {
           return (
             <li key={index}>
               <a
-                className={linkClassName}
+                className={clsx(linkClassName, {
+                  'text-black-new dark:text-white font-medium': currentAnchor === item.id,
+                })}
                 style={{
                   marginLeft: level === 2 ? '' : `${(level - 2) * 0.5}rem`,
                 }}
