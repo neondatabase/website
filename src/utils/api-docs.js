83f69e13386fb595b0f9e7768b7ce8ce93fe3ed1
Thi Van Le <vannnyle@gmail.com>
2023-11-03 11:18:33 +0100

    fix: remove h4 from ToC

--- src/utils/api-docs.js
+++ src/utils/api-docs.js
@@ -112,7 +112,7 @@ const getTableOfContents = (content) => {
     // replace mdx inline code with html inline code
     const titleWithInlineCode = title.replace(/`([^`]+)`/g, '<code>$1</code>');
 
-    if (title && depth && depth <= 3) {
+    if (title && depth && depth <= 2) {
       toc.push({
         title: titleWithInlineCode,
         id: slugify(title, { lower: true, strict: true, remove: /[*+~.()'"!:@]/g }),
