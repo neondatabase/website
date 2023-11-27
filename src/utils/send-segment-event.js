d281d2a9432296ce523ef0533c85486fc32534f1
Thi Van Le <vannnyle@gmail.com>
2023-11-27 17:31:34 +0100

    chore: remove console.log

--- src/utils/send-segment-event.js
+++ src/utils/send-segment-event.js
@@ -2,6 +2,5 @@ import { AnalyticsBrowser } from '@segment/analytics-next';
 
 const analytics = AnalyticsBrowser.load({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY });
 export default function sendSegmentEvent(eventName, properties) {
-  console.log('sendSegmentEvent', eventName, properties);
   analytics.track(eventName, properties);
 }
