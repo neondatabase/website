e6fc95f9e26404557eb2ad34b93c2874e8679e4f
Thi Van Le <vannnyle@gmail.com>
2023-12-08 17:15:54 +0100

    feat: add project id

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'f1g3w6',
   e2e: {
     baseUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL,
     viewportWidth: 1920,
