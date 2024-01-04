c99e8b57a611ba4c11cadeb9891eee15e084f238
Thi Van Le <vannnyle@gmail.com>
2024-01-04 12:11:10 +0100

    fix: update the segment function

--- src/utils/send-segment-event.js
+++ src/utils/send-segment-event.js
@@ -1,6 +1,20 @@
 import { AnalyticsBrowser } from '@segment/analytics-next';
 
-const analytics = AnalyticsBrowser.load({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY });
-export default function sendSegmentEvent(eventName, properties) {
-  analytics.track(eventName, properties);
+let analytics;
+console.log('NODE_ENV', process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'test') {
+  // Check if not in a test environment
+  analytics = AnalyticsBrowser.load({ writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY });
+}
+
+export default async function sendSegmentEvent(eventName, properties) {
+  try {
+    if (analytics && typeof analytics.track === 'function') {
+      await analytics.track(eventName, properties);
+    } else {
+      console.error('Analytics is not initialized or track is not a function');
+    }
+  } catch (error) {
+    console.error('Failed to send segment event:', error);
+  }
 }
