a936dae36495dc552f5eed98b1197b402ed95229
bgrenon <barry_grenon@yahoo.ca>
2024-01-09 17:35:26 -0400

    Update index.js

--- content/docs/shared-content/index.js
+++ content/docs/shared-content/index.js
@@ -1,7 +1,7 @@
 const sharedMdxComponents = {
   // name of component: path to component (not including content/docs/)
   NeedHelp: 'shared-content/need-help',
-  SignUp: 'shared-content/sign-up'
+  SignUp: 'shared-content/sign-up',
 };
 
 module.exports = sharedMdxComponents;
