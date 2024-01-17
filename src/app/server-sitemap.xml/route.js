7e19f3d49a3703bbc31bec130c043b402083fcd1
Thi Van Le <vannnyle@gmail.com>
2024-01-17 18:22:00 +0100

    feat: add server sitemap

---
+++ src/app/server-sitemap.xml/route.js
@@ -0,0 +1,19 @@
+import { getServerSideSitemap } from 'next-sitemap';
+
+import { getAllWpPosts } from 'utils/api-posts';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function GET(request) {
+  const wpPosts = await getAllWpPosts();
+
+  const entries = wpPosts.map((post) => ({
+    loc: `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/${post.slug}`,
+    lastmod: new Date(post.date).toISOString(),
+    changefreq: 'daily',
+    priority: 0.7,
+  }));
+
+  console.log(entries);
+
+  return getServerSideSitemap(request, entries);
+}
