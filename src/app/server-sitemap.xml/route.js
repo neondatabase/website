d8cd1392fa68f29e5282bad64f7a1125e343b35e
Thi Van Le <vannnyle@gmail.com>
2024-01-18 13:06:12 +0100

    fix: additional sitemap

--- src/app/server-sitemap.xml/route.js
+++ src/app/server-sitemap.xml/route.js
@@ -1,17 +1,26 @@
 import { getServerSideSitemap } from 'next-sitemap';
 
-import { getAllWpPosts } from 'utils/api-posts';
+import { getAllWpBlogCategories, getAllWpPosts } from 'utils/api-posts';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function GET() {
   const wpPosts = await getAllWpPosts();
+  const wpCategories = await getAllWpBlogCategories();
 
-  const entries = wpPosts.map((post) => ({
+  const wpPostEntries = wpPosts.map((post) => ({
     loc: `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/${post.slug}`,
-    lastmod: new Date(post.date).toISOString(),
+    lastmod: new Date(post.modifiedGmt).toISOString(),
     changefreq: 'daily',
     priority: 0.7,
   }));
 
+  const wpCategoryEntries = wpCategories.map((category) => ({
+    loc: `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/category/${category.slug}`,
+    lastmod: new Date().toISOString(),
+    changefreq: 'daily',
+    priority: 0.7,
+  }));
+
+  const entries = [...wpPostEntries, ...wpCategoryEntries];
   return getServerSideSitemap(entries);
 }
