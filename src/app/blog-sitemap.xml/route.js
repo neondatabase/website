c85084c6cfbf2f6044bba4c60e4b95c876be607d
Thi Van Le <vannnyle@gmail.com>
2024-01-22 13:58:05 +0100

    fix: update urlset attributes

--- src/app/blog-sitemap.xml/route.js
+++ src/app/blog-sitemap.xml/route.js
@@ -9,34 +9,34 @@ export async function GET() {
 
   return new Response(
     `<?xml version="1.0" encoding="UTF-8" ?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${wpPosts
-              .map(
-                (post) => `
-                <url>
-                    <loc>${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/${post.slug}</loc>
-                    <lastmod>${new Date(post.modifiedGmt).toISOString()}</lastmod>
-                    <changefreq>daily</changefreq>
-                    <priority>0.7</priority>
-                </url>
-            `
-              )
-              .join('')}
-            ${wpCategories
-              .map(
-                (category) => `
-                <url>
-                    <loc>${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/category/${
-                      category.slug
-                    }</loc>
-                    <lastmod>${new Date().toISOString()}</lastmod>
-                    <changefreq>daily</changefreq>
-                    <priority>0.7</priority>
-                </url>
-                `
-              )
-              .join('')}
-        </urlset>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+          ${wpPosts
+            .map(
+              (post) => `
+              <url>
+                  <loc>${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/${post.slug}</loc>
+                  <lastmod>${new Date(post.modifiedGmt).toISOString()}</lastmod>
+                  <changefreq>daily</changefreq>
+                  <priority>0.7</priority>
+              </url>
+          `
+            )
+            .join('')}
+          ${wpCategories
+            .map(
+              (category) => `
+              <url>
+                  <loc>${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/blog/category/${
+                    category.slug
+                  }</loc>
+                  <lastmod>${new Date().toISOString()}</lastmod>
+                  <changefreq>daily</changefreq>
+                  <priority>0.7</priority>
+              </url>
+              `
+            )
+            .join('')}
+      </urlset>
     `,
     {
       headers,
