3e932c321643f85a896901dc73b86b8e629bb8e0
americano98 <mr.nik.bazarov@gmail.com>
2024-02-01 15:02:12 +0700

    feat: upd hero

--- src/components/pages/pricing/hero/hero.jsx
+++ src/components/pages/pricing/hero/hero.jsx
@@ -2,9 +2,10 @@
 
 import clsx from 'clsx';
 import { LazyMotion, domAnimation, m, useAnimation } from 'framer-motion';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import AnimatedButton from 'components/shared/animated-button';
+import Button from 'components/shared/button';
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
@@ -15,84 +16,96 @@ import sendSegmentEvent from 'utils/send-segment-event';
 
 const items = [
   {
-    type: 'Free',
+    type: 'Free Tier',
     price: '$0 <span>/month</span>',
-    description:
-      'A generous free tier with essential features perfect for prototypes and personal projects.',
+    description: 'Generous free tier for personal projects and evaluating Neon.',
     features: [
-      { title: '1 project, 10 branches, unlimited DBs' },
-      { title: 'Shared compute, 3GiB storage' },
+      { title: '1 project, 10 branches' },
+      { title: 'Project sharing' },
+      { title: 'Point-in-time restore (24hrs)' },
       { title: 'Community support' },
+      { title: 'SOC 2 and compliance reports' },
     ],
     button: {
       url: LINKS.signup,
-      text: 'Get Started',
+      text: 'Start for free',
       theme: 'white-outline',
       event: 'pricing_hero_free_btn_click',
     },
   },
   {
-    type: 'Pro',
-    price: 'From $3 <span>/month</span>',
-    description:
-      'A pro plan with all the features for production. Only pay for the compute & storage used.',
+    type: 'Launch',
+    price: '$29 <span>/month</span>',
+    description: 'Perfect for static website, low code apps or building your zero-to-one MVP.',
     features: [
-      { title: 'Unlimited projects, branches, databases' },
-      { title: 'Configurable compute, unlimited storage' },
-      { title: 'Pro support' },
-      { title: 'Autoscaling, read replicas, project sharing' },
-      { title: 'IP allow' },
+      { title: '10 projects' },
+      { title: 'Branching' },
+      { title: 'Autoscale to 7 compute units' },
+      { title: '7 day point-in-time restore' },
+      { title: 'Neon customer support' },
     ],
     button: {
       url: '#estimates',
-      text: 'Estimate Your Cost',
+      text: 'Get started',
       theme: 'primary',
       event: 'pricing_hero_pro_btn_click',
     },
   },
   {
+    type: 'Scale',
+    price: '$69 <span>/month</span>',
+    description: 'Scale to production workloads and forget about babysitting infrastructure.',
+    features: [
+      { title: 'All features from Launch' },
+      { title: '50 projects' },
+      { title: 'Autoscale to 7 compute units' },
+      { title: '30 days point-in-time restore' },
+      { title: 'IP allow' },
+    ],
+    button: {
+      url: LINKS.contactSales,
+      text: 'Get started',
+      theme: 'white-outline',
+      event: 'pricing_hero_custom_btn_click',
+    },
+  },
+  {
     type: 'Enterprise',
     price: 'Custom',
-    description:
-      'Custom volume-based plans for medium to large teams, database fleets, and resale.',
+    description: 'Best for large teams, database fleets, and resale.',
     features: [
-      { title: 'Custom pricing with volume discounts' },
+      { title: 'Custom pricing with discounts' },
       { title: 'Prepaid plans and capacity contracts' },
       { title: 'Enterprise support' },
     ],
     button: {
       url: LINKS.contactSales,
-      text: 'Contact Sales',
+      text: 'Request trial',
       theme: 'white-outline',
       event: 'pricing_hero_custom_btn_click',
     },
   },
 ];
 
+const scaleCardBorderVariants = {
+  from: {
+    opacity: 0,
+  },
+  to: {
+    opacity: [0, 0.4, 0.2, 1, 0.5, 1],
+    transition: {
+      ease: 'easeInOut',
+      duration: 1,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
 const Hero = () => {
   const [isLoad, setIsLoad] = useState(false);
-  const [hoverCount, setHoverCount] = useState(0);
-  const [activeItemIndex, setActiveItemIndex] = useState(1);
   const controls = useAnimation();
-
-  const borderLightVariants = useMemo(
-    () => ({
-      from: {
-        opacity: 0,
-      },
-      to: {
-        opacity: hoverCount === 0 ? [0, 1, 0.5, 1, 0.75, 1] : [0, 0.4, 0.2, 1, 0.5, 1],
-        transition: {
-          ease: 'easeInOut',
-          duration: hoverCount === 0 ? 0.5 : 1,
-        },
-      },
-      exit: {
-        opacity: 0,
-      },
-    }),
-    [hoverCount]
-  );
 
   useEffect(() => {
     controls.start('to');
@@ -110,109 +123,117 @@ const Hero = () => {
           tag="h1"
           size="lg"
         >
-          <span className="text-pricing-primary-1">Start free.</span>{' '}
-          <span>Only pay for what you use.</span>
+          <span>Goodbye Infrastructure.</span>{' '}
+          <span className="text-pricing-primary-1">Hello, Serverless Postgres.</span>
         </Heading>
         <p className="mx-auto mt-5 max-w-[620px] text-center text-xl font-light leading-snug xl:mt-4 xl:max-w-[570px] xl:text-lg md:mt-3 md:text-base">
-          Neon brings serverless architecture to Postgres, which allows us to offer you flexible
+          Neon brings serverless architecture to PostgreSQL, which allows us to offer you flexible
           usage and volume-based plans.
         </p>
-        <div className="relative mx-auto mt-16 max-w-[1220px] xl:mt-12 lg:w-full lg:max-w-[704px] md:mt-9">
-          <ul className="relative z-10 grid grid-cols-3 gap-x-11 xl:gap-x-6 lg:grid-cols-1 lg:gap-x-4 lg:gap-y-4 md:grid-cols-1 md:gap-y-6">
-            {items.map(({ type, price, description, features, button }, index) => (
-              <li
-                className={clsx(
-                  'group relative rounded-[10px]',
-                  type === 'Pro' && 'lg:-order-1 lg:col-span-full'
-                )}
-                style={{
-                  '--accentColor':
-                    type === 'Free' ? '#ade0eb' : type === 'Pro' ? '#00e599' : '#f0f075',
-                  '--hoverColor':
-                    type === 'Free' ? '#c6eaf1' : type === 'Pro' ? '#00ffaa' : '#f5f5a3',
-                }}
-                key={index}
-                onPointerEnter={() => {
-                  setActiveItemIndex(index);
-                  setHoverCount((prev) => (prev === 1 ? 0 : prev + 1));
-                  controls.start('to');
-                }}
-              >
-                <Link
-                  className={clsx(
-                    'relative z-10 flex min-h-full flex-col rounded-[10px] px-7 py-5 transition-colors duration-500 xl:px-6 xl:py-5 sm:p-5',
-                    activeItemIndex !== index ? 'bg-gray-new-8' : 'bg-transparent'
-                  )}
-                  to={button.url}
-                  onClick={(e) => {
-                    sendGtagEvent(button.event);
-                    sendSegmentEvent(button.event);
+        <div className="relative mx-auto mt-16 xl:mt-12 lg:w-full lg:max-w-[704px] md:mt-9">
+          <h2 className="sr-only">Neon pricing plans</h2>
+          <ul className="grid-gap relative z-10 grid grid-cols-4 xl:grid-cols-2 lg:gap-y-4 md:grid-cols-1 md:gap-y-6">
+            {items.map(({ type, price, description, features, button }, index) => {
+              const isScalePlan = type === 'Scale';
 
-                    if (button.url === '#estimates') {
-                      e.preventDefault();
-                      const estimates = document.getElementById('estimates');
-                      estimates.scrollIntoView({ behavior: 'smooth', block: 'start' });
+              return (
+                <li
+                  className={clsx('group relative rounded-[10px]', isScalePlan && 'xl:-order-1')}
+                  key={index}
+                  onPointerEnter={() => {
+                    if (isScalePlan) {
+                      controls.start('to');
                     }
                   }}
                 >
-                  <div className="mb-6 flex min-h-[274px] flex-col border-b border-dashed border-gray-new-20 pb-4 xl:mb-5 xl:min-h-[265px] xl:pb-0 lg:min-h-max lg:pb-5">
-                    <span className="text-xl font-medium leading-none tracking-tight text-[var(--accentColor)] xl:text-lg">
-                      {type}
-                    </span>
-                    <h2
-                      className="mt-7 text-[36px] font-light leading-none tracking-tighter xl:mt-5 xl:text-[32px] md:mt-4 [&_span]:text-[28px] [&_span]:font-light [&_span]:-tracking-[0.06em] [&_span]:text-gray-new-50"
-                      dangerouslySetInnerHTML={{ __html: price }}
-                    />
-                    <AnimatedButton
-                      className="mt-7 w-full !bg-[var(--accentColor)] !py-4 !text-lg !font-medium tracking-tight group-hover:!bg-[var(--hoverColor)] xl:mt-7 sm:max-w-none"
-                      isAnimated={activeItemIndex === index}
-                      animationColor="var(--accentColor)"
-                      theme="primary"
-                      size="sm"
-                    >
-                      {button.text}
-                    </AnimatedButton>
-                    <p className="mt-9 font-light leading-snug tracking-tight text-gray-new-70 xl:mt-8">
-                      {description}
-                    </p>
-                  </div>
-                  <div className="mt-auto flex grow flex-col">
-                    <ul className="mb-4 flex flex-col flex-wrap space-y-4 xl:mb-0">
-                      {features.map(({ title, label }, index) => (
-                        <li className="relative pl-6 leading-tight tracking-tight" key={index}>
-                          <CheckIcon
-                            className="absolute left-0 top-[2px] h-4 w-4 text-[var(--accentColor)]"
-                            aria-hidden
-                          />
-                          <span>{title}</span>
-                          {label && (
-                            <span className="ml-2 whitespace-nowrap rounded-full bg-pricing-primary-4 px-3 py-1 align-middle text-[10px] font-semibold uppercase leading-none tracking-[0.02em] text-pricing-primary-1">
-                              {label}
-                            </span>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </Link>
-                <LazyMotion features={domAnimation}>
-                  <m.span
+                  <Link
                     className={clsx(
-                      'pointer-events-none absolute left-0 top-0 z-20 h-full w-full rounded-[10px] border transition-colors duration-300 md:border-[var(--accentColor)] md:!opacity-100',
-                      isLoad !== true && '!opacity-100',
-                      activeItemIndex === index
-                        ? 'border-[var(--accentColor)]'
-                        : 'border-transparent'
+                      !isScalePlan &&
+                        'border border-transparent bg-gray-new-8 group-hover:border-gray-new-15 group-hover:bg-gray-new-10',
+                      'relative z-10 flex min-h-full flex-col rounded-[10px]  px-7 pb-9 pt-5 transition-colors duration-500 xl:px-6 xl:py-5 sm:p-5'
                     )}
-                    initial="from"
-                    exit="exit"
-                    variants={borderLightVariants}
-                    animate={controls}
-                    aria-hidden
-                  />
-                </LazyMotion>
-              </li>
-            ))}
+                    to={button.url}
+                    onClick={(e) => {
+                      sendGtagEvent(button.event);
+                      sendSegmentEvent(button.event);
+
+                      if (button.url === '#estimates') {
+                        e.preventDefault();
+                        const estimates = document.getElementById('estimates');
+                        estimates.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                      }
+                    }}
+                  >
+                    <div className="mb-6 flex flex-col border-b border-dashed border-gray-new-20 pb-5 xl:mb-5">
+                      <h3
+                        className={clsx(
+                          isScalePlan && 'text-green-45',
+                          'text-xl font-medium leading-none tracking-tight xl:text-lg'
+                        )}
+                      >
+                        {type}
+                      </h3>
+                      <p
+                        className="mt-8 text-[36px] font-light leading-none tracking-tighter xl:mt-5 xl:text-[32px] md:mt-4 [&_span]:text-[28px] [&_span]:font-light [&_span]:-tracking-[0.06em] [&_span]:text-gray-new-50"
+                        dangerouslySetInnerHTML={{ __html: price }}
+                      />
+                      {isScalePlan ? (
+                        <AnimatedButton
+                          className="mt-7 w-full !bg-green-45 !py-4 !text-lg !font-medium tracking-tight group-hover:!bg-[#00ffaa] xl:mt-7 sm:max-w-none"
+                          animationColor="#00e599"
+                          theme="primary"
+                          size="sm"
+                          isAnimated
+                        >
+                          {button.text}
+                        </AnimatedButton>
+                      ) : (
+                        <Button
+                          className="mt-7 w-full bg-gray-new-15 !py-4 !text-lg !font-medium tracking-tight xl:mt-7 sm:max-w-none"
+                          size="sm"
+                        >
+                          {button.text}
+                        </Button>
+                      )}
+                      <p className="mt-9 font-light leading-snug tracking-tight text-gray-new-70 2xl:min-h-[66px] xl:mt-8 xl:min-h-[44px] lg:min-h-max">
+                        {description}
+                      </p>
+                    </div>
+                    <div className="mt-auto flex grow flex-col">
+                      <ul className="flex flex-col flex-wrap gap-y-4">
+                        {features.map(({ title }, index) => (
+                          <li className="relative pl-6 leading-tight tracking-tight" key={index}>
+                            <CheckIcon
+                              className={clsx(
+                                isScalePlan ? 'text-green-45' : 'text-gray-new-70',
+                                'absolute left-0 top-[2px] h-4 w-4 '
+                              )}
+                              aria-hidden
+                            />
+                            <span>{title}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </Link>
+                  {isScalePlan && (
+                    <LazyMotion features={domAnimation}>
+                      <m.span
+                        className={clsx(
+                          'pointer-events-none absolute left-0 top-0 z-20 h-full w-full rounded-[10px] border border-green-45 transition-colors duration-300 md:!opacity-100',
+                          isLoad !== true && '!opacity-100'
+                        )}
+                        initial="from"
+                        exit="exit"
+                        variants={scaleCardBorderVariants}
+                        animate={controls}
+                        aria-hidden
+                      />
+                    </LazyMotion>
+                  )}
+                </li>
+              );
+            })}
           </ul>
           <p className="mt-8 text-center text-[15px] font-light leading-snug tracking-extra-tight text-gray-new-50">
             On February 19th, 2024, Neon will launch new, simplified pricing plans. You can see the{' '}
@@ -225,6 +246,19 @@ const Hero = () => {
             </Link>
           </p>
         </div>
+        <p className="mt-16 text-center text-lg font-light leading-snug">
+          Not sure which plan is right for you?
+          <br />
+          Explore the{' '}
+          <Link
+            className="inline-block decoration-1 underline-offset-4 hover:!decoration-green-45/0"
+            to={LINKS.contactSales}
+            theme="green-underlined"
+          >
+            detailed plan comparison
+          </Link>
+          .
+        </p>
       </Container>
     </section>
   );
