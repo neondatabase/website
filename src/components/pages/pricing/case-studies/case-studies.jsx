6a821a9fe68bdd01b76708e8b10676b80ab9804a
Andy Hattemer <andy@neon.tech>
2024-02-19 12:46:19 -0500

    Pricing page - Update banner compute list items (#1454)
    * chore: fix compute lines
    
    * chore: tweak case-study headline

--- src/components/pages/pricing/case-studies/case-studies.jsx
+++ src/components/pages/pricing/case-studies/case-studies.jsx
@@ -62,13 +62,13 @@ const CaseStudies = ({ className = null }) => (
     <Container className="grid-gap-x grid grid-cols-12" size="medium">
       <div className="col-span-10 col-start-2 grid grid-cols-10 gap-x-10 rounded-2xl bg-gray-new-8 px-14 py-12 xl:col-span-full xl:col-start-1 xl:grid-cols-12 xl:items-center xl:gap-x-6 xl:p-8 lg:gap-x-4 lg:pb-10 lg:pt-9 md:py-8">
         <div className="col-span-4 col-start-1 flex flex-col items-start xl:max-w-[300px] xl:self-start xl:justify-self-start lg:col-span-full lg:max-w-none">
-          <GradientLabel>Customers</GradientLabel>
-          <h2 className="mt-3 max-w-[322px] text-[40px] font-medium leading-none tracking-[-0.02em] xl:max-w-[270px] xl:text-[44px] lg:max-w-none lg:text-4xl md:text-[32px]">
-            Database velocity from MVP to IPO
+          <GradientLabel>Why Neon?</GradientLabel>
+          <h2 className="mt-3 max-w-[340px] text-[40px] font-medium leading-none tracking-[-0.02em] xl:max-w-[270px] xl:text-[44px] lg:max-w-none lg:text-4xl md:text-[32px]">
+            Grow productivity, not costs.
           </h2>
           <p className="mt-4 max-w-[362px] text-lg font-light leading-snug xl:max-w-[280px] xl:text-base lg:max-w-[648px] md:mt-2.5 sm:max-w-none sm:pr-1.5">
-            Businesses of all sizes leverage features like branching and autoscaling to increase dev
-            velocity and gain a competitive advantage.{' '}
+            Teams of all sizes use branching to increase development velocity, and autoscaling means
+            costs stay efficient with zero effort.
           </p>
           <Link
             className="mt-4 inline-block decoration-1 underline-offset-4 hover:!decoration-green-45/0 sm:mt-3"
