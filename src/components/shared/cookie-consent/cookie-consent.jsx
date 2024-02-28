ce6bfc37b107700ac4e864adae28007cc232a514
Andy Hattemer <andy@neon.tech>
2024-02-28 07:54:48 -0500

    chore: simplify consent wording

--- src/components/shared/cookie-consent/cookie-consent.jsx
+++ src/components/shared/cookie-consent/cookie-consent.jsx
@@ -9,9 +9,7 @@ import sendGtagEvent from 'utils/send-gtag-event';
 import Button from '../button';
 
 const CookieConsent = ({}) => {
-  const [visualState, setVisualState] = useState('hidden');
-  const [purposes, setPurposes] = useState({});
-  const [choices, setChoices] = useState({});
+  const [visible, setVisible] = useState(false);
 
   const consentAll = () => {
     if (zaraz && zaraz.consent) {
@@ -19,7 +17,7 @@ const CookieConsent = ({}) => {
       zaraz.consent.sendQueuedEvents();
     }
     sendGtagEvent('cookie_consent', { action: 'accept_all' });
-    setVisualState('hidden');
+    setVisible(false);
   };
 
   const rejectAll = () => {
@@ -27,99 +25,31 @@ const CookieConsent = ({}) => {
       zaraz.consent.setAll(false);
     }
     sendGtagEvent('cookie_consent', { action: 'reject_all' });
-    setVisualState('hidden');
-  };
-
-  const updateChoice = (e) => {
-    setChoices((prev) => {
-      prev[e.target.id] = e.target.checked;
-      return prev;
-    });
-  };
-
-  const confirmChoices = () => {
-    Object.keys(choices).forEach((key) => {
-      zaraz.consent.set({ [key]: choices[key] });
-    });
-    setVisualState('hidden');
-  };
-
-  const expandBanner = () => {
-    setPurposes(zaraz.consent.purposes);
-    /* default choices to ticked */
-    setChoices(
-      Object.keys(zaraz.consent.getAll()).reduce((acc, key) => {
-        acc[key] = true;
-        return acc;
-      }, {})
-    );
-    setVisualState('expanded');
+    setVisible(false);
   };
 
   useEffect(() => {
     if (document.cookie.indexOf('neon_consent') === -1) {
-      setVisualState('visible');
+      setVisible(true);
     }
   }, []);
 
-  return visualState === 'visible' ? (
-    <div className="fixed bottom-4 left-4 z-50 flex max-w-[590px] items-center gap-4 rounded-[10px] border border-gray-new-30 bg-gray-new-15 px-6 py-4 text-white shadow-md">
+  return visible ? (
+    <div className="fixed bottom-4 left-4 z-50 flex max-w-[510px] items-end  gap-4 rounded-[10px] border border-gray-new-30 bg-gray-new-15 px-6 py-4 text-white shadow-md">
       <p className="text-sm">
-        We use cookies to improve your website experience. Click Accept to agree to the{' '}
+        We use first-party cookies to improve our services. Learn more at{' '}
         <Link to={LINKS.cookiePolicy} className="underline">
           Cookie Policy
         </Link>
         .
       </p>
-      <Button className="h-9" size="xxs" theme="white-outline" onClick={expandBanner}>
-        Customize
+      <Button className="h-9" size="xxs" theme="gray-dark-outline" onClick={rejectAll}>
+        Opt Out
       </Button>
       <Button className="h-9" size="xxs" theme="quaternary" onClick={consentAll}>
         Accept
       </Button>
     </div>
-  ) : visualState === 'expanded' ? (
-    <>
-      <div className="fixed bottom-4 left-4 z-50 flex w-full max-w-md flex-col gap-4 rounded-[10px] border border-gray-new-30 bg-gray-new-15 px-6 py-4 text-white shadow-md">
-        <span className="text-xl font-bold">Cookie Settings</span>
-        <p className="mb-4 text-sm">
-          We use cookies to improve your website experience. Click Accept to agree to the{' '}
-          <Link to={LINKS.cookiePolicy} className="underline">
-            Cookie Policy
-          </Link>
-          .
-        </p>
-
-        {Object.keys(purposes).map((key) => (
-          <div className="flex items-start gap-4 pl-2">
-            <input
-              type="checkbox"
-              id={key}
-              name={key}
-              className="mt-1"
-              defaultChecked
-              onChange={updateChoice}
-            />
-            <label htmlFor={key} className="flex flex-col text-sm">
-              <span className="font-bold">{purposes[key].name.en}</span>
-              <span>{purposes[key].description.en}</span>
-            </label>
-          </div>
-        ))}
-        <div className="mt-4 flex justify-end gap-4">
-          <Button className="h-9" size="xxs" theme="white-outline" onClick={rejectAll}>
-            Reject All
-          </Button>
-          <Button className="h-9" size="xxs" theme="white-outline" onClick={consentAll}>
-            Accept All
-          </Button>
-          <Button className="h-9" size="xxs" theme="quaternary" onClick={confirmChoices}>
-            Confirm Choices
-          </Button>
-        </div>
-      </div>
-      <div className="fixed inset-0 z-40" onClick={() => setVisualState('visible')} />
-    </>
   ) : (
     <></>
   );
