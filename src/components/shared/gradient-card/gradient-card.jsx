2e9811c1a003e8c489a96e43403b03d5e3479c4b
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2024-03-07 18:14:29 +0100

    fix: update GradientCard

--- src/components/shared/gradient-card/gradient-card.jsx
+++ src/components/shared/gradient-card/gradient-card.jsx
@@ -7,14 +7,14 @@ const GradientCard = ({ className, children, as: Tag = 'div', ...rest }) => {
   const clickable = Tag === Link;
 
   return (
-    <Tag
-      className={clsx(className, 'group relative block h-full overflow-hidden rounded-xl')}
-      {...rest}
-    >
+    <Tag className={clsx(className, 'group relative block h-full rounded-xl')} {...rest}>
+      {/* border gradient */}
+      <span className="absolute -inset-0.5 rounded-[inherit] bg-[linear-gradient(150deg,rgba(92,97,101,1)0%,rgba(255,255,255,0.02)50%,rgba(30,31,33,1)100%)]" />
+
       {/* bg */}
       <span
         className={clsx(
-          'absolute inset-0 opacity-100',
+          'absolute inset-0 rounded-[inherit] opacity-100',
           'bg-[radial-gradient(162.08%_141.42%_at_0%_0%,rgba(48,50,54,0.20)0%,rgba(48,50,54,0.00)48.97%),linear-gradient(165deg,#1A1C1E_6.13%,#111213_75.96%)]',
           clickable && 'transition-opacity duration-300 group-hover:opacity-0'
         )}
@@ -24,7 +24,7 @@ const GradientCard = ({ className, children, as: Tag = 'div', ...rest }) => {
       {clickable && (
         <span
           className={clsx(
-            'absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100',
+            'absolute inset-0 rounded-[inherit] opacity-0 transition-opacity duration-300 group-hover:opacity-100',
             'bg-[radial-gradient(162.08%_141.42%_at_0%_0%,rgba(58,60,64,0.50)0%,rgba(58,60,64,0.00)48.97%),linear-gradient(165deg,#2C2E32_6.13%,#18191B_75.96%)]'
           )}
         />
@@ -32,14 +32,6 @@ const GradientCard = ({ className, children, as: Tag = 'div', ...rest }) => {
 
       {/* noise */}
       <span className="absolute inset-0 bg-[url('/images/noise.png')] bg-cover opacity-40" />
-
-      {/* border gradient */}
-      <div
-        className={clsx(
-          'absolute inset-0 rounded-[inherit] !border-2',
-          'border-image-[linear-gradient(116.81deg,#5E6268_0%,#2E2F32_24.63%,#1D1D1F_55.02%,#1D1E1F_100%),linear-gradient(56.71deg,#151618_0%,rgba(21,22,24,0)_28.44%)]'
-        )}
-      />
 
       {/* content */}
       <div className="relative h-full">{children}</div>
