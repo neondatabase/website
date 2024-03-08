9b6f8e9758ea697f8de75ac76dfe5d92f58dddb1
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2024-03-08 16:39:16 +0100

    fix: fixes after review

--- src/components/pages/ai/boost/boost.jsx
+++ src/components/pages/ai/boost/boost.jsx
@@ -1,40 +1,31 @@
+import Image from 'next/image';
+
 import Container from 'components/shared/container/container';
 import GradientLabel from 'components/shared/gradient-label';
 import Link from 'components/shared/link/link';
 
-import Increase from './images/increase.inline.svg';
+import increase from './images/increase.svg';
 
 const Boost = () => (
   <section className="boost mt-[154px] xl:mt-28 lg:mt-24 md:mt-20">
     <Container className="flex flex-col items-center" size="medium">
       <GradientLabel>Perfomance</GradientLabel>
       <h2 className="mt-3 text-center text-5xl font-medium leading-dense tracking-extra-tight xl:text-[44px] lg:text-4xl md:text-[32px]">
-        Boosting AI performance
+        Boost AI performance
         <br />
         with parallel index building
       </h2>
       <p className="mt-3 max-w-[485px] text-center text-lg font-light leading-snug tracking-normal xl:text-base">
         Experience up to 30x&nbsp;
         {/* TODO: insert href */}
-        <Link
-          className="!text-lg tracking-normal underline-offset-[3px] xl:!text-base"
-          theme="green-underlined"
-          to="/"
-          // target={linkTarget}
-          // rel={linkTarget ? 'noopener noreferrer' : undefined}
-          size="sm"
-        >
+        <Link theme="green-underlined" to="/" size="sm">
           faster index builds
         </Link>
         , ensuring seamless scaling for your AI applications.
       </p>
     </Container>
-    {/* TODO: adaptive mt */}
-    <Container className="mt-[78px] !p-0 xl:mt-[65px]" size="medium">
-      <div className="aspect-[1472/521] w-full overflow-hidden">
-        {/* TODO: optimize svg rendering */}
-        <Increase className="relative aspect-[1472/568] h-auto w-full [shape-rendering:geometricprecision]" />
-      </div>
+    <Container className="mt-2 flex justify-center !p-0" size="medium">
+      <Image className="sm:w-[150%] sm:max-w-none" src={increase} width={1472} height={568} />
     </Container>
   </section>
 );
