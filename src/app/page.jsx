60473cf6808418d0ef3fb465737185799bed08f7
americano98 <mr.nik.bazarov@gmail.com>
2024-03-12 13:57:52 +0700

    feat: add lightning section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Lightning from 'components/pages/home/lightning';
 import Logos from 'components/pages/home/logos';
 import Layout from 'components/shared/layout';
 import SEO_DATA from 'constants/seo-data';
@@ -8,6 +9,7 @@ export const metadata = getMetadata(SEO_DATA.index);
 const Home = () => (
   <Layout className="bg-black" headerTheme="black" footerWithTopBorder withOverflowHidden>
     <Logos />
+    <Lightning />
   </Layout>
 );
 
---
+++ src/components/pages/home/lightning/lightning.jsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+import Container from 'components/shared/container';
+import phoneIllustration from 'images/pages/home/lightning/phone.png';
+
+const Lightning = () => (
+  <section className="lightning safe-paddings mt-[216px] bg-black">
+    <Container className="pb-[311px]" size="960">
+      <h2 className="text-[128px] font-medium leading-[0.95] -tracking-wider text-white">
+        Lightning fast. Edge&nbsp;ready.
+      </h2>
+      <div className="ml-32 mt-[104px] font-medium leading-none text-white">
+        <span className="text-[128px] tracking-[-0.06em]">10</span>
+        <span className="text-[38px] tracking-extra-tight">ms</span>
+      </div>
+      <p className="ml-32 mt-8 max-w-[288px] leading-snug tracking-extra-tight text-gray-new-80">
+        <span className="font-medium text-white">Achieve low-latency</span> from an edge function to
+        your regional compute.
+      </p>
+      <p className="ml-32 mt-5 max-w-[288px] leading-snug tracking-extra-tight text-gray-new-80">
+        The Neon serverless driver, designed for JavaScript and TypeScript, ensures{' '}
+        <span className="font-medium text-white">low-latency Postgres queries</span>. It facilitates
+        seamless data retrieval from both serverless and edge environments, utilizing HTTP.
+      </p>
+      <Image
+        className="absolute -right-9 top-32"
+        src={phoneIllustration}
+        width={482}
+        height={893}
+      />
+    </Container>
+  </section>
+);
+
+export default Lightning;
