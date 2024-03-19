029d675e4bc9fc357b96f4c5fbfca19f0cd074f5
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2024-03-19 15:20:29 +0100

    style: adaptive get-started

--- src/components/pages/home/get-started/get-started.jsx
+++ src/components/pages/home/get-started/get-started.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 import Button from 'components/shared/button';
@@ -5,20 +6,33 @@ import Container from 'components/shared/container';
 import bgImage from 'images/pages/home/get-started/dots.webp';
 
 const GetStarted = () => (
-  <section className="get-started mb-[358px] mt-[445px]">
+  <section
+    className={clsx(
+      'get-started mb-[358px] mt-[445px]',
+      'xl:mb-[230px] xl:mt-[230px] lg:mb-[156px] lg:mt-[179px] md:mb-[110px] md:mt-[116px]'
+    )}
+  >
     <Container className="flex flex-col items-center justify-center" size="1100">
       <Image
-        className="absolute max-w-none"
+        className={clsx(
+          'absolute max-w-none',
+          'xl:max-w-[1426px] lg:max-w-[1073px] lg:-translate-y-2 lg:translate-x-px md:max-w-[872px]'
+        )}
         src={bgImage}
         width={bgImage.width / 2}
         height={bgImage.height / 2}
       />
-      <h2 className="relative text-center text-[68px] font-medium leading-[0.9] -tracking-[0.03em] text-white">
+      <h2
+        className={clsx(
+          'relative text-center text-[68px] font-medium leading-[0.9] -tracking-[0.03em] text-white',
+          'xl:text-[56px] xl:leading-none xl:tracking-extra-tight lg:text-[44px] md:text-[32px]'
+        )}
+      >
         Features of tomorrow.
         <br /> Available today.
       </h2>
       <Button
-        className="relative mt-9 !font-semibold tracking-tighter"
+        className="relative mt-9 !font-semibold tracking-tighter xl:mt-8 lg:mt-7 md:mt-5"
         size="lg"
         theme="green-outline"
         to="#"
