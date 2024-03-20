5a33bab133559a31534ab6a9431b8d4a16d08290
americano98 <mr.nik.bazarov@gmail.com>
2024-03-20 17:24:02 +0700

    chore: lightning section responsive

--- src/components/pages/home/bento/bento.jsx
+++ src/components/pages/home/bento/bento.jsx
@@ -6,25 +6,25 @@ import regionsImage from 'images/pages/home/bento/regions.jpg';
 import timerImage from 'images/pages/home/bento/timer.jpg';
 
 const Bento = () => (
-  <section className="bento safe-paddings mt-16 xl:mt-10">
-    <Container className="xl:max-w-[704px] lg:max-w-full md:max-w-lg" size="960">
-      <h2 className="text-[68px] font-medium leading-[0.9] tracking-extra-tight text-white xl:text-[56px] lg:pl-16 lg:text-[44px] md:pl-0 md:text-[32px]">
+  <section className="bento safe-paddings mt-16 xl:mt-10 lg:mt-9">
+    <Container className="xl:max-w-[704px] lg:max-w-full" size="960">
+      <h2 className="text-[68px] font-medium leading-[0.9] tracking-extra-tight text-white xl:text-[56px] lg:pl-16 lg:text-[44px] sm:pl-0 sm:text-[32px]">
         Better database.
         <br />
         For&nbsp;modern workflows.
       </h2>
       <div className="mt-10 grid grid-cols-2 gap-5 xl:mt-9 xl:gap-4 lg:mt-8 sm:mt-5 sm:gap-x-[18px]">
-        <div className="relative col-span-2 grid min-h-[384px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] xl:min-h-[282px] lg:aspect-[2.5] md:min-h-[250px] md:w-full">
+        <div className="relative col-span-2 grid min-h-[384px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] xl:min-h-[282px] lg:aspect-[2.5] lg:min-h-min sm:min-h-[250px] sm:w-full">
           <div className="relative z-10 col-span-full row-span-full">
             <Image
-              className="absolute inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:h-full md:w-auto md:max-w-none md:-translate-x-1/2 md:-translate-y-1/2"
+              className="absolute inset-0 sm:inset-auto sm:left-1/2 sm:top-1/2 sm:h-full sm:w-auto sm:max-w-none sm:-translate-x-1/2 sm:-translate-y-1/2"
               src={regionsImage}
               width={960}
               height={384}
               alt=""
             />
           </div>
-          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 md:p-4">
+          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 sm:p-4">
             <p className="pointer-events-auto max-w-lg text-lg font-light leading-snug tracking-extra-tight text-white/60 xl:max-w-[412px] xl:text-base sm:leading-tight">
               <strong className="font-medium text-white">
                 Read replicas available in 6 regions.
@@ -37,18 +37,18 @@ const Bento = () => (
             aria-hidden
           />
         </div>
-        <div className="relative grid min-h-[491px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] xl:min-h-[360px] lg:aspect-[0.9572] md:col-span-full md:min-h-[250px]">
+        <div className="relative grid min-h-[491px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] xl:min-h-[360px] lg:aspect-[0.9572] md:min-h-min sm:col-span-full sm:min-h-[250px]">
           <div className="relative z-10 col-span-full row-span-full">
             <Image
-              className="absolute inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:h-full md:w-auto md:max-w-none md:-translate-x-1/2 md:-translate-y-1/2"
+              className="absolute inset-0 sm:inset-auto sm:left-1/2 sm:top-1/2 sm:h-full sm:w-auto sm:max-w-none sm:-translate-x-1/2 sm:-translate-y-1/2"
               src={apiAndCliImage}
               width={470}
               height={491}
               alt=""
             />
           </div>
-          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 md:p-4">
-            <p className="pointer-events-auto text-lg font-light leading-snug tracking-extra-tight text-white/60 xl:text-base md:text-[15px] sm:leading-tight">
+          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 sm:p-4">
+            <p className="pointer-events-auto text-lg font-light leading-snug tracking-extra-tight text-white/60 xl:text-base sm:text-[15px] sm:leading-tight">
               <strong className="font-medium text-white">API and CLI at your disposal.</strong>{' '}
               Manage your database programmatically or from the terminal.
             </p>
@@ -58,18 +58,18 @@ const Bento = () => (
             aria-hidden
           />
         </div>
-        <div className="relative grid min-h-[491px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] bg-home-bento-timer-border xl:min-h-[360px] lg:aspect-[0.9572] md:col-span-full md:min-h-[250px]">
+        <div className="relative grid min-h-[491px] grid-cols-1 grid-rows-1 overflow-hidden rounded-[10px] bg-home-bento-timer-border xl:min-h-[360px] lg:aspect-[0.9572] md:min-h-min sm:col-span-full sm:min-h-[250px]">
           <div className="relative z-10 col-span-full row-span-full">
             <Image
-              className="absolute inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:h-full md:w-auto md:max-w-none md:-translate-x-1/2 md:-translate-y-1/2"
+              className="absolute inset-0 sm:inset-auto sm:left-1/2 sm:top-1/2 sm:h-full sm:w-auto sm:max-w-none sm:-translate-x-1/2 sm:-translate-y-1/2"
               src={timerImage}
               width={470}
               height={491}
               alt=""
             />
           </div>
-          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 md:p-4">
-            <p className="pointer-events-auto text-lg font-light leading-snug tracking-extra-tight text-white/60 xl:text-base md:text-[15px] sm:leading-tight">
+          <div className="pointer-events-none relative z-20 col-span-full row-span-full flex items-end px-6 pb-7 xl:px-5 xl:pb-6 sm:p-4">
+            <p className="pointer-events-auto text-lg font-light leading-snug tracking-extra-tight text-white/60 xl:text-base sm:text-[15px] sm:leading-tight">
               <strong className="font-medium text-white">Point-in-Time Recovery.</strong> Instant,
               up to 30 days, granularity down to the transaction or second.
             </p>
--- src/components/pages/home/lightning/lightning.jsx
+++ src/components/pages/home/lightning/lightning.jsx
@@ -5,29 +5,37 @@ import phoneIllustration from 'images/pages/home/lightning/phone.png';
 
 // TODO: add responsive styles, rive animation, check heading gradient when font will be available
 const Lightning = () => (
-  <section className="lightning safe-paddings mt-[216px]">
-    <Container className="pb-[311px]" size="960">
-      <h2 className="inline-block bg-home-lightning-title bg-clip-text pb-6 text-[128px] font-medium leading-[0.95] -tracking-wider text-transparent">
+  <section className="lightning safe-paddings mt-[216px] xl:mt-32 lg:mt-24 sm:mt-20">
+    <Container
+      className="pb-[311px] xl:max-w-[704px] xl:pb-[156px] lg:max-w-[640px] lg:pb-[60px] md:max-w-none md:pl-[84px] sm:pl-5"
+      size="960"
+    >
+      <h2 className="inline-block bg-home-lightning-title bg-clip-text pb-6 text-[128px] font-medium leading-[0.95] -tracking-wider text-transparent xl:text-[96px] lg:max-w-lg lg:text-[72px] sm:text-[52px]">
         Lightning fast. Edge&nbsp;ready.
       </h2>
-      <div className="ml-32 mt-20 font-medium leading-none text-white">
-        <span className="text-[128px] tracking-[-0.06em]">10</span>
-        <span className="text-[38px] tracking-extra-tight">ms</span>
+      <div className="ml-32 mt-20 font-medium leading-none text-white xl:ml-24 xl:mt-10 lg:ml-8  lg:mt-6 md:ml-0 sm:mt-[5px]">
+        <span className="text-[128px] tracking-[-0.06em] xl:text-[96px] lg:text-[72px] sm:text-6xl">
+          10
+        </span>
+        <span className="text-[38px] tracking-extra-tight xl:text-[28px] lg:text-[21px] sm:text-lg">
+          ms
+        </span>
       </div>
-      <p className="ml-32 mt-8 max-w-[288px] leading-snug tracking-extra-tight text-gray-new-80">
+      <p className="ml-32 mt-8 max-w-[288px] font-light tracking-extra-tight text-gray-new-80 xl:ml-24 xl:mt-6 xl:leading-snug lg:ml-8  lg:mt-5 lg:max-w-[256px] md:ml-0 sm:mt-[9px] sm:max-w-[212px] sm:text-[15px]">
         <span className="font-medium text-white">Achieve low-latency</span> from an edge function to
         your regional compute.
       </p>
-      <p className="ml-32 mt-5 max-w-[288px] leading-snug tracking-extra-tight text-gray-new-80">
+      <p className="ml-32 mt-5 max-w-[288px] font-light tracking-extra-tight text-gray-new-80 xl:ml-24 xl:mt-4 xl:leading-snug lg:ml-8  lg:mt-3 lg:max-w-[256px] md:ml-0 sm:max-w-[212px] sm:text-[15px]">
         The Neon serverless driver, designed for JavaScript and TypeScript, ensures{' '}
         <span className="font-medium text-white">low-latency Postgres queries</span>. It facilitates
         seamless data retrieval from both serverless and edge environments, utilizing HTTP.
       </p>
       <Image
-        className="absolute -right-9 top-32"
+        className="absolute -right-6 top-32 xl:-right-11 xl:top-[84px] xl:max-w-[348px] lg:left-[337px] lg:right-auto lg:top-14 lg:max-w-[271px] sm:left-56 sm:top-6"
         src={phoneIllustration}
-        width={482}
-        height={893}
+        width={464}
+        height={840}
+        alt=""
       />
     </Container>
   </section>
