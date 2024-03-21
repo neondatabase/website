0021570b6e65bb4d030b8aa1567ea0a5f9c1dad5
Ekaterina Martyshevskaia <74831191+katermart@users.noreply.github.com>
2024-03-21 10:22:30 +0100

    style: new color for blog category

--- src/constants/blog.js
+++ src/constants/blog.js
@@ -6,6 +6,7 @@ const CATEGORY_COLORS = {
   engineering: 'text-purple-70',
   community: 'text-yellow-70',
   ai: 'text-blue-80',
+  'case-study': 'text-pink-90',
 };
 
 const CATEGORY_HOVER_COLORS = {
@@ -20,6 +21,7 @@ const CATEGORY_BG_COLORS = {
   engineering: 'bg-purple-70/10',
   community: 'bg-yellow-70/10',
   ai: 'bg-blue-80/10',
+  'case-study': 'bg-pink-90/10',
 };
 
 export {
