7cfd8273dd59588a8dcb40339f4362cf6d1bb9b3
Andrew Golovanov <spacewebdeveloper@gmail.com>
2024-04-05 12:38:53 +0200

    fix: minor changes

--- src/components/pages/home/ai-index/video/video.jsx
+++ src/components/pages/home/ai-index/video/video.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-import ParallaxVocabulary from '../parallax-vocabulary';
+import ParallaxVocabulary from './parallax-vocabulary';
 
 const Video = () => {
   const videoRef = useRef(null);
@@ -23,12 +23,21 @@ const Video = () => {
 
   return (
     <div
-      className="relative z-0 mx-auto -mt-48 aspect-square max-w-[1160px] mix-blend-plus-lighter xl:mt-[-152px] xl:max-w-[860px] lg:mt-[-117px] lg:max-w-[680px] sm:-mt-[52px] sm:max-w-[91%]"
+      className="relative z-0 mx-auto -mt-48 aspect-square max-w-[1160px] xl:mt-[-152px] xl:max-w-[860px] lg:mt-[-117px] lg:max-w-[680px] sm:-mt-[52px] sm:max-w-[91%]"
       ref={ref}
     >
-      <video ref={videoRef} controls={false} width={1160} height={1160} loop playsInline muted>
-        <source src="/videos/pages/home/ai-loop-crf-32.mp4" type="video/mp4" />
-        <source src="/videos/pages/home/ai-loop-crf-40.webm" type="video/webm" />
+      <video
+        className="mix-blend-lighten [filter:brightness(1)_contrast(105%)_saturate(100%)]"
+        controls={false}
+        width={1160}
+        height={1160}
+        ref={videoRef}
+        loop
+        playsInline
+        muted
+      >
+        <source src="/videos/pages/home/ai-loop.mp4" type="video/mp4" />
+        <source src="/videos/pages/home/ai-loop.webm" type="video/webm" />
       </video>
       <ParallaxVocabulary className="absolute left-1/2 top-[15.5%] w-[1280px] -translate-x-1/2 xl:top-8 xl:w-[930px] lg:-top-14 lg:w-[740px] md:top-[48%] md:w-[110%] md:-translate-x-1/2 md:-translate-y-1/2" />
     </div>
