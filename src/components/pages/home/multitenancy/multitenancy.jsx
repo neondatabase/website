becfebef9ea3eef95dd789cdb10b80106066e3c1
Andrew Golovanov <spacewebdeveloper@gmail.com>
2024-04-08 19:08:50 +0200

    feat: add video to next-gen section

--- src/components/pages/home/multitenancy/multitenancy.jsx
+++ src/components/pages/home/multitenancy/multitenancy.jsx
@@ -1,9 +1,5 @@
-import Image from 'next/image';
-
 import Container from 'components/shared/container';
-import neonGridImage from 'images/pages/home/multitenancy/neon-grid.jpg';
 
-// TODO: add video instead of image
 const Multitenancy = () => (
   <section className="multitenancy safe-paddings mt-8 overflow-hidden xl:-mt-9 lg:-mt-8 md:mt-12">
     <Container className="relative z-10 xl:max-w-[704px] lg:pl-24" size="960">
@@ -13,16 +9,21 @@ const Multitenancy = () => (
         for modern teams
       </h2>
     </Container>
-    {/* TODO: replace illustration with video */}
-    <Image
-      className="relative left-1/2 z-0 mx-auto -mt-[91px] max-w-[1920px] -translate-x-1/2 xl:-mt-[60px] xl:max-w-[1380px] lg:-mt-[46px] lg:max-w-[1150px] sm:-mt-8 sm:max-w-[790px]"
-      src={neonGridImage}
+    <video
+      className="relative left-1/2 mt-[54px] max-w-[1920px] -translate-x-1/2 xl:max-w-[1380px] lg:mt-11 lg:max-w-[1150px] md:mt-10 sm:mt-6 sm:max-w-[790px]"
+      height={474}
       width={1920}
-      height={758}
-      alt=""
-    />
+      controls={false}
+      muted
+      playsInline
+      loop
+      autoPlay
+    >
+      <source src="/videos/pages/home/next-gen.mp4" type="video/mp4" />
+      <source src="/videos/pages/home/next-gen.webm" type="video/webm" />
+    </video>
     <Container
-      className="relative z-10 -mt-[84px] xl:-mt-14 xl:max-w-[704px] lg:-mt-11 lg:pl-24 sm:-mt-9"
+      className="relative z-10 mt-14 xl:max-w-[704px] lg:mt-[50px] lg:pl-24 md:mt-11 sm:mt-6"
       size="960"
     >
       <p className="max-w-[608px] text-xl leading-snug tracking-extra-tight text-gray-new-50 xl:max-w-xl xl:text-lg lg:max-w-[480px] lg:text-sm sm:text-base">
