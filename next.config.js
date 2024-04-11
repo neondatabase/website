e32775d67bb1a26399a00b232be1d30a74686895
americano98 <mr.nik.bazarov@gmail.com>
2024-04-11 12:18:46 +0400

    chore: cache control videos

--- next.config.js
+++ next.config.js
@@ -47,6 +47,15 @@ const defaultConfig = {
         ],
       },
       {
+        source: '/videos/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/docs/:all*(svg|jpg|png)',
         headers: [
           {
