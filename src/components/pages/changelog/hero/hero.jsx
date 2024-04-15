f79608843ebc5cc719e7049ab1758eeaa1bd4eae
americano98 <mr.nik.bazarov@gmail.com>
2024-04-15 18:14:15 +0400

    chore: esbuild to inter for headings

--- src/components/pages/changelog/hero/hero.jsx
+++ src/components/pages/changelog/hero/hero.jsx
@@ -20,7 +20,7 @@ const Hero = ({ className = null, withContainer = false }) => {
         })}
         size="sm"
       >
-        <h1 className="font-title text-[36px] font-medium xl:text-3xl">{TITLE}</h1>
+        <h1 className="text-[36px] font-semibold xl:text-3xl">{TITLE}</h1>
         <div className="flex items-center justify-between sm:flex-col sm:items-start sm:gap-y-4">
           <p>{DESCRIPTION}</p>
           {!withContainer && (
--- src/components/pages/doc/community-banner/community-banner.jsx
+++ src/components/pages/doc/community-banner/community-banner.jsx
@@ -16,7 +16,7 @@ const CommunityBanner = ({ buttonText, buttonUrl, children = null, logo }) => (
   <section className="not-prose relative my-10">
     <span className="absolute -inset-px block rounded-[10px] bg-[linear-gradient(90deg,rgba(48,50,54,1)50%,rgba(0,229,153,0.4)100%)]" />
     <span className="absolute -top-px right-1.5 h-px w-[28%] bg-[radial-gradient(circle,rgba(0,229,153,0.7)0%,rgba(0,229,153,0.05)100%)] sm:hidden" />
-    <div className="relative flex items-center overflow-hidden rounded-[10px] bg-[#18191B] p-8 sm:p-6">
+    <div className="relative flex items-center overflow-hidden rounded-[10px] bg-[#0A0A0A] p-8 sm:p-6">
       <div>
         <h2 className="!my-0 max-w-[334px] font-title text-[26px] font-medium leading-dense text-white xs:text-2xl">
           {children}
