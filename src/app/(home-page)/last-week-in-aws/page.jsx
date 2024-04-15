0d61c88da2a9ca614bfc6f0c4f3adc0330daa830
americano98 <mr.nik.bazarov@gmail.com>
2024-04-15 12:11:09 +0400

    chore: rm redudant components

--- src/app/(home-page)/last-week-in-aws/page.jsx
+++ src/app/(home-page)/last-week-in-aws/page.jsx
@@ -1,14 +1,3 @@
-import Advantages from 'components/pages/home/advantages';
-import CTA from 'components/pages/home/cta';
-import DataBranching from 'components/pages/home/data-branching';
-import Hero from 'components/pages/home/hero';
-import NeonCli from 'components/pages/home/neon-cli';
-import Partnership from 'components/pages/home/partnership';
-import SaaS from 'components/pages/home/saas';
-import Scalability from 'components/pages/home/scalability';
-import Storage from 'components/pages/home/storage';
-import Layout from 'components/shared/layout';
-import Subscribe from 'components/shared/subscribe';
 import SEO_DATA from 'constants/seo-data';
 import getMetadata from 'utils/get-metadata';
 
@@ -17,20 +6,7 @@ export const metadata = getMetadata({
   robotsNoindex: 'noindex',
 });
 
-const LastWeekInAWS = () => (
-  <Layout headerTheme="black-pure" withOverflowHidden>
-    <Hero />
-    <CTA />
-    <Advantages />
-    <Scalability />
-    <Storage />
-    <DataBranching />
-    <NeonCli />
-    <SaaS />
-    <Partnership />
-    <Subscribe />
-  </Layout>
-);
+const LastWeekInAWS = () => null;
 
 export default LastWeekInAWS;
 
