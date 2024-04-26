9217a7a18e29a7ee0785c10b810adbec61d9e85d
americano98 <mr.nik.bazarov@gmail.com>
2024-04-26 21:03:27 +0400

    chore: video upd

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -37,8 +37,8 @@ const ITEMS = [
     video: {
       icon: branchingIcon,
       title: 'Branching',
-      mp4: '/videos/pages/home/hero/branching.mp4?updated=20240426193505',
-      m3u8: '/videos/pages/home/hero/branching.m3u8?updated=20240426193505',
+      mp4: '/videos/pages/home/hero/branching.mp4?updated=20240426210205',
+      m3u8: '/videos/pages/home/hero/branching.m3u8?updated=20240426210205',
     },
     title: 'Branching',
     description:
