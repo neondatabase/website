5b8844ec8c8bb9d30630c8503199393aa61b0ae3
Andy Hattemer <andy@neon.tech>
2024-05-03 11:45:36 -0400

    chore: fix getcookie

--- src/utils/get-cookie.js
+++ src/utils/get-cookie.js
@@ -1,8 +1,10 @@
 export default function getCookie(name) {
-  const cookieValue = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith(`${name}=`))
-    ?.split('=')[1];
+  const cookieValue =
+    document &&
+    document.cookie
+      .split('; ')
+      .find((row) => row.startsWith(`${name}=`))
+      ?.split('=')[1];
 
   return cookieValue;
 }
