45ce02a7960f4307855c1fb783d14c0aa6846942
americano98 <mr.nik.bazarov@gmail.com>
2024-05-08 18:41:28 +0300

    chore: add overlay to border

--- src/components/pages/home/instant-provisioning/instant-provisioning.jsx
+++ src/components/pages/home/instant-provisioning/instant-provisioning.jsx
@@ -20,7 +20,7 @@ const BlurWrapper = ({ children, className }) => (
       aria-hidden
     />
     <div
-      className="absolute inset-[5px] z-10 rounded-[10px] border border-white/[0.04]"
+      className="absolute inset-[5px] z-10 rounded-[10px] border border-white/[0.04] mix-blend-overlay"
       aria-hidden
     />
     {children}
