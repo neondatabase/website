36e5bf70bd1d587cca852ac91a6fe089e08a516c
Simon Kataev <saimon.kat23@gmail.com>
2024-05-17 12:34:42 +0200

    feat: update Get started docs cards styles

--- content/docs/introduction.md
+++ content/docs/introduction.md
@@ -9,15 +9,15 @@ Neon is a fully managed serverless Postgres with a [generous free tier](https://
 
 ## Get started
 
-<DetailIconCards>
+<DetailIconCards withBorderWrapper>
 
-<a href="/docs/get-started-with-neon/signing-up" description="Sign up for free and learn the basics of database branching with Neon" icon="enable">Day 1 - Playing with Neon</a>
+<a href="/docs/get-started-with-neon/signing-up" description="Sign up for free and learn the basics of database branching with Neon" icon="user"><span>Day 1.</span> Playing with Neon</a>
 
-<a href="/docs/get-started-with-neon/connect-neon" description="Connect Neon to the platform, language, ORM and other tools in your tech stack" icon="queries">Day 2 - Connect Neon to your stack</a>
+<a href="/docs/get-started-with-neon/connect-neon" description="Connect Neon to the platform, language, ORM and other tools in your tech stack" icon="audio-jack"><span>Day 2.</span> Connect Neon to your stack</a>
 
-<a href="/docs/get-started-with-neon/workflow-primer" description="Add branching to your CI/CD automation" icon="setup">Day 3 - Branching workflows</a>
+<a href="/docs/get-started-with-neon/workflow-primer" description="Add branching to your CI/CD automation" icon="split-branch"><span>Day 3.</span> Branching workflows</a>
 
-<a href="/docs/get-started-with-neon/production-checklist" description="Key features to get you production ready" icon="cli">Day 4 - Get ready for production</a>
+<a href="/docs/get-started-with-neon/production-checklist" description="Key features to get you production ready" icon="laptop"><span>Day 4.</span> Get ready for production</a>
 
 </DetailIconCards>
 
--- src/components/pages/doc/detail-icon-cards/detail-icon-cards.jsx
+++ src/components/pages/doc/detail-icon-cards/detail-icon-cards.jsx
@@ -1,7 +1,9 @@
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import Link from 'components/shared/link/link';
+import ArrowRightIcon from 'icons/arrow-right.inline.svg';
 
 import AChart from './images/a-chart.inline.svg';
 import AppStore from './images/app-store.inline.svg';
@@ -40,6 +42,7 @@ import Hourglass from './images/hourglass.inline.svg';
 import Import from './images/import.inline.svg';
 import Invert from './images/invert.inline.svg';
 import Ladder from './images/ladder.inline.svg';
+import Laptop from './images/laptop.inline.svg';
 import LockLandscape from './images/lock-landscape.inline.svg';
 import Metrics from './images/metrics.inline.svg';
 import Neon from './images/neon.inline.svg';
@@ -67,6 +70,7 @@ import Todo from './images/todo.inline.svg';
 import Transactions from './images/transactions.inline.svg';
 import TrendUp from './images/trend-up.inline.svg';
 import Unlock from './images/unlocked.inline.svg';
+import User from './images/user.inline.svg';
 import Vercel from './images/vercel.inline.svg';
 import Wallet from './images/wallet.inline.svg';
 import Warning from './images/warning.inline.svg';
@@ -75,78 +79,87 @@ import X from './images/x.inline.svg';
 
 const icons = {
   'a-chart': AChart,
-  'audio-jack': AudioJack,
+  'app-store': AppStore,
   atom: Atom,
+  'audio-jack': AudioJack,
+  autoscaling: Autoscaling,
   aws: AWS,
   'binary-code': BinaryCode,
+  branching: Branching,
   bug: Bug,
-  cheque: Cheque,
+  'calendar-day': CalendarDay,
+  cards: Cards,
   check: Check,
+  cheque: Cheque,
+  'chart-bar': ChartBar,
+  cli: CLI,
+  'cli-cursor': CliCursor,
   code: Code,
   csv: CSV,
-  'calendar-day': CalendarDay,
   data: Data,
   database: Database,
   discord: Discord,
   download: Download,
-  stopwatch: Stopwatch,
-  gamepad: Gamepad,
-  gui: GUI,
-  import: Import,
-  ladder: Ladder,
-  'lock-landscape': LockLandscape,
-  table: RowTable,
-  'split-branch': SplitBranch,
-  sql: SQL,
-  'app-store': AppStore,
-  cards: Cards,
-  'cli-cursor': CliCursor,
-  transactions: Transactions,
-  github: Github,
-  openai: OpenAI,
-  todo: Todo,
-  'trend-up': TrendUp,
-  autoscaling: Autoscaling,
-  branching: Branching,
-  cli: CLI,
   enable: Enable,
-  'find-replace': FindReplace,
   filter: Filter,
-  vercel: Vercel,
+  'find-replace': FindReplace,
+  gamepad: Gamepad,
   gear: Gear,
+  github: Github,
   globe: Globe,
+  gui: GUI,
   handshake: Handshake,
-  hourglass: Hourglass,
-  hook: Hook,
   heroku: Heroku,
+  hook: Hook,
+  hourglass: Hourglass,
+  import: Import,
+  invert: Invert,
+  ladder: Ladder,
+  laptop: Laptop,
+  'lock-landscape': LockLandscape,
   metrics: Metrics,
+  neon: Neon,
   network: Network,
+  openai: OpenAI,
   perfomance: Perfomance,
-  puzzle: Puzzle,
+  postgres: Postgres,
   prisma: Prisma,
   privacy: Privacy,
-  postgres: Postgres,
-  research: Research,
-  neon: Neon,
-  invert: Invert,
+  puzzle: Puzzle,
   queries: Queries,
   refresh: Refresh,
-  setup: Setup,
-  sparkle: Sparkle,
-  search: SearchContent,
-  'scale-up': ScaleUp,
+  research: Research,
   'respond-arrow': RespondArrow,
-  'chart-bar': ChartBar,
+  'scale-up': ScaleUp,
   screen: Screen,
-  x: X,
+  search: SearchContent,
+  setup: Setup,
+  'split-branch': SplitBranch,
+  sparkle: Sparkle,
+  sql: SQL,
+  stopwatch: Stopwatch,
+  table: RowTable,
+  todo: Todo,
+  transactions: Transactions,
+  'trend-up': TrendUp,
   unlock: Unlock,
-  wrench: Wrench,
-  warning: Warning,
+  user: User,
+  vercel: Vercel,
   wallet: Wallet,
+  warning: Warning,
+  wrench: Wrench,
+  x: X,
 };
 
-const DetailIconCards = ({ children = null }) => (
-  <ul className="not-prose !my-10 grid grid-cols-2 gap-4 !p-0 sm:grid-cols-1">
+const DetailIconCards = ({ children = null, withBorderWrapper = false }) => (
+  <ul
+    className={clsx(
+      'not-prose !my-10 grid grid-cols-2 sm:grid-cols-1',
+      withBorderWrapper
+        ? 'relative gap-6 rounded-[10px] border border-gray-new-94 !p-6 duration-200 dark:border-gray-new-20 sm:!p-3'
+        : 'gap-5 !p-0'
+    )}
+  >
     {React.Children.map(children, (child, index) => {
       const { children, href, description, icon } = child.props ?? {};
       const Icon = icons[icon];
@@ -154,17 +167,47 @@ const DetailIconCards = ({ children = null }) => (
       return (
         <li className="!m-0 flex before:hidden" key={index}>
           <Link
-            className="relative flex w-full items-start gap-x-3.5 rounded-[10px] border border-gray-new-94 px-6 py-5 transition-colors duration-200 before:absolute before:inset-0 before:rounded-[10px] before:bg-[linear-gradient(275.74deg,#FAFAFA_0%,rgba(250,250,250,0)100%)] before:opacity-0 before:transition-opacity before:duration-200 hover:border-gray-new-80 hover:before:opacity-100 dark:border-gray-new-20 dark:before:bg-[linear-gradient(275.74deg,rgba(36,38,40,0.8)_0%,rgba(36,38,40,0)_100%)] dark:hover:border-gray-new-30 sm:p-3"
+            className={clsx(
+              'relative flex w-full items-start',
+              withBorderWrapper
+                ? 'group gap-x-3'
+                : 'gap-x-3.5 rounded-[10px] border border-gray-new-94 px-6 py-5 transition-colors duration-200 before:absolute before:inset-0 before:rounded-[10px] before:bg-[linear-gradient(275.74deg,#FAFAFA_0%,rgba(250,250,250,0)100%)] before:opacity-0 before:transition-opacity before:duration-200 hover:border-gray-new-80 hover:before:opacity-100 dark:border-gray-new-20 dark:before:bg-[linear-gradient(275.74deg,rgba(36,38,40,0.8)_0%,rgba(36,38,40,0)_100%)] dark:hover:border-gray-new-30 sm:p-3'
+            )}
             to={href}
           >
-            <Icon className="relative z-10 mt-0.5 h-4 w-4 shrink-0 text-secondary-8 dark:text-green-45" />
-            <div className="relative z-10 flex flex-col gap-x-2.5">
-              <h3 className="text-lg font-semibold leading-tight text-black-new dark:text-white">
+            <div
+              className={clsx(
+                'relative z-10 shrink-0 ',
+                withBorderWrapper
+                  ? 'h-6 w-6 rounded border border-gray-new-94 bg-[linear-gradient(180deg,#FFF,#FAFAFA)] p-[5px] dark:border-white/10 dark:bg-[linear-gradient(180deg,#242628_31.25%,#1D1E20_100%)]'
+                  : 'mt-0.5 h-4 w-4 text-secondary-8 dark:text-green-45'
+              )}
+            >
+              <Icon className="h-full w-full" />
+            </div>
+            <div
+              className={clsx(
+                'relative z-10 flex flex-col gap-x-2.5',
+                withBorderWrapper && 'pt-0.5'
+              )}
+            >
+              <h3 className="text-lg font-semibold leading-tight text-black-new dark:text-white [&_span]:text-gray-new-30 dark:[&_span]:text-gray-new-80">
                 {children}
               </h3>
-              <p className="mt-2.5 text-sm leading-normal text-gray-new-50 dark:text-gray-new-80">
+              <p
+                className={clsx(
+                  'text-sm leading-normal text-gray-new-50 dark:text-gray-new-80',
+                  withBorderWrapper ? 'mt-1 max-w-[280px] sm:max-w-full' : 'mt-2.5'
+                )}
+              >
                 {description}
               </p>
+              {withBorderWrapper && (
+                <span className="mt-1.5 inline-flex items-center text-sm font-medium text-secondary-8 transition-colors duration-200 group-hover:text-secondary-7 dark:text-green-45 dark:group-hover:text-[#00FFAA] 2xl:text-sm lg:text-base sm:text-sm">
+                  <span>Learn more</span>
+                  <ArrowRightIcon className="ml-1.5" />
+                </span>
+              )}
             </div>
           </Link>
         </li>
@@ -175,6 +218,7 @@ const DetailIconCards = ({ children = null }) => (
 
 DetailIconCards.propTypes = {
   children: PropTypes.node,
+  withBorderWrapper: PropTypes.bool,
 };
 
 export default DetailIconCards;
