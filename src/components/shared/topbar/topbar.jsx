f955d3bd352950e22a14fe2975d721765fb7594c
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-06-12 11:13:34 -0700

    Update topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-40 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/blog/how-to-create-previews-with-anonymized-production-like-data-in-seconds"
+    to="https://neon.tech/demos/instant-postgres/"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,7 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-      Your previews will never be the same: Create database branches with anonymized PII in seconds.
+      In Neon, you can provision Postgres databases in <1s. Check out this demo
     </span>
     <ChevronIcon
       className={clsx(
