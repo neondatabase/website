4d85bd10b2fa2834670094282738903a9e8c18f3
americano98 <mr.nik.bazarov@gmail.com>
2024-07-19 13:43:45 +0300

    chore: rm old about us page components

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,34 +1,14 @@
 import { notFound } from 'next/navigation';
 
-import CTA from 'components/pages/about/cta';
-import Team from 'components/pages/about/team';
 import Layout from 'components/shared/layout';
 import { getAboutPage } from 'utils/api-pages';
 import getMetadata from 'utils/get-metadata';
-import getReactContentWithLazyBlocks from 'utils/get-react-content-with-lazy-blocks';
 
-export const viewport = {
-  themeColor: '#ffffff',
-};
-
-const AboutUsPage = async () => {
-  const page = await getAboutPage();
-
-  if (!page) return notFound();
-
-  const { content } = page;
-
-  const contentWithLazyBlocks = getReactContentWithLazyBlocks(
-    content,
-    {
-      aboutusteam: Team,
-      aboutuscta: CTA,
-    },
-    true
-  );
-
-  return <Layout>{contentWithLazyBlocks}</Layout>;
-};
+const AboutUsPage = () => (
+  <Layout>
+    <h1>About Us</h1>
+  </Layout>
+);
 
 export async function generateMetadata() {
   const page = await getAboutPage();
