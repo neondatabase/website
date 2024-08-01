85d24f4949397ea137e53bc82787d19249116bea
Simon Kataev <saimon.kat23@gmail.com>
2024-08-01 14:54:29 +0200

    chore: remove extra br from variable page

--- src/components/pages/variable/hero/hero.jsx
+++ src/components/pages/variable/hero/hero.jsx
@@ -2,10 +2,10 @@ const Hero = () => (
   <section className="hero safe-paddings relative overflow-hidden">
     <h1 className="text-[56px] font-semibold leading-dense tracking-tighter xl:text-5xl lg:text-4xl md:text-[28px] md:leading-tight">
       Postgres for variable traffic
-      <br /> 
     </h1>
     <p className="mt-4 text-2xl leading-snug tracking-extra-tight text-gray-new-80 xl:text-xl md:mt-3 md:text-lg">
-      Neon dynamically scales according to load. Get optimal performance during traffic peaks without overprovisioning.
+      Neon dynamically scales according to load. Get optimal performance during traffic peaks
+      without overprovisioning.
     </p>
   </section>
 );
