00aec2e7f7cecb267a2ff3ac9487e9ab94100df7
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-08-02 11:27:50 -0700

    Update workflow.jsx

--- src/components/pages/enterprise/benefits/workflow/workflow.jsx
+++ src/components/pages/enterprise/benefits/workflow/workflow.jsx
@@ -9,7 +9,7 @@ const Workflow = () => (
   <div className="workflow grid w-full grid-cols-10 items-center gap-x-10 xl:gap-x-6 lg:gap-y-7 md:gap-y-6">
     <div className="order-1 col-span-5 ml-10 justify-self-start 2xl:ml-8 lg:order-none lg:col-span-full lg:ml-0 lg:justify-self-stretch lg:text-center">
       <h3 className="text-4xl font-medium leading-tight tracking-tighter xl:text-[32px] lg:text-center lg:text-[28px] md:text-[22px]">
-        Get engineering time back
+        Move your dev environments to Neon
       </h3>
       <p className="mt-2.5 max-w-[590px] text-lg font-light leading-snug xl:text-base lg:mx-auto lg:max-w-[584px] md:mt-2">
         Use Neon's git-like branching to bring the database into CI/CD workflows, making{' '}
@@ -32,7 +32,7 @@ const Workflow = () => (
         className="group mt-6 inline-flex items-center rounded-[50px] bg-gray-new-15 bg-opacity-80 py-2.5 pl-4 pr-2.5 leading-tight tracking-extra-tight transition-colors duration-200 hover:bg-gray-new-20 sm:flex-1 xs:w-full"
         to="https://neon.tech/blog/development-environments-for-aws-rds-using-neon-postgres"
       >
-        <span>Move dev to Neon</span>
+        <span>See benefits vs RDS</span>
         <ArrowIcon className="ml-6 text-gray-new-70 transition-colors duration-200 group-hover:text-white sm:ml-auto" />
       </Link>
     </div>
