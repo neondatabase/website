c15db0d507c28c56e9991741b8e52263593cbddb
Saimon Kataev <saimon.kat23@gmail.com>
2024-08-05 11:49:57 +0300

    chore: homepage hero videos posters

--- src/components/pages/home/hero/video/video.jsx
+++ src/components/pages/home/hero/video/video.jsx
@@ -70,7 +70,7 @@ const Video = forwardRef(
           >
             <video
               className={clsx(
-                'pointer-events-none absolute left-0 top-0 z-0 h-auto min-w-[704px] 2xl:min-w-[652px] xl:min-w-[608px] lg:min-w-[514px] md:min-w-[480px] sm:!left-0 sm:h-auto sm:min-w-0 sm:!translate-x-0',
+                'pointer-events-none absolute left-0 top-0 z-0 h-auto min-w-[704px] 2xl:min-w-[652px] xl:min-w-[608px] lg:min-w-[514px] md:min-w-[480px] sm:!left-0 sm:h-auto sm:min-w-0 sm:translate-x-0',
                 videoClassName,
                 !isActive && 'transition-all delay-700 duration-700',
                 isActive && '!left-0 lg:!left-1/2 lg:-translate-x-1/2'
