c60d868ad7c884553751a40d024395a6bca80a93
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-08-05 18:47:43 -0700

    Update example.jsx

--- src/components/pages/cost-fleets/example/example.jsx
+++ src/components/pages/cost-fleets/example/example.jsx
@@ -10,19 +10,18 @@ const Example = () => (
   <div className="relative mb-10 mt-11 w-full overflow-hidden rounded-lg bg-[#0D0E10] px-8 py-6 xl:mb-9 xl:mt-10 lg:mb-9 lg:mt-9 md:mb-6 md:mt-7 sm:p-6">
     <div className="relative z-10 pb-[18px]">
       <h3 className="mb-5 text-2xl font-medium leading-snug tracking-tighter xl:text-xl sm:mb-4 sm:text-lg">
-        Use case description: dev platform
+        Costs estimator
       </h3>
       <div className="space-y-4 tracking-extra-tight text-gray-new-70">
         <p>
-          Pied Piper, a rapidly growing developer platform, is enhancing its user experience by soon
-          allowing end users to create Postgres databases directly within the platform.
-          Their&nbsp;goal is to provide every customer with their own isolated instance.
+          Pied Piper, a developer platform, wants to give one managed Postgres database to each one of their end users.
+          
         </p>
         <p>
           Pied Piper.&apos;s users fall into two categories:{' '}
           <strong className="font-normal text-white">companies with Pro accounts</strong>, storing
           an average of 20 GB, and{' '}
-          <strong className="font-normal text-white">hobbyists on the free tier</strong>, storing an
+          <strong className="font-normal text-white">hobbyists on the Free plan</strong>, storing an
           average of 0.5 GB.
         </p>
       </div>
