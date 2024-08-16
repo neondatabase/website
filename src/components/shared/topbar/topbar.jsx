5eec49870113e5617769adeaeee8aaede8e3d1e5
Andy Hattemer <andy@neon.tech>
2024-08-15 21:56:55 -0400

    Add Neon Twin Pt 2 Callout in banner (#2038)
    * Update topbar.jsx
    
    * Update src/components/shared/topbar/topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/blog/neon-is-coming-to-azure"
+    to="https://neon.tech/blog/neon-twin-deploy-workflow"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,7 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-      Neon is coming to Azure. Join the waitlist
+      Neon Twin: Dev/preview/test on Neon, ship to prod on RDS - Read how here
     </span>
     <ChevronIcon
       className={clsx(
