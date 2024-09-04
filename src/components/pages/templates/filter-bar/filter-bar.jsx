4cb40afb64f0061cf68bc5150ae3fc1f9d39ec75
Thi Van Le <vannnyle@gmail.com>
2024-09-04 14:52:14 +0200

    chore: add missing key

--- src/components/pages/templates/filter-bar/filter-bar.jsx
+++ src/components/pages/templates/filter-bar/filter-bar.jsx
@@ -53,14 +53,14 @@ const FilterBar = () => (
     />
     <ul className="mt-8 flex flex-col divide-y divide-gray-new-15/80">
       {filters.map(({ type, items }) => (
-        <li className="flex flex-col pb-6 pt-6 first:pt-0 last:pb-0">
+        <li className="flex flex-col pb-6 pt-6 first:pt-0 last:pb-0" key={type}>
           <fieldset>
             <legend className="block font-semibold leading-tight tracking-extra-tight">
               {type}
             </legend>
             <div className="mt mt-4 flex flex-col gap-y-3">
               {items.map(({ name, value }) => (
-                <label className="flex items-center gap-2.5 dark:text-gray-new-90">
+                <label className="flex items-center gap-2.5 dark:text-gray-new-90" key={value}>
                   <input
                     className="h-4 w-4 appearance-none rounded-sm border bg-center bg-no-repeat transition-colors duration-100 checked:bg-[url('/images/templates/check-dark-mode.svg')] dark:border-gray-new-30 dark:checked:border-white dark:checked:bg-white"
                     type="checkbox"
