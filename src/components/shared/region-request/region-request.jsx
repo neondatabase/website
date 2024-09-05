2ee42fc2692ec363b604cd6223511e05b9cfae33
Thi Van Le <vannnyle@gmail.com>
2024-09-05 13:37:50 +0200

    chore: minor fix

--- src/components/shared/region-request/region-request.jsx
+++ src/components/shared/region-request/region-request.jsx
@@ -172,6 +172,7 @@ const DEFAULT_DATA = {
 };
 
 function getCookie(name) {
+  if (typeof document === 'undefined') return null;
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
