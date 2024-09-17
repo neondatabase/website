70e340e0da2f8bc484ac8ea0df6aa0eb9260f872
Simon Kataev <saimon.kat23@gmail.com>
2024-09-17 16:19:47 +0200

    feat: update docs home intro cta

--- src/components/shared/doc-cta/doc-cta.jsx
+++ src/components/shared/doc-cta/doc-cta.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 import LINKS from 'constants/links';
@@ -17,17 +18,39 @@ const DocCta = ({
   description = DEFAULT_DATA.description,
   buttonText = DEFAULT_DATA.buttonText,
   buttonUrl = DEFAULT_DATA.buttonUrl,
+  noButton = false,
 }) => (
-  <figure className="doc-cta not-prose my-5 flex items-end gap-x-16 rounded-[10px] border border-gray-new-90 bg-[linear-gradient(to_right,#FAFAFA_0%,rgba(250,250,250,0)100%)] px-7 py-6 dark:border-gray-new-20 dark:bg-[linear-gradient(to_right,#18191B_28.86%,#131415_74.18%)] md:flex-col md:items-start sm:p-6">
+  <figure
+    className={clsx(
+      'doc-cta not-prose my-12 flex items-end gap-x-16 rounded-[10px] px-6 py-5',
+      'border border-gray-new-90 bg-[linear-gradient(to_right,#FAFAFA_0%,rgba(250,250,250,0)100%)]',
+      'dark:border-gray-new-15 dark:bg-[linear-gradient(to_right,#18191B_28.86%,#131415_74.18%)]',
+      'md:flex-col md:items-start'
+    )}
+  >
     <div>
-      <h2 className="!my-0 font-title text-2xl font-medium leading-dense">{title}</h2>
-      <p className="mt-2 text-sm font-light text-gray-new-20 dark:text-gray-new-80">
-        {description}
-      </p>
+      <h2 className="!my-0 font-title text-xl font-semibold leading-tight tracking-extra-tight">
+        {title}
+      </h2>
+      <p
+        className={clsx(
+          'mt-2 tracking-extra-tight text-gray-new-20 dark:text-gray-new-80',
+          '[&_a]:border-b [&_a]:border-transparent [&_a]:text-secondary-8 [&_a]:no-underline',
+          '[&_a]:transition-[border-color] [&_a]:duration-200 [&_a]:ease-in-out hover:[&_a]:border-secondary-8',
+          'dark:[&_a]:text-primary-1 dark:hover:[&_a]:border-primary-1'
+        )}
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
     </div>
-    <Button className="px-6 py-3 font-semibold leading-none md:mt-4" to={buttonUrl} theme="primary">
-      {buttonText}
-    </Button>
+    {!noButton && (
+      <Button
+        className="px-6 py-3 font-semibold leading-none md:mt-4"
+        to={buttonUrl}
+        theme="primary"
+      >
+        {buttonText}
+      </Button>
+    )}
   </figure>
 );
 
@@ -36,6 +59,7 @@ DocCta.propTypes = {
   description: PropTypes.node,
   buttonText: PropTypes.string,
   buttonUrl: PropTypes.string,
+  noButton: PropTypes.bool,
 };
 
 export default DocCta;
