a6f2412bcebd283332d6cc4107dde61c95fe3a68
Simon Kataev <saimon.kat23@gmail.com>
2024-09-17 16:35:10 +0200

    refactor: docs home intro cta attributes

--- src/components/shared/doc-cta/doc-cta.jsx
+++ src/components/shared/doc-cta/doc-cta.jsx
@@ -18,23 +18,33 @@ const DocCta = ({
   description = DEFAULT_DATA.description,
   buttonText = DEFAULT_DATA.buttonText,
   buttonUrl = DEFAULT_DATA.buttonUrl,
-  noButton = false,
+  isIntro = false,
 }) => (
   <figure
     className={clsx(
-      'doc-cta not-prose my-12 flex items-end gap-x-16 rounded-[10px] px-6 py-5',
+      'doc-cta not-prose rounded-[10px]',
+      isIntro
+        ? 'my-12 px-6 py-5 md:my-8'
+        : 'my-5 flex items-end gap-x-16 px-7 py-6 md:flex-col md:items-start',
       'border border-gray-new-90 bg-[linear-gradient(to_right,#FAFAFA_0%,rgba(250,250,250,0)100%)]',
-      'dark:border-gray-new-15 dark:bg-[linear-gradient(to_right,#18191B_28.86%,#131415_74.18%)]',
-      'md:flex-col md:items-start'
+      'dark:border-gray-new-20 dark:bg-[linear-gradient(to_right,#18191B_28.86%,#131415_74.18%)]'
     )}
   >
     <div>
-      <h2 className="!my-0 font-title text-xl font-semibold leading-tight tracking-extra-tight">
+      <h2
+        className={clsx(
+          '!my-0',
+          isIntro
+            ? 'text-xl font-semibold leading-tight tracking-extra-tight'
+            : 'font-title text-2xl font-medium leading-dense'
+        )}
+      >
         {title}
       </h2>
       <p
         className={clsx(
-          'mt-2 tracking-extra-tight text-gray-new-20 dark:text-gray-new-80',
+          'mt-2 text-gray-new-20 dark:text-gray-new-80',
+          isIntro ? 'tracking-extra-tight' : 'text-sm font-light',
           '[&_a]:border-b [&_a]:border-transparent [&_a]:text-secondary-8 [&_a]:no-underline',
           '[&_a]:transition-[border-color] [&_a]:duration-200 [&_a]:ease-in-out hover:[&_a]:border-secondary-8',
           'dark:[&_a]:text-primary-1 dark:hover:[&_a]:border-primary-1'
@@ -42,7 +52,7 @@ const DocCta = ({
         dangerouslySetInnerHTML={{ __html: description }}
       />
     </div>
-    {!noButton && (
+    {!isIntro && (
       <Button
         className="px-6 py-3 font-semibold leading-none md:mt-4"
         to={buttonUrl}
@@ -59,7 +69,7 @@ DocCta.propTypes = {
   description: PropTypes.node,
   buttonText: PropTypes.string,
   buttonUrl: PropTypes.string,
-  noButton: PropTypes.bool,
+  isIntro: PropTypes.bool,
 };
 
 export default DocCta;
