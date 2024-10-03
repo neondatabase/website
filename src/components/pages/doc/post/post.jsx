27e8197d6f6385459ab3aa95ad23ee28eaa4c8af
Van Le Thi <48465000+vannyle@users.noreply.github.com>
2024-10-03 10:06:58 +0200

    feat: add use case Dev test (#2211)
    * feat: add dev-test page
    
    * chore: minor update
    
    * feat: add testimonial
    
    * feat: menu item, text color
    
    * feat: set color white for strong
    
    * chore: update content
    
    * feat: implement carousel in use case post
    
    * chore: missing link
    
    * refactor: separate function handleOnDragEnd
    
    * fix: content updates, enable showing subitem in ToC
    
    * chore: decrease text size
    
    * Update dev-test.md
    
    * Update dev-test.md
    
    * chore: format content markdown files with Prettier
    
    * Update dev-test.md
    
    ---------
    
    Co-authored-by: carlota☀️ <82476502+carlotas19@users.noreply.github.com>
    Co-authored-by: GitHub Action <action@github.com>

--- src/components/pages/doc/post/post.jsx
+++ src/components/pages/doc/post/post.jsx
@@ -70,7 +70,7 @@ const Post = ({
           <h1
             className={clsx(
               isUseCase
-                ? 'mt-12 text-[56px] font-semibold leading-dense tracking-tighter xl:text-5xl lg:text-4xl md:text-[28px] md:leading-tight'
+                ? 'text-[56px] font-semibold leading-dense tracking-tighter xl:text-5xl lg:text-4xl md:text-[28px] md:leading-tight'
                 : 'font-title text-[36px] font-medium leading-tight tracking-tighter xl:text-3xl',
               tag && 'inline'
             )}
@@ -106,7 +106,7 @@ const Post = ({
       )}
     >
       <nav className="no-scrollbars sticky bottom-10 top-[104px] max-h-[calc(100vh-80px)] overflow-y-auto overflow-x-hidden">
-        {enableTableOfContents && <TableOfContents items={tableOfContents} />}
+        {enableTableOfContents && <TableOfContents items={tableOfContents} isUseCase={isUseCase} />}
         <div
           className={clsx(
             enableTableOfContents &&
