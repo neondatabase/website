4e156b75875c5e2a9e1dbe916bf16b0116ff1e34
Thi Van Le <vannnyle@gmail.com>
2024-10-04 14:31:08 +0200

    chore: update topbar link

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import PropTypes from 'prop-types';
 
 import Link from 'components/shared/link';
+import LINKS from 'constants/links';
 import ChevronIcon from 'icons/chevron-down.inline.svg';
 import sendGtagEvent from 'utils/send-gtag-event';
 
@@ -18,7 +19,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/deploy"
+    to={LINKS.deploy}
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
