1ce611c1b2e41232d0f3f6560f1ad92a2e29c93b
bgrenon <barry_grenon@yahoo.ca>
2024-10-20 10:29:49 -0300

    docs: stashed for 10-25 changelog

---
+++ content/changelog/2024-10-25.md.m
@@ -0,0 +1,26 @@
+---
+description: The latest product updates from Neon
+---
+
+### TBD
+
+<details>
+<summary>**Fixes & improvements**</summary>
+
+
+- **Contact support form improvements**:
+  - You can now attach files along with your problem description when requesting help from Neon support. File size limit is 50 MB and we support the following file types:
+    - PDF (. pdf)
+    - PNG (. png)
+    - JPEG (. jpeg)
+    - GIF (. gif)
+    - Text (. txt)
+
+- **SQL Editor AI features are now open to everyone**:
+
+- **Neon Console enhancements**:
+
+- **Neon API change**:
+  - Added `state_changed_at` field for branches. When getting branch details, the response now returns a state_changed_at field, showing the timestamp (UTC) when the branch's `current_state` first started.
+
+</details>
