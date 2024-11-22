6a793554399f4af66a70283e6ad32669378b4b0d
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-11-22 11:57:50 -0800

    Update hero.jsx

--- src/components/pages/variable/hero/hero.jsx
+++ src/components/pages/variable/hero/hero.jsx
@@ -1,10 +1,10 @@
 const Hero = () => (
   <section className="hero safe-paddings relative overflow-hidden">
     <h1 className="text-[56px] font-semibold leading-dense tracking-tighter xl:text-5xl lg:text-4xl md:text-[28px] md:leading-tight">
-      Autoscaling For Variable Traffic
+      Scale With Traffic
     </h1>
     <p className="mt-4 text-2xl leading-snug tracking-extra-tight text-gray-new-80 xl:text-xl md:mt-3 md:text-lg">
-      Neon scales up and down automatically, managing traffic spikes without overspending.    
+      Neon scales up and down automatically, matching your workload.    
     </p>
   </section>
 );
