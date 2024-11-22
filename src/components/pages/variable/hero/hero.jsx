a65bdb8004b214e9e6f933e50a2dbbd713a4a315
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-11-22 11:35:26 -0800

    Update hero.jsx

--- src/components/pages/variable/hero/hero.jsx
+++ src/components/pages/variable/hero/hero.jsx
@@ -1,11 +1,10 @@
 const Hero = () => (
   <section className="hero safe-paddings relative overflow-hidden">
     <h1 className="text-[56px] font-semibold leading-dense tracking-tighter xl:text-5xl lg:text-4xl md:text-[28px] md:leading-tight">
-      Postgres for variable traffic
+      Autoscaling For Variable Traffic
     </h1>
     <p className="mt-4 text-2xl leading-snug tracking-extra-tight text-gray-new-80 xl:text-xl md:mt-3 md:text-lg">
-      Neon dynamically scales according to load. Get optimal performance during traffic peaks
-      without overprovisioning.
+      Neon scales up and down automatically, managing traffic spikes without overspending.    
     </p>
   </section>
 );
