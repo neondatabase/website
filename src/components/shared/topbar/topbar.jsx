6be6da65869e4174b55472b1a233e1db6d1f3aa5
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-12-03 09:57:45 -0800

    Update topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/blog/track-schema-changes-automatically-in-your-pull-requests"
+    to="https://neon.tech/blog/neon-is-now-available-as-an-azure-native-integration"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,8 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-      Use Neon branches for schema migrations: Test with production-like data and automatically
-      track schema changes in your PRs
+      Neon is now available as an Azure Native Integration: You can deploy Neon databases directly from the Azure Portal
     </span>
     <ChevronIcon
       className={clsx(
