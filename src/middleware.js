5bba2de0a9a650167591b43ac0afb03e84edf617
Simon Kataev <saimon.kat23@gmail.com>
2024-12-04 17:45:52 +0100

    refactor: middleware homepage redirects

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable no-console */
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
@@ -10,7 +11,8 @@ const SITE_URL =
     ? `https://${process.env.VERCEL_BRANCH_URL}`
     : process.env.NEXT_PUBLIC_DEFAULT_SITE_URL;
 
-const protectedRoutes = ['/generate-ticket', '/tickets'];
+const homeProtectedRoutes = ['/', '/home'];
+const ticketsProtectedRoutes = ['/generate-ticket', '/tickets'];
 
 const extractHandleFromPath = (pathname) => pathname.split('/').slice(-2)[0];
 
@@ -29,21 +31,36 @@ export async function middleware(req) {
       return NextResponse.next();
     }
 
-    // Check if user is logged in
-    const isLoggedIn = await checkCookie('neon_login_indicator');
-    if (process.env.NODE_ENV === 'production' && isLoggedIn) {
-      const referer = await getReferer();
-      if (
-        referer.includes(process.env.VERCEL_BRANCH_URL) ||
-        referer.includes(process.env.NEXT_PUBLIC_DEFAULT_SITE_URL)
-      ) {
-        return NextResponse.redirect('/home');
-      }
+    // Check for home protected routes
+    if (homeProtectedRoutes.includes(pathname)) {
+      try {
+        const isLoggedIn = await checkCookie('neon_login_indicator');
 
-      return NextResponse.redirect(LINKS.console);
+        if (pathname === '/' && isLoggedIn) {
+          try {
+            const referer = await getReferer();
+            if (
+              referer.includes(process.env.VERCEL_BRANCH_URL) ||
+              referer.includes(process.env.NEXT_PUBLIC_DEFAULT_SITE_URL)
+            ) {
+              return NextResponse.redirect(new URL(`${SITE_URL}/home`));
+            }
+          } catch (error) {
+            console.error('Error getting referer:', error);
+          }
+          return NextResponse.redirect(new URL(LINKS.console));
+        }
+
+        if (pathname === '/home' && !isLoggedIn) {
+          return NextResponse.redirect(new URL(SITE_URL));
+        }
+      } catch (error) {
+        console.error('Error checking login indicator:', error);
+      }
     }
 
-    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+    // Check for tickets protected routes
+    if (ticketsProtectedRoutes.some((route) => pathname.startsWith(route))) {
       try {
         const token = await getToken({ req });
         const isAuthenticated = !!token?.githubHandle;
