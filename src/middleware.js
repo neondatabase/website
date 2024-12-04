79c3f4cc300dc969ff835b314ff92682d7f39a0c
Simon Kataev <saimon.kat23@gmail.com>
2024-12-04 12:18:22 +0100

    refactor: move checkCookie redirects from homepage to middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,9 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
+import { checkCookie, getReferer } from 'app/actions';
+import LINKS from 'constants/links';
+
 const SITE_URL =
   process.env.VERCEL_ENV === 'preview'
     ? `https://${process.env.VERCEL_BRANCH_URL}`
@@ -9,7 +12,7 @@ const SITE_URL =
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
-  // Add any assets or page that you don't want to run middleware on
+
   if (
     pathname.startsWith('/_next/') ||
     pathname.startsWith('/favicon.ico') ||
@@ -19,13 +22,23 @@ export async function middleware(req) {
   }
 
   const token = await getToken({ req });
-  // if token exists, user is authorized
+  const isLoggedIn = await checkCookie('neon_login_indicator', req);
+  const referer = await getReferer(req);
+
+  if (process.env.NODE_ENV === 'production' && isLoggedIn) {
+    if (
+      referer.includes(process.env.VERCEL_BRANCH_URL) ||
+      referer.includes(process.env.NEXT_PUBLIC_DEFAULT_SITE_URL)
+    ) {
+      return NextResponse.redirect(new URL(`${SITE_URL}/home`));
+    }
+    return NextResponse.redirect(new URL(LINKS.console));
+  }
+
   if (token?.githubHandle) {
-    // authorized user should be moved to his ticket edit page from anywhere
     if (pathname === '/generate-ticket' || pathname.endsWith(`/tickets/${token.githubHandle}`)) {
       return NextResponse.redirect(new URL(`${SITE_URL}/tickets/${token.githubHandle}/edit`));
     }
-    // if user is authorized but tries to access another user's edit page, redirect to /tickets/:handle
     if (pathname.endsWith(`/edit`) && token?.githubHandle !== pathname.split('/').slice(-2)[0]) {
       return NextResponse.redirect(
         new URL(`${SITE_URL}${pathname.split('/').slice(0, -1).join('/')}`)
