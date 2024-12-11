0577990f4003f5d208438142c732a228454987ad
Rishi Raj Jain <rishi@neon.tech>
2024-12-11 21:07:52 +0530

    fix: redirect from script

--- src/app/(home-page)/layout.jsx
+++ src/app/(home-page)/layout.jsx
@@ -1,12 +1,17 @@
+import Script from 'next/script';
+
 import Layout from 'components/shared/layout';
 
 // eslint-disable-next-line react/prop-types
 const HomeLayout = ({ children }) => (
-  <Layout isHeaderSticky isHeaderStickyOverlay withOverflowHidden headerWithBorder>
-    {children}
-  </Layout>
+  <>
+    <Script id="redirect_script" strategy="beforeInteractive">
+      {`if (document.cookie.includes('neon_login_indicator')) window.location.href = 'https://console.neon.tech';`}
+    </Script>
+    <Layout isHeaderSticky isHeaderStickyOverlay withOverflowHidden headerWithBorder>
+      {children}
+    </Layout>
+  </>
 );
 
 export default HomeLayout;
-
-export const revalidate = 60;
