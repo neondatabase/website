d24c944e761e98d2427b21dbfb56bd987573a0dc
Rishi Raj Jain <jain71000@gmail.com>
2024-12-13 18:03:01 +0530

    Update layout.jsx

--- src/app/(home-page)/layout.jsx
+++ src/app/(home-page)/layout.jsx
@@ -4,14 +4,9 @@ import Layout from 'components/shared/layout';
 
 // eslint-disable-next-line react/prop-types
 const HomeLayout = ({ children }) => (
-  <>
-    <Script id="redirect_script" strategy="beforeInteractive">
-      {`if (document.cookie.includes('neon_login_indicator')) window.location.href = 'https://console.neon.tech';`}
-    </Script>
-    <Layout isHeaderSticky isHeaderStickyOverlay withOverflowHidden headerWithBorder>
-      {children}
-    </Layout>
-  </>
+  <Layout isHeaderSticky isHeaderStickyOverlay withOverflowHidden headerWithBorder>
+    {children}
+  </Layout>
 );
 
 export default HomeLayout;
