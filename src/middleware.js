c05fb0096f4a1f66eadce4700f9e765a1d5462d7
Rishi Raj Jain <jain71000@gmail.com>
2024-12-16 15:58:33 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -20,26 +20,26 @@ const generateEditPageURL = (handle) => `${SITE_URL}/tickets/${handle}/edit`;
 export async function middleware(req) {
   try {
     const { pathname } = req.nextUrl;
-
-     try {	
-      const isLoggedIn = await checkCookie('neon_login_indicator');	
-      if (pathname === '/' && isLoggedIn) {	
-        try {	
-          const referer = await getReferer();	
-          if (	
-            referer.includes(process.env.VERCEL_BRANCH_URL) ||	
-            referer.includes(process.env.NEXT_PUBLIC_DEFAULT_SITE_URL)	
-          ) {	
-            return NextResponse.redirect(new URL('/home', req.url));	
-          }	
-        } catch (error) {	
-          console.error('Error getting referer:', error);	
-        }	
-        return NextResponse.redirect(LINKS.console);	
-      }	
-      if (pathname === '/home' && !isLoggedIn) return NextResponse.redirect(new URL(SITE_URL));	
-    } catch (error) {	
-      console.error('Error checking login indicator:', error);	
+    
+    try {
+      const isLoggedIn = await checkCookie('neon_login_indicator');
+      if (pathname === '/' && isLoggedIn) {
+        try {
+          const referer = await getReferer();
+          if (
+            referer.includes(process.env.VERCEL_BRANCH_URL) ||
+            referer.includes(process.env.NEXT_PUBLIC_DEFAULT_SITE_URL)
+          ) {
+            return NextResponse.redirect(new URL('/home', req.url));
+          }
+        } catch (error) {
+          console.error('Error getting referer:', error);
+        }
+        return NextResponse.redirect(LINKS.console);
+      }
+      if (pathname === '/home' && !isLoggedIn) return NextResponse.redirect(new URL(SITE_URL));
+    } catch (error) {
+      console.error('Error checking login indicator:', error);
     }
 
     // Check for tickets protected routes
