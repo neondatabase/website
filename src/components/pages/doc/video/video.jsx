99e6dd665a0fe915e2f2a9faa4226510dc0655c8
Saimon Kataev <saimon.kat23@gmail.com>
2024-12-17 13:25:24 +0100

    fix: video border radius

--- src/components/pages/doc/video/video.jsx
+++ src/components/pages/doc/video/video.jsx
@@ -8,7 +8,7 @@ const Video = ({ name, width, height }) => (
     mp4: -pix_fmt yuv420p -vf scale={width}:-2 -movflags faststart -vcodec libx264 -crf 20
     webm: -c:v libvpx-vp9 -crf 20 -vf scale={width}:-2 -deadline best -an
   */
-  <PauseableVideo videoClassName="!rounded-none" width={width} height={height}>
+  <PauseableVideo width={width} height={height}>
     <source src={`/videos/pages/doc/${name}.mp4`} type="video/mp4" />
     <source src={`/videos/pages/doc/${name}.webm`} type="video/webm" />
   </PauseableVideo>
