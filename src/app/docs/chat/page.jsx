ad3f565cb5b495cbf4d771d9726d8cde6ff78679
Saimon Kataev <saimon.kat23@gmail.com>
2024-12-18 17:14:21 +0100

    fix: return inkeep chat to docs chat page

--- src/app/docs/chat/page.jsx
+++ src/app/docs/chat/page.jsx
@@ -1,4 +1,4 @@
-// import InkeepEmbedded from 'components/shared/inkeep-embedded';
+import InkeepEmbedded from 'components/shared/inkeep-embedded';
 import SEO_DATA from 'constants/seo-data';
 import getMetadata from 'utils/get-metadata';
 
@@ -7,7 +7,9 @@ export const metadata = getMetadata(SEO_DATA.docsChat);
 const DocsChat = () => (
   <div className="col-span-12 flex h-[calc(100vh-215px)] w-full items-center justify-center">
     <h1 className="sr-only">Neon AI Chat</h1>
-    <div className="w-[640px]">{/* <InkeepEmbedded /> */}</div>
+    <div className="w-[640px]">
+      <InkeepEmbedded />
+    </div>
   </div>
 );
 
