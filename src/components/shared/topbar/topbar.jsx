20afbbff267db391e2fa89c3c46677bb72601f0d
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2024-12-27 09:30:49 -0800

    Update topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/blog/three-signs-its-time-to-move-away-from-rds"
+    to="https://neon.tech/blog/hyperdrive-neon-faq"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,7 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-     RDS can create serious bottlenecks in engineering productivity — is this you? See how Neon can help
+        If you're using Cloudflare Workers, combine Hyperdrive and Neon for 10x query speed – Learn more    
     </span>
     <ChevronIcon
       className={clsx(
