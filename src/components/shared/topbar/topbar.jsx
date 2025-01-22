ea38eec438033d497097ca247fcdb0f99a1266b2
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2025-01-21 16:45:57 -0800

    Update topbar.jsx

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/blog/api-track-schema-changes"
+    to="https://neon.tech/rds"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,7 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-      Use the compare_schema API to monitor database schema changes in CI/CD pipelines and agentic systems
+      Switch from AWS RDS to Neon for simplified environment management, seamless scaling, and reduced maintenance — Compare both tools
     </span>
     <ChevronIcon
       className={clsx(
