36d9133192539c32c9a1d3420f5c3bce086c2afc
Saimon Kataev <saimon.kat23@gmail.com>
2025-01-27 16:01:36 +0100

    fix: blog sidebar position

--- src/app/blog/(index)/layout.jsx
+++ src/app/blog/(index)/layout.jsx
@@ -10,7 +10,7 @@ const BlogPageLayout = async ({ children }) => {
   return (
     <div className="safe-paddings py-16 lg:py-12 sm:py-10">
       <Container className="flex flex-col" size="1344">
-        <div className="juctify-center mb-16 flex flex-col text-center lg:mb-12 md:mb-8">
+        <div className="mb-16 flex flex-col justify-center text-center lg:mb-12 md:mb-8">
           <h1 className="font-title text-[68px] font-medium leading-none tracking-extra-tight xl:text-[56px] lg:text-5xl md:text-4xl">
             What's new?
           </h1>
@@ -18,7 +18,7 @@ const BlogPageLayout = async ({ children }) => {
             Discover our newest features, product updates, and technical improvements.
           </p>
         </div>
-        <div className="flex gap-24 xl:gap-6 lg:flex-col lg:gap-0">
+        <div className="flex gap-8 pl-16 xl:gap-6 xl:pl-0 lg:flex-col lg:gap-0">
           <Sidebar categories={categories} />
           <div className="relative max-w-3xl lg:max-w-full">{children}</div>
         </div>
