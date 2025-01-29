0db1e1f27500fc06d43ee88aeb2fc6455ee93519
Saimon Kataev <saimon.kat23@gmail.com>
2025-01-29 14:13:58 +0100

    refactor: rename extra categories postSlug to basePath

--- src/components/pages/blog/blog-post-card/blog-post-card.jsx
+++ src/components/pages/blog/blog-post-card/blog-post-card.jsx
@@ -39,7 +39,7 @@ const BlogPostCard = ({
   const link = (() => {
     if (category) {
       const extraCategory = EXTRA_CATEGORIES.find((cat) => cat.slug === category);
-      if (extraCategory) return `/${extraCategory.postSlug}/${slug}`;
+      if (extraCategory) return `${extraCategory.basePath}${slug}`;
     }
 
     return `${LINKS.blog}/${slug}`;
