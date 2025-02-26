965688f17869799f39faa8b0f1b90052291ad7b4
Saimon Kataev <saimon.kat23@gmail.com>
2025-02-26 13:24:23 +0100

    fix: guides algolia index name

--- src/app/guides/(index)/layout.jsx
+++ src/app/guides/(index)/layout.jsx
@@ -18,7 +18,7 @@ const GuidesPageLayout = async ({ children }) => {
           className="grid w-full flex-1 grid-cols-12 gap-x-10 pb-20 pt-16 xl:gap-x-7 lg:block lg:gap-x-5 lg:pt-11 md:pt-10 sm:pt-8"
           size="1344"
         >
-          <AlgoliaSearch indexName={process.env.NEXT_PUBLIC_ALGOLIA_BLOG_INDEX_NAME}>
+          <AlgoliaSearch indexName={process.env.NEXT_PUBLIC_ALGOLIA_GUIDES_INDEX_NAME}>
             <Sidebar className="col-span-2 col-start-2 mt-[88px] xl:col-span-3 xl:col-start-1 lg:top-16 lg:col-span-full lg:mx-auto lg:mb-8 lg:mt-0 lg:max-w-[750px]" />
             <div className="col-span-7 col-start-4 flex flex-col 2xl:col-span-7 2xl:mx-5 xl:col-span-9 xl:ml-11 xl:mr-0 xl:max-w-[750px] lg:mx-auto lg:pt-0">
               <BlogHeader
