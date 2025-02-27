64d93b92fe7303c41f64938c0d8916cee20e8a90
Saimon Kataev <saimon.kat23@gmail.com>
2025-02-27 15:03:40 +0100

    test: border for video

--- src/components/pages/serverless-apps/autoscaling/autoscaling.jsx
+++ src/components/pages/serverless-apps/autoscaling/autoscaling.jsx
@@ -1,8 +1,9 @@
 import Container from 'components/shared/container/container';
+import GradientBorder from 'components/shared/gradient-border/index';
 import PauseableVideo from 'components/shared/pauseable-video';
 
 const Autoscaling = () => (
-  <section className="autoscaling safe-paddings pt-40 xl:pt-[136px] lg:pt-[104px] md:pt-20">
+  <section className="autoscaling safe-paddings pt-40 xl:pt-[136px] lg:pt-[108px] md:pt-20">
     <Container className="flex flex-col items-center" size="960">
       <header className="flex w-full justify-between lg:gap-28 sm:flex-col sm:items-center sm:gap-4 sm:text-center">
         <h2 className="max-w-md font-title text-[48px] font-medium leading-[0.9] tracking-tighter xl:text-[44px] xl:leading-none lg:flex-1 lg:text-4xl sm:max-w-full sm:text-[32px]">
@@ -14,7 +15,7 @@ const Autoscaling = () => (
           to provision for peak — while ensuring good performance.
         </p>
       </header>
-      <div className="relative mt-10 w-full">
+      <div className="relative mt-10 w-full overflow-hidden rounded-[10px] bg-[#0A0A0A]">
         {/* 
           Recommended video omtimization parameters:
           mp4: -pix_fmt yuv420p -vf scale=1920:-2 -movflags faststart -vcodec libx264 -crf 20
@@ -24,11 +25,12 @@ const Autoscaling = () => (
           <source src="/videos/pages/serverless-apps/autoscaling.mp4" type="video/mp4" />
           <source src="/videos/pages/serverless-apps/autoscaling.webm" type="video/webm" />
         </PauseableVideo>
-        <p className="absolute bottom-0 left-0 max-w-[784px] p-6 font-light tracking-extra-tight text-gray-new-80 lg:relative lg:mt-4 lg:p-0">
+        <p className="absolute bottom-0 left-0 max-w-[784px] p-6 font-light tracking-extra-tight text-gray-new-80 lg:relative lg:max-w-[605px] md:p-5">
           Neon integrates a pooler built on PgBouncer directly into its architecture: every endpoint
           has connection pooling enabled. Connections scale automatically with traffic up to 10,000
           concurrently.
         </p>
+        <GradientBorder className="hidden lg:block" withBlend />
       </div>
     </Container>
   </section>
