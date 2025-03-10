772baf46d5cb8a6af521ec4da4015f2f1d9bb6e5
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2025-03-10 10:08:16 -1000

    Update topbar.jsx (#3125)

--- src/components/shared/topbar/topbar.jsx
+++ src/components/shared/topbar/topbar.jsx
@@ -18,7 +18,7 @@ const TopBar = ({ isDarkTheme }) => (
       'safe-paddings relative z-50 flex h-9 w-full items-center justify-center gap-x-2.5 overflow-hidden px-4 py-2.5 leading-none transition-colors duration-200 dark:bg-[#0B0C0F] dark:hover:bg-gray-new-8',
       isDarkTheme ? 'bg-[#0B0C0F] hover:bg-gray-new-8' : 'bg-[#F5FBFD] hover:bg-[#f1fcff]'
     )}
-    to="https://neon.tech/startups"
+    to="https://neon.tech/blog/outage-simulator"
     onClick={() => {
       sendGtagEvent('click_announcement_banner');
     }}
@@ -70,7 +70,7 @@ const TopBar = ({ isDarkTheme }) => (
         isDarkTheme ? 'text-gray-new-90' : 'text-gray-new-15'
       )}
     >
-      Are you enrolled in a startup accelerator? Apply for our Startup Deal
+      Run our Outage Simulator — take down a 1TB Neon DB and restore it in milliseconds with Instant Restore
     </span>
     <ChevronIcon
       className={clsx(
