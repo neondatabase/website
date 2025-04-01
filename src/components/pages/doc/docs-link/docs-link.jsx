0c41f18ca017aa61fc4a58b315a763712c8f0871
Saimon Kataev <saimon.kat23@gmail.com>
2025-04-01 17:53:33 +0200

    fix: docs link inline format (#3259)

--- src/components/pages/doc/docs-link/docs-link.jsx
+++ src/components/pages/doc/docs-link/docs-link.jsx
@@ -33,7 +33,6 @@ const DocsLink = ({ href, children, ...otherProps }) => {
 
   return (
     <Link
-      className="!block text-pretty [&_svg]:ml-2 [&_svg]:inline-flex"
       to={href}
       target={isExternal ? '_blank' : undefined}
       rel={isExternal ? 'noopener noreferrer' : undefined}
