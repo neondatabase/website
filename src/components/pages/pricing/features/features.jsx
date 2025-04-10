1d781cd1d2f48b7a4a53ffa976ac57d7840885b6
Saimon Kataev <saimon.kat23@gmail.com>
2025-04-10 15:35:20 +0200

    feat: features section and faq moved to shared section

---
+++ src/components/pages/pricing/features/features.jsx
@@ -0,0 +1,77 @@
+import Container from 'components/shared/container/container';
+import Link from 'components/shared/link';
+import LINKS from 'constants/links';
+
+const DATA = [
+  {
+    title: 'High availability',
+    description: 'Storage uses multi-AZ to minimize disruptions without HA standbys.',
+    link: `${LINKS.docs}/introduction/high-availability`,
+  },
+  {
+    title: 'Read replicas',
+    description: 'Offload read-heavy workloads to replicas, avoiding storage costs.',
+    link: `${LINKS.blog}/neon-read-replicas-in-the-wild-how-beatgig-uses-them`,
+  },
+  {
+    title: 'Autoscaling',
+    description: 'Neon automatically adjusts compute and storage as needed.',
+    link: LINKS.autoscaling,
+  },
+  {
+    title: 'Monitoring',
+    description: 'Monitor resource usage in real time with optimization dashboards.',
+    link: `${LINKS.docs}/introduction/monitoring-page`,
+  },
+  {
+    title: 'Connection pooling',
+    description: 'Built on pgBouncer, ready to scale to 10,000 connections.',
+    link: LINKS.connectionPooling,
+  },
+  {
+    title: 'Postgres extensions library',
+    description: 'Extend with pg_search, PostGIS, TimescaleDB, etc.',
+    link: `${LINKS.docs}/extensions/pg-extensions`,
+  },
+  {
+    title: 'Neon Auth',
+    description: 'Simple setup, built right into the platform — no added overhead.',
+    link: LINKS.auth,
+  },
+  {
+    title: 'GPDR and ISO compliance',
+    description: 'Visit our Security page for more information.',
+    link: LINKS.security,
+  },
+];
+
+const Features = () => (
+  <section className="features mt-[184px] scroll-mt-5 px-safe xl:mt-40 lg:mt-36 md:mt-[104px]">
+    <Container size="1152" className="xl:px-16">
+      <h2 className="text-balance text-center font-title text-[52px] font-medium leading-none tracking-extra-tight xl:text-5xl lg:text-4xl md:text-[32px]">
+        Features in all Neon databases
+      </h2>
+      <ul className="mx-auto mt-14 grid grid-cols-4 justify-center gap-x-10 gap-y-11 xl:grid-cols-3 lg:mt-12 lg:max-w-[640px] lg:grid-cols-2 lg:gap-10 sm:mt-10 xs:grid-cols-1">
+        {DATA.map(({ title, description, link }) => (
+          <li className="sm:max-w-[260px]" key={title}>
+            <span className="pricing-check-icon block size-6 scale-150 bg-green-45" aria-hidden />
+            <h3 className="mt-2.5 text-xl font-medium leading-snug tracking-extra-tight">
+              {title}
+            </h3>
+            <p className="mt-2 leading-snug tracking-extra-tight text-gray-new-70">{description}</p>
+            <Link
+              href={link}
+              className="mt-4 text-[15px] leading-none tracking-[-0.03em]"
+              theme="white"
+              withArrow
+            >
+              Read more
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  </section>
+);
+
+export default Features;
