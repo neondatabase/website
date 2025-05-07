ebc6f70ac16a5cf74a87a8b0359dac81fb5df2cf
Saimon Kataev <saimon.kat23@gmail.com>
2025-05-07 14:20:26 +0200

    fix: remove console log

--- src/utils/send-gtag-event.js
+++ src/utils/send-gtag-event.js
@@ -1,5 +1,4 @@
 export default function sendGtagEvent(eventName, properties) {
-  console.log('sendGtagEvent', eventName, properties);
   if (window.zaraz) {
     window.zaraz.track(eventName, properties);
   }
