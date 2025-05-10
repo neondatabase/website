116de8b100629882d1cac55e084ffce2657b1d83
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2025-05-09 17:53:34 -0700

    Update task.jsx

--- src/components/pages/migration/task/task.jsx
+++ src/components/pages/migration/task/task.jsx
@@ -10,15 +10,14 @@ const Task = () => (
       <div className="flex items-start gap-16 lg:justify-start lg:gap-16 sm:flex-col sm:gap-8">
         <div className="flex-1 xl:mt-1.5 lg:mt-0 sm:w-full">
           <p className="mb-4 text-base font-medium uppercase tracking-wide text-gray-new-50 xl:mb-3 lg:mb-4 lg:text-sm md:mb-3  md:text-[13px]">
-            Your only task
+            import data assistant
           </p>
           <h2 className="font-title text-5xl font-medium leading-none tracking-tighter xl:text-[44px] lg:text-[40px] md:text-[32px]">
-            Enter connection URL for database
+            1-step migration tool
           </h2>
           <div className="mt-3 text-pretty text-lg leading-snug tracking-extra-tight text-gray-new-70 xl:mt-[14px] lg:text-[15px] md:mt-3 md:text-base md:font-light">
             <p>
-              The only thing you have to do is click on Import database in the Neon Console and
-              provide the connection string for your existing Postgres database.
+              Our Import Data Assistant can help you automatically copy your existing database to Neon. You only need to provide a connection string to get started.
             </p>
           </div>
         </div>
