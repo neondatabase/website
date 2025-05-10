001621b24892b5b336ae6fca865ebd7d8ab504ae
carlota☀️ <82476502+carlotas19@users.noreply.github.com>
2025-05-09 17:58:04 -0700

    Update tasks-steps.jsx

--- src/components/pages/migration/tasks-steps/tasks-steps.jsx
+++ src/components/pages/migration/tasks-steps/tasks-steps.jsx
@@ -10,7 +10,7 @@ const ITEMS = [
   {
     title: 'Run checks',
     description:
-      'The Import Data Assistant checks Postgres version, region, and extensions, and prompts adjustments.',
+      'The Import Data Assistant will check your Postgres version, region, and extensions, and prompt adjustments.',
     image: {
       src: stepOne,
       alt: 'Run checks',
@@ -21,7 +21,7 @@ const ITEMS = [
   {
     title: 'Create a project',
     description:
-      'The Import Data Assistant generates a Neon project that matches your source database environment.',
+      'Then, the Assistant will generate a Neon project that matches your source database environment.',
     image: {
       src: stepTwo,
       alt: 'Create a project',
@@ -32,7 +32,7 @@ const ITEMS = [
   {
     title: 'Transfer data',
     description:
-      'Assistant generates pre-populated <code>pg_dump</code> and <code>pg_restore</code> commands and verify your migration.',
+      'The last step is to generates pre-populated <code>pg_dump</code> and <code>pg_restore</code> commands and verify your migration.',
     image: {
       src: stepThree,
       alt: 'Transfer data',
@@ -51,10 +51,10 @@ const TasksSteps = () => (
     <Container className="relative lg:mx-8 md:mx-1" size="768">
       <header className="max-w-[616px]">
         <p className="mb-4 text-base font-medium uppercase tracking-wide text-gray-new-50 lg:text-sm md:mb-3 md:text-[13px]">
-          Tasks for Assistant
+          Postgres migrations, automated
         </p>
         <h2 className="font-title text-5xl font-medium leading-none tracking-tighter xl:text-balance xl:text-[44px] lg:text-[40px] md:text-pretty md:text-[32px]">
-          The Import Data Assistant takes care of data transfer
+          No manual steps, no stress
         </h2>
       </header>
     </Container>
