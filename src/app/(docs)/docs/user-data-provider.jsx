4210818e9ec8f9ba946d5b546ff22fb2ed0d4b70
Krzysztof Szafrański <krzysztof@neon.tech>
2025-05-20 18:11:35 +0200

    feat: project_id and org_id selectors

--- src/app/(docs)/docs/user-data-provider.jsx
+++ src/app/(docs)/docs/user-data-provider.jsx
@@ -5,19 +5,25 @@ import { createContext, useContext, useEffect, useRef, useState } from 'react';
 
 const UserDataContext = createContext({
   loggedIn: false,
+  selection: null,
+  setSelection: () => {},
+  data: {},
 });
 
 export const UserDataProvider = ({ children }) => {
   const iframeRef = useRef(null);
-  const [userData, setUserData] = useState({
-    loggedIn: false,
+
+  const [userData, setUserData] = useState({});
+  const [selection, setSelection] = useState({
+    org_id: null,
+    project_id: null,
   });
 
   useEffect(() => {
     const handleEvent = (event) => {
       const { data, origin } = event;
       if (origin === 'https://console.neon.tech' || origin === 'http://localhost:30000') {
-        setUserData({ loggedIn: true, ...data });
+        setUserData(data);
       } else {
         console.log('ignoring message from untrusted origin', origin);
       }
@@ -27,15 +33,32 @@ export const UserDataProvider = ({ children }) => {
     return () => window.removeEventListener('message', handleEvent);
   }, []);
 
+  const getDataUrl = () => {
+    const params = new URLSearchParams();
+    if (selection.org_id) {
+      params.append('org_id', selection.org_id);
+    }
+    if (selection.project_id) {
+      params.append('project_id', selection.project_id);
+    }
+    return `http://localhost:30000/docs_data?${params.toString()}`;
+  };
+
   return (
-    <UserDataContext.Provider value={userData}>
+    <UserDataContext.Provider
+      value={{
+        loggedIn: !!userData.id,
+        selection,
+        setSelection,
+        data: userData,
+      }}
+    >
       <iframe
         ref={iframeRef}
-        src="http://localhost:30000/docs_data"
+        src={getDataUrl()}
         style={{ display: 'none' }}
         hidden
         onLoad={() => {
-          console.log('sending message!');
           iframeRef.current.contentWindow.postMessage('user-data-request', '*');
         }}
       />
--- src/components/pages/doc/user-data-example/user-data-example.jsx
+++ src/components/pages/doc/user-data-example/user-data-example.jsx
@@ -3,9 +3,53 @@
 import { useUserData } from '../../../../app/(docs)/docs/user-data-provider';
 
 const UserDataExample = () => {
-  const userData = useUserData();
+  const {
+    loggedIn,
+    setSelection,
+    data: { selected_org_id, selected_project_id, orgs, org_projects, connection_uri },
+  } = useUserData();
 
-  return <span id="user-data">{JSON.stringify(userData)}</span>;
+  if (!loggedIn) {
+    return <span>Please log in</span>;
+  }
+
+  return (
+    <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
+      <select
+        value={selected_org_id}
+        onChange={(e) =>
+          setSelection((prev) => ({
+            ...prev,
+            org_id: e.target.value,
+          }))
+        }
+      >
+        {orgs.map((org) => (
+          <option key={org.id} value={org.id}>
+            {org.name}
+          </option>
+        ))}
+      </select>
+
+      <select
+        value={selected_project_id}
+        onChange={(e) =>
+          setSelection((prev) => ({
+            ...prev,
+            project_id: e.target.value,
+          }))
+        }
+      >
+        {org_projects.map((project) => (
+          <option key={project.id} value={project.id}>
+            {project.name}
+          </option>
+        ))}
+      </select>
+
+      <span>Use this connection string: {connection_uri}</span>
+    </div>
+  );
 };
 
 export default UserDataExample;
