86b9d04a15d22b5f325fff5d0596546e1bbe17e5
Vladislav Burlak <vburlak@users.noreply.github.com>
2025-05-22 19:48:09 +0300

    refactor(components): add proper glass effect to branching-demo component

--- src/components/pages/multi-tb/branching/branching.jsx
+++ src/components/pages/multi-tb/branching/branching.jsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
+
 import Container from 'components/shared/container';
+import GradientBorder from 'components/shared/gradient-border';
 import Link from 'components/shared/link';
 import LINKS from 'constants/links';
+import branchingDemoMobile from 'images/pages/multi-tb/branching-demo/branching-demo-mobile.jpg';
+import glass from 'images/pages/multi-tb/branching-demo/glass.jpg';
 
 const Branching = () => (
   <section className="branching mt-[168px] lg:mt-24 md:mt-[68px]">
@@ -22,17 +27,34 @@ const Branching = () => (
       </Link>
     </Container>
     <Container
-      className="mb-14 mt-[50px] lg:mx-0 lg:mb-12 lg:mt-11 md:mx-0 md:mb-[32px] md:mt-[30px]"
+      className="mb-14 mt-[50px] lg:mx-0 lg:mb-12 lg:mt-11 md:mx-0 md:mb-[32px] md:mt-[30px] md:flex md:items-center"
       size="960"
     >
-      <div className="relative min-h-[486px] rounded-[10px] bg-black-new lg:min-h-[412px] md:min-h-[290px]">
+      <div className="relative min-h-[486px] rounded-[10px] bg-[#0A0A0B] lg:min-h-[412px] md:mx-auto md:min-h-min md:w-auto">
+        <Image
+          className="hidden md:block"
+          src={branchingDemoMobile}
+          alt=""
+          width={480}
+          height={435}
+          placeholder="blur"
+        />
         {/* TODO: Embed Brancing Demo component */}
         <div
-          className="branching-demo-glass absolute -left-[8px] -top-[8px] -z-20 h-[calc(100%+16px)] w-[calc(100%+16px)] overflow-hidden rounded-[14px]"
+          className="branching-demo-glass absolute -left-[8px] -top-[8px] -z-20 h-[calc(100%+16px)] w-[calc(100%+16px)] overflow-hidden rounded-[14px] md:hidden"
           aria-hidden
         >
-          <div className="absolute inset-0 bg-black-pure blur" />
+          <Image
+            className="absolute inset-0 h-full w-full object-cover"
+            src={glass}
+            alt=""
+            width={976}
+            height={502}
+            placeholder="blur"
+          />
+          <GradientBorder withBlend />
         </div>
+        <GradientBorder withBlend />
         <span className="absolute right-[-241px] top-[-224px] -z-30 hidden size-[600px] bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(92,129,182,0.24)_0%,_rgba(92,129,182,0.00)_100%)] opacity-30" />
         <span className="absolute bottom-[-193px] left-[-218px] -z-30 hidden size-[550px] bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(7,125,148,0.25)_0%,_rgba(7,125,148,0.00)_100%)] opacity-30" />
       </div>
