455d27200cf615c3a25951c9f8a363f635f561b8
Vladislav Burlak <vburlak@users.noreply.github.com>
2025-06-05 15:38:09 +0300

    refactor(components, hooks): add use-toast hook, update checkbox and toast

--- src/components/pages/multi-tb/branching/branching-demo/branching-demo-toast/branching-demo-toast.jsx
+++ src/components/pages/multi-tb/branching/branching-demo/branching-demo-toast/branching-demo-toast.jsx
@@ -1,15 +1,20 @@
 'use client';
 
 import * as Toast from '@radix-ui/react-toast';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const BranchingDemoToast = ({ open, onOpenChange, message }) => (
+const BranchingDemoToast = ({ open, onOpenChange, duration = 4000, message }) => (
   <Toast.Provider swipeDirection="right">
     <Toast.Root
-      className="data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=end]:animate-swipeOut rounded-md border border-[#1D2025] bg-[#121418] px-3 py-2 text-gray-new-70 drop-shadow-[0_4px_40px_0_rgba(0,0,0,0.25)] data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:transition-[transform_200ms_ease-out]"
+      className={clsx(
+        'rounded-md border border-[#1D2025] bg-[#121418] px-3 py-2 text-gray-new-70 drop-shadow-[0_4px_40px_0_rgba(0,0,0,0.25)]',
+        'data-[state=closed]:animate-fade-out-overlay data-[state=open]:animate-fade-in-overlay',
+        'data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:transition-[transform_200ms_ease-out]'
+      )}
       open={open}
-      duration={4000}
+      duration={duration}
       onOpenChange={onOpenChange}
     >
       <Toast.Title className="whitespace-nowrap text-sm leading-snug tracking-extra-tight">
@@ -23,6 +28,7 @@ const BranchingDemoToast = ({ open, onOpenChange, message }) => (
 BranchingDemoToast.propTypes = {
   open: PropTypes.bool.isRequired,
   onOpenChange: PropTypes.func.isRequired,
+  duration: PropTypes.number,
   message: PropTypes.string.isRequired,
 };
 
--- src/components/pages/multi-tb/branching/branching-demo/checkbox/checkbox.jsx
+++ src/components/pages/multi-tb/branching/branching-demo/checkbox/checkbox.jsx
@@ -2,7 +2,7 @@ import { clsx } from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const CustomCheckbox = ({ checked, onChange, id, label, tabIndex }) => (
+const CustomCheckbox = ({ checked, onChange, id, label, tabIndex, disabled }) => (
   <>
     <input
       type="checkbox"
@@ -11,6 +11,7 @@ const CustomCheckbox = ({ checked, onChange, id, label, tabIndex }) => (
       className="peer pointer-events-none absolute inset-0 h-full w-full opacity-0"
       aria-label={`Select ${label}`}
       tabIndex={tabIndex}
+      disabled={disabled}
       onChange={onChange}
     />
     <div
@@ -19,7 +20,7 @@ const CustomCheckbox = ({ checked, onChange, id, label, tabIndex }) => (
         'border border-white mix-blend-overlay transition-colors',
         'peer-checked:border-0 peer-checked:bg-white peer-checked:mix-blend-normal',
         'peer-hover:border-white peer-hover:mix-blend-normal',
-        'group-focus:ring group-focus:ring-white group-focus:ring-opacity-50'
+        !disabled && 'group-focus:ring group-focus:ring-white group-focus:ring-opacity-50'
       )}
       role="presentation"
     >
@@ -46,6 +47,7 @@ CustomCheckbox.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   tabIndex: PropTypes.number,
+  disabled: PropTypes.bool,
 };
 
 export default CustomCheckbox;
