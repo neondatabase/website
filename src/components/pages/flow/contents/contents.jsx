3652a2818f6312d47d936e156472bfd1b252cf3b
Saimon Kataev <saimon.kat23@gmail.com>
2025-07-09 01:26:13 +0200

    feat: add anchors for contents sections

--- src/components/pages/flow/contents/contents.jsx
+++ src/components/pages/flow/contents/contents.jsx
@@ -6,10 +6,11 @@ const Contents = ({ contents }) => (
   <section className="safe-paddings table-of-contents mt-[136px] w-full xl:mt-[120px] lg:mt-28 md:mt-20">
     <h2 className="sr-only">Table of contents</h2>
     <ul className="w-full">
-      {contents.map(({ section, items }, index) => (
+      {contents.map(({ section, id, items }, index) => (
         <li
           className="flex flex-col gap-5 border-t border-gray-new-15/80 py-6 first:border-0 first:pt-0 last:pb-0 md:gap-4 md:py-5"
-          key={section}
+          key={id}
+          id={id}
         >
           <h3 className="text-2xl font-medium leading-tight tracking-tighter lg:text-xl">
             {section}
