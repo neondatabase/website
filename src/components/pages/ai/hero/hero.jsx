ffbaace3f567e8f6b243ab57f4e367516ab9b43d
Vladislav Burlak <vburlak@users.noreply.github.com>
2025-07-14 15:26:51 +0300

    refactor(components): update ai/hero

--- src/components/pages/ai/hero/hero.jsx
+++ src/components/pages/ai/hero/hero.jsx
@@ -8,7 +8,7 @@ import sparks from './images/sparks.png';
 
 const Hero = () => (
   <section className="hero safe-paddings relative pt-[136px] lg:pt-16 md:pt-12">
-    <Container className="md:max-w-[640px] md:px-5" size="768">
+    <Container className="md:max-w-[640px] md:px-5" size="832">
       <div className="relative mb-10 size-[72px] lg:mb-8 lg:size-16 lg:rounded-[14px] md:mb-7 md:size-14">
         <Image
           className="relative z-10 size-full rounded-2xl shadow-[0px_5px_14px_0px_rgba(0,0,0,0.6)] md:rounded-xl"
@@ -28,10 +28,10 @@ const Hero = () => (
           aria-hidden
         />
       </div>
-      <h1 className="max-w-[646px] font-title text-[60px] font-medium leading-none tracking-extra-tight xl:text-[56px] lg:max-w-xl lg:text-5xl md:max-w-full md:text-4xl">
-        Postgres for&nbsp;AI. <br className="md:hidden" />
+      <h1 className="max-w-3xl font-title text-[60px] font-medium leading-none tracking-extra-tight xl:text-[56px] lg:max-w-xl lg:text-5xl md:max-w-full md:text-4xl">
+        Neon for&nbsp;AI.{' '}
         <span className="text-gray-new-60">
-          Perfect for Agents, AI&nbsp;coding, or just&nbsp;vibing.
+          Designed for agents. Optimized for vectors. Backed by Postgres.
         </span>
       </h1>
       <Button
