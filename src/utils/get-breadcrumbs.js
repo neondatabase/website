ee5794e470d5d32b50621149d574b325a5643cd6
Saimon Kataev <saimon.kat23@gmail.com>
2025-09-12 15:57:37 +0200

    refactor: breadcrumbs for docs and postgresql

--- src/utils/get-breadcrumbs.js
+++ src/utils/get-breadcrumbs.js
@@ -1,8 +1,21 @@
 // eslint-disable-next-line import/no-import-module-exports
 import { getNormalizedChildren } from './get-flat-sidebar';
 
+const { DOCS_HOME_SLUG, POSTGRESQL_HOME_SLUG } = require('../constants/docs');
+
 const getBreadcrumbs = (slug, flatSidebar, sidebar) => {
-  const path = flatSidebar.find((i) => i.slug === slug)?.path;
+  // Hide breadcrumbs for index pages
+  if (slug === DOCS_HOME_SLUG || slug === POSTGRESQL_HOME_SLUG) {
+    return [];
+  }
+
+  const matchingItems = flatSidebar.filter((i) => i.slug === slug);
+  const path =
+    matchingItems.length > 0
+      ? matchingItems.reduce((longest, current) =>
+          current.path.length > longest.path.length ? current : longest
+        ).path
+      : null;
 
   if (!path) return [];
 
@@ -21,7 +34,9 @@ const getBreadcrumbs = (slug, flatSidebar, sidebar) => {
     return current;
   }, sidebar);
 
-  return crumbs.slice(1);
+  // If we only have one breadcrumb and it's the target item, keep it
+  // Otherwise, remove the first breadcrumb (which is usually a parent)
+  return crumbs.length === 1 ? crumbs : crumbs.slice(1);
 };
 
 module.exports = {
