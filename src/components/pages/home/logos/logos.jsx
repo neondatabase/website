ec3c7383466fa8275cf80f8e0a534d9638bff189
Saimon Kataev <saimon.kat23@gmail.com>
2025-11-05 17:43:58 +0100

    refactor: icon names, title centered

--- src/components/pages/home/logos/logos.jsx
+++ src/components/pages/home/logos/logos.jsx
@@ -2,8 +2,8 @@ import clsx from 'clsx';
 
 import Container from 'components/shared/container';
 import bcgIcon from 'icons/home/logos/bcg.svg';
-import meta from 'icons/home/logos/meta.svg';
-import outfrontIcon from 'icons/home/logos/outfront7.svg';
+import metaIcon from 'icons/home/logos/meta.svg';
+import outfrontIcon from 'icons/home/logos/outfront.svg';
 import pepsiIcon from 'icons/home/logos/pepsi.svg';
 import replitIcon from 'icons/home/logos/replit.svg';
 import retoolIcon from 'icons/home/logos/retool.svg';
@@ -30,7 +30,7 @@ const logos = [
     className: 'lg:h-[14px]',
   },
   {
-    logo: meta,
+    logo: metaIcon,
     alt: 'Meta',
     height: 20,
     width: 100,
@@ -59,7 +59,7 @@ const Logos = () => (
       className="z-20 flex items-center gap-x-[111px] gap-y-6 xl:max-w-[960px] xl:gap-x-20 lg:justify-center lg:gap-x-[42px] md:flex-col sm:items-start"
       size="1100"
     >
-      <h2 className="max-w-[400px] text-[36px] font-medium leading-dense tracking-extra-tight text-white xl:text-[32px] lg:max-w-xs lg:text-[26px] md:max-w-full sm:text-[22px]">
+      <h2 className="max-w-[400px] text-[36px] font-medium leading-dense tracking-extra-tight text-white xl:text-[32px] lg:max-w-xs lg:text-[26px] md:w-full md:max-w-full md:text-center sm:text-[22px]">
         Trusted in production by&nbsp;thousands of&nbsp;teams.
       </h2>
       <ul className="flex shrink flex-wrap items-center gap-x-14 gap-y-12 xl:gap-x-12 xl:gap-y-9 lg:max-w-[50%] lg:justify-normal lg:gap-x-8 lg:gap-y-7 md:max-w-[95%] md:justify-center sm:gap-x-7 sm:gap-y-5">
