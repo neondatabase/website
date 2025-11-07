d8d46f306c6bfb68b957810dea960c4711a5c600
Raman Yasel <raman.yasel@gmail.com>
2025-11-07 12:07:00 +0300

    feat: add branching adaptive

--- src/components/pages/home-new/branching/branching.jsx
+++ src/components/pages/home-new/branching/branching.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 import Container from 'components/shared/container';
@@ -30,29 +31,46 @@ const ITEMS = [
 
 const Branching = () => (
   <section
-    className="branching safe-paddings relative scroll-mt-16 pt-[160px] lg:scroll-mt-0"
+    className="branching safe-paddings relative scroll-mt-16 pt-[160px] xl:pt-[100px] lg:scroll-mt-0 lg:pt-11 md:pt-[30px]"
     id="instant-branching"
   >
-    <Container className="relative grid grid-cols-[224px_1fr] gap-x-32 before:block" size="1600">
-      <div className="border-t border-gray-new-20 pt-[35px]">
+    <Container
+      className="relative grid grid-cols-[224px_1fr] gap-x-32 before:block xl:grid-cols-1 xl:px-16 xl:before:hidden lg:px-16"
+      size="1600"
+    >
+      <div className="min-w-0 border-t border-gray-new-20 pt-[35px] xl:border-none">
         <Heading
-          className="max-w-[930px]"
+          className="max-w-[930px] xl:max-w-[760px]"
           icon="branching"
           theme="dark"
           title="<strong>Instant branching.</strong> Develop and test new features and migrations with data & schema branches to eliminate surprises in production deploys."
         />
 
         {/* TODO: replace image with animation */}
-        <Image className="mt-14" src={branching} width={1184} height={500} quality={100} alt="" />
+        <Image
+          className={clsx(
+            'pointer-events-none relative -ml-[34px] mt-14 max-w-none',
+            'xl:mt-12 xl:w-[94vw] lg:-ml-4 lg:w-[96vw] md:left-1/2 md:-ml-[50vw] md:w-screen'
+          )}
+          src={branching}
+          width={1184}
+          height={500}
+          quality={100}
+          alt=""
+        />
 
-        <ul className="mt-11 grid grid-cols-3 gap-x-16">
+        <ul className="mt-11 grid grid-cols-3 gap-x-16 xl:mt-9 lg:mt-12 lg:gap-x-8 lg:px-8 md:mt-10 md:grid-cols-1 md:gap-y-7 md:px-0">
           {ITEMS.map(({ icon, title, description }, index) => (
-            <li className="" key={index}>
+            <li key={index}>
               <div className="flex items-center gap-x-2.5">
                 <Image className="" src={icon} width={16} height={16} alt="" />
-                <h3 className="text-base font-medium tracking-tight text-white">{title}</h3>
+                <h3 className="text-base font-medium tracking-tight text-white lg:text-[14px]">
+                  {title}
+                </h3>
               </div>
-              <p className="mt-2.5 tracking-tight text-gray-new-50">{description}</p>
+              <p className="mt-2.5 text-base tracking-tight text-gray-new-50 lg:text-[14px] md:mt-1.5 md:pr-8">
+                {description}
+              </p>
             </li>
           ))}
         </ul>
