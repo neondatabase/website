e67a8ff254362b013118177d4c4bce9fc451ff48
Andrew Koreykin <ankormoreankor@gmail.com>
2025-11-11 15:44:54 +0400

    chore: add doordash logo

--- src/components/pages/home/logos/logos.jsx
+++ src/components/pages/home/logos/logos.jsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 
 import Container from 'components/shared/container';
 import bcgIcon from 'icons/home/logos/bcg.svg';
+import doordashIcon from 'icons/home/logos/doordash.svg';
 import metaIcon from 'icons/home/logos/meta.svg';
 import outfrontIcon from 'icons/home/logos/outfront.svg';
 import pepsiIcon from 'icons/home/logos/pepsi.svg';
@@ -18,6 +19,30 @@ const logos = [
     className: 'lg:h-7',
   },
   {
+    logo: replitIcon,
+    alt: 'Replit',
+    width: 116,
+  },
+  {
+    logo: outfrontIcon,
+    alt: 'Outfront Media',
+    width: 123,
+  },
+
+  {
+    logo: doordashIcon,
+    alt: 'DoorDash',
+    height: 20,
+    width: 171,
+    className: 'lg:h-[14px]',
+  },
+  {
+    logo: bcgIcon,
+    alt: 'Boston Consulting Group',
+    width: 101,
+    className: 'lg:h-[14px]',
+  },
+  {
     logo: zimmerBiometIcon,
     alt: 'Zimmer Biomet',
     width: 146,
@@ -29,6 +54,7 @@ const logos = [
     height: 20,
     className: 'lg:h-[14px]',
   },
+
   {
     logo: metaIcon,
     alt: 'Meta',
@@ -36,33 +62,18 @@ const logos = [
     width: 100,
     className: 'lg:h-[14px]',
   },
-  {
-    logo: bcgIcon,
-    alt: 'Boston Consulting Group',
-    width: 101,
-  },
-  {
-    logo: outfrontIcon,
-    alt: 'Outfront Media',
-    width: 123,
-  },
-  {
-    logo: replitIcon,
-    alt: 'Replit',
-    width: 116,
-  },
 ];
 
 const Logos = () => (
   <section className="safe-paddings mt-[176px] xl:mt-24 lg:mt-20 sm:mt-24">
     <Container
-      className="z-20 flex items-center gap-x-[111px] gap-y-6 xl:max-w-[960px] xl:gap-x-20 lg:justify-center lg:gap-x-[42px] md:flex-col sm:items-start"
+      className="z-20 flex items-center gap-x-[109px] gap-y-6 xl:gap-x-20 xl:px-8 lg:max-w-[960px] lg:justify-center lg:gap-x-[42px] md:flex-col sm:items-start"
       size="1100"
     >
-      <h2 className="max-w-[400px] text-[36px] font-medium leading-dense tracking-extra-tight text-white xl:text-[32px] lg:max-w-xs lg:text-[26px] md:w-full md:max-w-full md:text-center sm:text-[22px]">
-        Trusted in production by&nbsp;thousands of&nbsp;teams.
+      <h2 className="w-full max-w-[400px] text-[36px] font-medium leading-dense tracking-extra-tight text-white xl:max-w-[360px] xl:text-[32px] md:w-full md:max-w-full md:text-center md:text-[26px] sm:text-[22px]">
+        Trusted in production <br /> by thousands of teams.
       </h2>
-      <ul className="flex shrink flex-wrap items-center gap-x-14 gap-y-12 xl:gap-x-12 xl:gap-y-9 lg:max-w-[50%] lg:justify-normal lg:gap-x-8 lg:gap-y-7 md:max-w-[95%] md:justify-center sm:gap-x-7 sm:gap-y-5">
+      <ul className="flex shrink flex-wrap items-center gap-11 xl:gap-x-10 xl:gap-y-9 lg:justify-normal lg:gap-x-6 lg:gap-y-7 md:justify-center sm:gap-x-7 sm:gap-y-5">
         {logos.map(({ logo, width, height, alt, className }, index) => (
           <li className={clsx('flex', index !== 0 && index !== 3 && 'justify-center')} key={index}>
             <img
