57a0a6bc316b71b3b2059e837341718cc46b6062
Saimon Kataev <saimon.kat23@gmail.com>
2025-11-19 18:48:46 +0100

    fix: heading label theme color

--- src/components/pages/home-new/heading-label/heading-label.jsx
+++ src/components/pages/home-new/heading-label/heading-label.jsx
@@ -12,7 +12,7 @@ const themeClassName = {
 const HeadingLabel = ({ className, theme = 'white', children }) => (
   <span
     className={clsx(
-      'flex w-fit items-center gap-2 font-mono-new text-xs font-medium uppercase leading-none text-gray-new-80',
+      'flex w-fit items-center gap-2 font-mono-new text-xs font-medium uppercase leading-none',
       'sm:gap-1.5 sm:text-[10px]',
       themeClassName[theme],
       className
