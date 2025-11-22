34df7908d699a9b279445250d30786ae382413bb
Saimon Kataev <saimon.kat23@gmail.com>
2025-11-22 20:19:28 +0100

    fix: heading component update for icons

--- src/components/pages/home-new/heading/heading.jsx
+++ src/components/pages/home-new/heading/heading.jsx
@@ -5,54 +5,50 @@ import PropTypes from 'prop-types';
 import autoscalingIcon from 'icons/home-new/autoscaling.svg';
 import branchingIcon from 'icons/home-new/branching.svg';
 import featuresIcon from 'icons/home-new/features.svg';
-import perfomanceIcon from 'icons/home-new/perfomance.svg';
+import performanceIcon from 'icons/home-new/performance.svg';
 
 const ICONS = {
   autoscaling: autoscalingIcon,
   branching: branchingIcon,
   features: featuresIcon,
-  perfomance: perfomanceIcon,
+  performance: performanceIcon,
 };
 
-const Heading = ({ className = '', theme, icon, title }) => {
-  const iconSrc = ICONS[icon];
-
-  return (
-    <div
-      className={clsx(
-        'flex max-w-[960px] flex-col gap-y-14',
-        'xl:max-w-[800px] xl:gap-y-12',
-        'lg:max-w-xl lg:gap-y-7',
-        'md:max-w-full',
-        className
-      )}
-    >
-      {iconSrc && (
-        <Image
-          className="pointer-events-none xl:size-12 lg:size-10 md:size-9"
-          src={iconSrc}
-          width={56}
-          height={56}
-          alt=""
-        />
-      )}
-      <h2
-        className={clsx(
-          'indent-24 text-[48px] font-normal leading-dense tracking-tighter [&>strong]:font-normal',
-          'xl:text-[40px] lg:indent-16 lg:text-[28px] md:indent-0 md:text-[24px]',
-          theme === 'dark' && 'text-gray-new-50 [&>strong]:text-white',
-          theme === 'light' && 'text-gray-new-40 [&>strong]:text-black-pure'
-        )}
-        dangerouslySetInnerHTML={{ __html: title }}
+const Heading = ({ className = '', theme = 'dark', icon, title }) => (
+  <div
+    className={clsx(
+      'flex max-w-[960px] flex-col gap-y-14',
+      'xl:max-w-[800px] xl:gap-y-12',
+      'lg:max-w-xl lg:gap-y-7',
+      'md:max-w-full',
+      className
+    )}
+  >
+    {icon && (
+      <Image
+        className="pointer-events-none xl:size-12 lg:size-10 md:size-9"
+        src={ICONS[icon]}
+        width={56}
+        height={56}
+        alt=""
       />
-    </div>
-  );
-};
+    )}
+    <h2
+      className={clsx(
+        'indent-24 text-[48px] font-normal leading-dense tracking-tighter [&>strong]:font-normal',
+        'xl:text-[40px] lg:indent-16 lg:text-[28px] md:indent-0 md:text-[24px]',
+        theme === 'dark' && 'text-gray-new-50 [&>strong]:text-white',
+        theme === 'light' && 'text-gray-new-40 [&>strong]:text-black-pure'
+      )}
+      dangerouslySetInnerHTML={{ __html: title }}
+    />
+  </div>
+);
 
 Heading.propTypes = {
   className: PropTypes.string,
   theme: PropTypes.oneOf(['light', 'dark']).isRequired,
-  icon: PropTypes.oneOf(ICONS.keys).isRequired,
+  icon: PropTypes.oneOf(ICONS.keys),
   title: PropTypes.string.isRequired,
 };
 
