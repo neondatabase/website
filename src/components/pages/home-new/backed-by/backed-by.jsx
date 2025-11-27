130bdfc2352d8478d50adf08cbbf160c51129db2
Saimon Kataev <saimon.kat23@gmail.com>
2025-11-27 13:49:04 +0100

    feat: quotes animation, styles update

--- src/components/pages/home-new/backed-by/backed-by.jsx
+++ src/components/pages/home-new/backed-by/backed-by.jsx
@@ -8,6 +8,8 @@ import bgNoise from 'images/pages/home-new/backed-by/bg-noise.jpg';
 
 import HeadingLabel from '../heading-label';
 
+import Quotes from './quotes';
+
 const ITEMS = [
   {
     icon: databaseIcon,
@@ -24,7 +26,7 @@ const ITEMS = [
 ];
 
 const BackedBy = () => (
-  <section className="backed-by safe-paddings relative overflow-hidden bg-[#E4F1EB] pb-[168px] pt-40 xl:py-[136px] lg:py-[88px] md:py-14">
+  <section className="backed-by safe-paddings relative overflow-hidden bg-[#E4F1EB] pb-[168px] pt-40 text-black-pure xl:py-[136px] lg:py-[88px] md:py-14">
     <Container className="z-10" size="1344">
       <div className="relative z-10 flex gap-16 xl:gap-[108px] xl:pl-2 lg:gap-8 lg:pl-3.5 md:gap-5 md:p-0 sm:flex-col sm:gap-20">
         <div className="flex-1 border-l border-gray-new-50 px-8 xl:pl-6 xl:pr-0 lg:pl-[18px] sm:border-none sm:pl-0">
@@ -35,7 +37,7 @@ const BackedBy = () => (
             className={clsx(
               'text-[44px] leading-dense tracking-tighter text-gray-new-40',
               'xl:text-[36px] lg:text-2xl md:text-xl',
-              '[&>strong]:font-normal [&>strong]:text-black'
+              '[&>strong]:font-normal [&>strong]:text-black-pure'
             )}
           >
             <strong>Backed by Giants. Trusted Postgres. Battle-tested scale.</strong> Neon was
@@ -51,7 +53,7 @@ const BackedBy = () => (
                   height={32}
                   alt=""
                 />
-                <h3 className="text-4xl leading-dense tracking-tighter text-black xl:text-[36px] lg:text-[28px] md:text-[24px]">
+                <h3 className="text-4xl leading-dense tracking-tighter xl:text-[36px] lg:text-[28px] md:text-[24px]">
                   {title}
                 </h3>
                 <p className="mt-1.5 tracking-extra-tight text-gray-new-40 xl:text-sm xl:leading-snug lg:mt-1 sm:text-balance">
@@ -70,32 +72,16 @@ const BackedBy = () => (
           <HeadingLabel className="md:mb-4" theme="black">
             Trusted by the best
           </HeadingLabel>
-          <blockquote className="mt-auto font-mono-new text-xl leading-snug tracking-extra-tight text-black xl:text-lg lg:text-[15px]">
-            Neon's serverless philosophy is{' '}
-            <span className="-mx-1 bg-[#39A57D]/60 px-1">aligned with our vision:</span> no
-            infrastructure to manage, no servers to provision, no database cluster to maintain.
-            <cite className="mt-5 block text-base not-italic leading-normal text-gray-new-15 xl:text-sm">
-              <span className="font-medium">Edouard Bonlieu</span> – Co-founder at Koyeb
-            </cite>
-          </blockquote>
+          <Quotes />
         </div>
       </div>
-      <span
-        className={clsx(
-          'pointer-events-none absolute -bottom-[300px] -right-[100px] aspect-[0.64] w-[460px]',
-          'rotate-[75deg] rounded-[100%] opacity-30  blur-[105px]',
-          'bg-[#39A57D]/10 bg-[linear-gradient(0deg,rgba(57,165,125,0.10),rgba(57,165,125,0.10)),radial-gradient(70.97%_70.97%_at_50%_29.03%,#61756C_21.28%,rgba(97,117,108,0.00)_100%)]',
-          'xl:-bottom-[400px] xl:-right-[220px] xl:w-[350px] xl:rotate-[30deg]'
-        )}
-        aria-hidden
-      />
     </Container>
     <Image
-      className="absolute right-0 top-0 xl:-right-[20%] xl:h-full sm:-right-1/2"
+      className="pointer-events-none absolute right-0 top-0 xl:-right-[20%] xl:h-full sm:-right-1/2"
       src={bgNoise}
       alt=""
-      width={550}
-      height={900}
+      width={1175}
+      height={927}
       quality={100}
     />
   </section>
