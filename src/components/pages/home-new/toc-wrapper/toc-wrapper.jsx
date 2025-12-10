916c8b9ec21ffc4c0b138dd87b5cfc8c5b65276f
Raman Yasel <raman.yasel@gmail.com>
2025-12-10 15:45:21 +0300

    fix: toc overflow

--- src/components/pages/home-new/toc-wrapper/toc-wrapper.jsx
+++ src/components/pages/home-new/toc-wrapper/toc-wrapper.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Toc from './toc';
 
 const TocWrapper = ({ children }) => (
-  <div className="relative">
+  <div className="relative overflow-hidden">
     {/* Position TOC absolutely aligned with container (max-width: 1600px) + left padding (32px) */}
     <div className="absolute bottom-0 left-[calc(50%-min(100vw,1600px)/2+32px)] top-0 h-full xl:hidden">
       <Toc />
