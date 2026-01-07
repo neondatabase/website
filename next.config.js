7537fa068376f677273e1441b97a4ffdc9917f32
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2026-01-07 12:00:22 -0800

    fix: add redirect for BAA signature package (#4322)

--- next.config.js
+++ next.config.js
@@ -353,6 +353,11 @@ const defaultConfig = {
         permanent: true,
       },
       {
+        source: '/baa/signed',
+        destination: 'https://ironcladapp.com/public-launch/6884048e9f9f2acee1cf6353',
+        permanent: true,
+      },
+      {
         source: '/docs/use-cases/saas-apps',
         destination: '/use-cases/postgres-for-saas',
         permanent: true,
