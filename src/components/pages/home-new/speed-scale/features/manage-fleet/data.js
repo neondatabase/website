673e27f16304b696c7cd26490b2f97b32b78e434
Saimon Kataev <saimon.kat23@gmail.com>
2026-01-08 16:10:40 +0100

    chore: update manage fleet animation sql code

--- src/components/pages/home-new/speed-scale/features/manage-fleet/data.js
+++ src/components/pages/home-new/speed-scale/features/manage-fleet/data.js
@@ -36,7 +36,9 @@ export const API_CALL_CODE =
   'curl -X <span>POST</span> https://api.neon.tech/v2/projects/:id/database';
 export const CONNECTION_STRING = 'postgresql://example@ep-812.eu-west-1.aws.neon.tech/primary';
 
-export const SQL_CODE = `CREATE TABLE IF NOT EXISTS playing_with_neon(id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL);
+export const SQL_CODE = `CREATE TABLE IF NOT EXISTS playing_with_neon(
+  id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL
+);
 INSERT INTO playing_with_neon(name, value)
-SELECT LEFT(md5(i::TEXT), 10), random() FROM generate_series(1, 10) s(i);
+SELECT LEFT(md5(i::TEXT),10),random() FROM generate_series(1,10)s(i);
 SELECT * FROM playing_with_neon;`;
