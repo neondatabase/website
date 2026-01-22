9fab2bc4ade3a5d95a9b9d39416ca4fb2dba538d
Drew <34187607+ankormoreankor@users.noreply.github.com>
2026-01-22 18:29:29 +0400

    Brand logos update (#4373)
    * chore: add png brand logos
    
    * chore: fixes
    
    * fix: replace logomarks

--- src/components/pages/brand/logomark/logomark.jsx
+++ src/components/pages/brand/logomark/logomark.jsx
@@ -1,26 +1,37 @@
+'use client';
+
 import clsx from 'clsx';
 import Image from 'next/image';
 
 import DownloadIcon from 'icons/download.inline.svg';
 
 import Section from '../section';
+import { handleDownload } from '../utils';
 
 const logos = [
   {
-    src: '/brand/neon-logomark-light-color.svg?updated=2026-01-15',
-    name: 'neon-logomark-light-color.svg',
+    svgSrc: '/brand/neon-logomark-light-color.svg?updated=2026-01-21',
+    svgName: 'neon-logomark-light-color.svg',
+    pngSrc: '/brand/neon-logomark-light-color@2x.png?updated=2026-01-21',
+    pngName: 'neon-logomark-light-color@2x.png',
   },
   {
-    src: '/brand/neon-logomark-light-mono.svg?updated=2026-01-15',
-    name: 'neon-logomark-light-mono.svg',
+    svgSrc: '/brand/neon-logomark-light-mono.svg?updated=2026-01-21',
+    svgName: 'neon-logomark-light-mono.svg',
+    pngSrc: '/brand/neon-logomark-light-mono@2x.png?updated=2026-01-21',
+    pngName: 'neon-logomark-light-mono@2x.png',
   },
   {
-    src: '/brand/neon-logomark-dark-color.svg?updated=2026-01-15',
-    name: 'neon-logomark-dark-color.svg',
+    svgSrc: '/brand/neon-logomark-dark-color.svg?updated=2026-01-21',
+    svgName: 'neon-logomark-dark-color.svg',
+    pngSrc: '/brand/neon-logomark-dark-color@2x.png?updated=2026-01-21',
+    pngName: 'neon-logomark-dark-color@2x.png',
   },
   {
-    src: '/brand/neon-logomark-dark-mono.svg?updated=2026-01-15',
-    name: 'neon-logomark-dark-mono.svg',
+    svgSrc: '/brand/neon-logomark-dark-mono.svg?updated=2026-01-21',
+    svgName: 'neon-logomark-dark-mono.svg',
+    pngSrc: '/brand/neon-logomark-dark-mono@2x.png?updated=2026-01-21',
+    pngName: 'neon-logomark-dark-mono@2x.png',
   },
 ];
 
@@ -38,30 +49,48 @@ const Logomark = () => (
           )}
           key={index}
         >
-          <a
-            className="group relative flex h-[200px] items-center justify-center lg:h-[164px] md:h-[152px]"
-            href={logo.src}
-            download={logo.name}
-          >
+          <div className="group relative flex h-[200px] items-center justify-center lg:h-[164px] md:h-[152px]">
             <Image
               className="md:w-[52px]"
-              src={logo.src}
+              src={logo.svgSrc}
               alt="Neon logomark"
               width={64}
               height={64}
               unoptimized
             />
-            <span
+            <div
               className={clsx(
-                'absolute right-2.5 top-2.5 flex h-7 w-7 items-center justify-center border',
+                'absolute right-2.5 top-2.5 flex gap-2',
                 'opacity-0 transition-opacity duration-300',
-                'border-gray-new-30 bg-gray-new-8 text-gray-new-94',
                 'group-hover:opacity-100'
               )}
             >
-              <DownloadIcon />
-            </span>
-          </a>
+              <button
+                className={clsx(
+                  'flex h-7 items-center gap-1.5 border px-2.5',
+                  'border-gray-new-30 bg-gray-new-8 text-gray-new-94',
+                  'transition-colors duration-200 hover:bg-gray-new-15'
+                )}
+                type="button"
+                onClick={() => handleDownload(logo.pngSrc, logo.pngName)}
+              >
+                <span className="text-xs font-medium">PNG</span>
+                <DownloadIcon className="h-3.5 w-3.5" />
+              </button>
+              <button
+                className={clsx(
+                  'flex h-7 items-center gap-1.5 border px-2.5',
+                  'border-gray-new-30 bg-gray-new-8 text-gray-new-94',
+                  'transition-colors duration-200 hover:bg-gray-new-15'
+                )}
+                type="button"
+                onClick={() => handleDownload(logo.svgSrc, logo.svgName)}
+              >
+                <span className="text-xs font-medium">SVG</span>
+                <DownloadIcon className="h-3.5 w-3.5" />
+              </button>
+            </div>
+          </div>
         </li>
       ))}
     </ul>
