241a6ffd659b56f9f993b1bac36f3d27d73bb701
Pavel Vinogradov <vin.pavel13@gmail.com>
2026-02-11 16:41:32 +0300

    feat: update markdown for TemplatePage

--- src/components/pages/doc/aside/aside.jsx
+++ src/components/pages/doc/aside/aside.jsx
@@ -22,9 +22,9 @@ const Aside = ({
   return (
     <div
       className={clsx(
-        'relative col-span-2 -ml-12 w-full max-w-64 xl:hidden',
+        'relative col-span-2 -ml-6 w-full max-w-64 xl:hidden',
         isTemplate
-          ? 'col-span-2 col-start-11 -ml-12 max-w-64 2xl:col-span-3 2xl:col-start-10 2xl:ml-auto 2xl:max-w-[238px]'
+          ? 'col-span-2 col-start-11 -ml-6 max-w-64 2xl:col-span-3 2xl:col-start-10 2xl:ml-auto 2xl:max-w-[238px]'
           : '',
         className
       )}
