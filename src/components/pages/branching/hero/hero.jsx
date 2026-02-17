6d538577a384974811b94fba256c1024eaccb633
Mikhail Belov <r3m8oqwe@yandex.ru>
2026-02-17 17:17:37 +0400

    chore: mdx 6.0.0 update

--- src/components/pages/branching/hero/hero.jsx
+++ src/components/pages/branching/hero/hero.jsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 
 import Container from 'components/shared/container';
+import PauseableVideo from 'components/shared/pauseable-video';
 import SectionLabel from 'components/shared/section-label';
-import background from 'images/pages/branching/bg.png';
+// import background from 'images/pages/branching/bg.png';
 
 const Hero = () => (
   <section className="hero relative min-h-[848px] w-full overflow-hidden border-b border-gray-new-20 pb-20 pt-[104px] xl:min-h-[650px] xl:pb-[136px] xl:pt-20 lg:min-h-[525px] lg:pb-[88px] lg:pt-16 md:min-h-[509px] md:pb-20 md:pt-12">
@@ -16,36 +17,19 @@ const Hero = () => (
         Ship software faster using Neon branches
       </p>
     </Container>
-    <Image
-      className="absolute bottom-0 left-0 -z-10 h-full min-h-[1150px] w-full object-cover xl:min-h-[864px] xl:object-[56.5%_50%] lg:min-h-[625px] lg:object-[56%_50%] md:min-h-[509px] md:object-[80%_50%]"
-      width={1920}
-      height={1150}
-      src={background}
-      sizes="(max-width: 767px) 850px, (max-width: 1023px) 1044px, (max-width: 1279px) 1442px, 100vw"
-      aria-hidden="true"
-      alt=""
-      priority
-    />
-    {/*
 
-    TODO: Replace the static <Image /> background with <PauseableVideo /> once the motion asset is ready.
-    Import PauseableVideo and hook up /videos/pages/branching/branching.{mp4,webm} (+ poster) when available.
-
-    Recommended video omtimization parameters:
-       mp4: -pix_fmt yuv420p -vf scale=2392:-2 -movflags faststart -vcodec libx264 -crf 20
-       webm: -c:v libvpx-vp9 -crf 20 -vf scale=2392:-2 -deadline best -an
-
-    <PauseableVideo
-      className="absolute inset-0 -z-10 h-full w-full"
-      width={1920}
-      height={848}
-      poster="/images/pages/branching/branching.jpg"
-    >
-      <source src="/videos/pages/branching/branching.mp4" type="video/mp4" />
-      <source src="/videos/pages/branching/branching.webm" type="video/webm" />
-    </PauseableVideo>
-    
-    */}
+    <div className="absolute inset-0 -z-10 h-full w-full">
+      <PauseableVideo
+        className="relative h-full w-full"
+        width={1920}
+        height={848}
+        poster="/images/pages/branching/branching-poster.jpg"
+        videoClassName="h-full w-full object-cover"
+      >
+        <source src="/videos/pages/branching/branching.mp4" type="video/mp4" />
+        <source src="/videos/pages/branching/branching.webm" type="video/webm" />
+      </PauseableVideo>
+    </div>
   </section>
 );
 
