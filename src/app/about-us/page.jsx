847ba3eba0aeee69036ef836dfeb19ebea7bb2ff
Andy Hattemer <andy@neon.tech>
2026-02-19 09:41:55 -0500

    fix: better careers link (#4517)

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -21,7 +21,7 @@ const AboutUsPage = () => (
     <CTANew
       title="Become a part of our&nbsp;team"
       description="We're looking for people who care deeply about quality to build with us."
-      buttonText="View Open Roles"
+      buttonText="View Open Roles on Databricks Careers Site"
       buttonUrl={LINKS.careers}
     />
   </Layout>
