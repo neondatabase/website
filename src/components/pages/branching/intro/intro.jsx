a7893a27227ed575aa9465652ac85f92b6324e54
Olga <olga.volotovich@mail.ru>
2026-03-10 11:17:58 +0100

    refactor: upd layout

--- src/components/pages/branching/intro/intro.jsx
+++ src/components/pages/branching/intro/intro.jsx
@@ -7,14 +7,14 @@ const Intro = () => (
   <section className="intro safe-paddings pt-[200px] xl:pt-[136px] lg:pt-28 md:pt-[88px]">
     <Container className="w-full max-w-[1344px]" size="1280">
       <div className="grid grid-cols-[768fr_534fr] gap-x-8 xl:grid-cols-[656fr_534fr] lg:grid-cols-[656fr_494fr] md:gap-y-20">
-        <div className="border-l border-gray-new-20 pl-8 lg:pl-4 md:col-span-full md:border-l-0 md:pl-0">
+        <div className="flex min-h-[422px] flex-col border-l border-gray-new-20 pl-8 xl:min-h-[416px] lg:min-h-[406px] lg:pl-4 md:col-span-full md:min-h-fit md:border-l-0 md:pl-0">
           <SectionLabel icon="arrow" theme="white">
             Database Branches
           </SectionLabel>
           <h2 className="mt-5 max-w-[544px] text-[48px] font-normal leading-dense tracking-tighter text-white xl:max-w-[448px] xl:text-[36px] lg:max-w-[336px] md:mt-4 md:max-w-none md:text-[28px]">
             Neon is Postgres with instant branching built in
           </h2>
-          <p className="mt-[190px] max-w-[448px] text-[20px] tracking-tight text-gray-new-70 xl:mt-[219px] xl:max-w-[416px] xl:text-[18px] lg:mt-[178px] lg:max-w-[336px] lg:text-base md:mt-6 md:max-w-none md:text-[15px]">
+          <p className="mt-auto max-w-[448px] text-[20px] tracking-tight text-gray-new-70 xl:max-w-[416px] xl:text-[18px] lg:max-w-[336px] lg:text-base md:mt-6 md:max-w-none md:text-[15px]">
             Instead of provisioning heavyweight database instances for every environments, you
             create branches that are available immediately.
           </p>
