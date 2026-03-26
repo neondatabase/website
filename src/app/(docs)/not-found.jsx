0da85f8796e7f5eb455f34e05e4fe447f25a0a18
Olga <olga.volotovich@mail.ru>
2026-03-26 14:42:17 +0100

    fix: 404 page for docs

---
+++ src/app/(docs)/not-found.jsx
@@ -0,0 +1,10 @@
+import Hero from 'components/pages/error/hero';
+
+const DocsNotFoundPage = () => (
+  <Hero
+    title="Page not found..."
+    text="Sorry, the page you are looking for doesn't exist or has been moved."
+  />
+);
+
+export default DocsNotFoundPage;
